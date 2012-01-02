7436d1c133300afe1b2882103d69a19132aae1f2
laufhannes <laufhannes@users.noreply.github.com>
2012-01-02 17:20:19 +0000

    * FIX: #224

--- inc/draw/Plot.WeekKM.php
+++ inc/draw/Plot.WeekKM.php
@@ -12,19 +12,24 @@ $Weeks                 = array();
 $Kilometers            = array();
 $KilometersCompetition = array();
 $possibleKM            = 0;
+$firstWeek             = 1;
+$lastWeek              = date("W", mktime(0,0,0,12,28,$Year)); // http://de.php.net/manual/en/function.date.php#49457
 
 if ($Year >= START_YEAR && $Year <= date('Y') && START_TIME != time()) {	
-	for ($w = 0; $w <= 53; $w++) {
+	for ($w = $firstWeek; $w <= $lastWeek; $w++) {
+		$Weeks[]                 = array($w-$firstWeek, ($w%5 == 0) ? $w : '');
 		$Kilometers[]            = 0;
 		$KilometersCompetition[] = 0;
 	}
 
 	$Data = Mysql::getInstance()->fetchAsArray('SELECT (`typeid` = '.CONF_WK_TYPID.') as `wk`, SUM(`distance`) as `km`, WEEK(FROM_UNIXTIME(`time`),1) as `w` FROM `'.PREFIX.'training` WHERE `sportid`='.CONF_RUNNINGSPORT.' AND YEAR(FROM_UNIXTIME(`time`))='.$Year.' GROUP BY (`typeid` = '.CONF_WK_TYPID.'), WEEK(FROM_UNIXTIME(`time`),1)');
 	foreach ($Data as $dat) {
-		if ($dat['wk'] == 1)
-			$KilometersCompetition[$dat['w']] = $dat['km'];
-		else
-			$Kilometers[$dat['w']] = $dat['km'];
+		if ($dat['w'] >= $firstWeek && $dat['w'] <= $lastWeek) {
+			if ($dat['wk'] == 1)
+				$KilometersCompetition[$dat['w']-$firstWeek] = $dat['km'];
+			else
+				$Kilometers[$dat['w']-$firstWeek] = $dat['km'];
+		}
 	}
 
 	if (CONF_RECHENSPIELE) {
@@ -39,6 +44,7 @@ if ($Year >= START_YEAR && $Year <= date('Y') && START_TIME != time()) {
 $Plot->Data[] = array('label' => 'Wettkampf-Kilometer', 'data' => $KilometersCompetition);
 $Plot->Data[] = array('label' => 'Kilometer', 'data' => $Kilometers);
 
+$Plot->setXLabels($Weeks);
 $Plot->addYAxis(1, 'left');
 $Plot->addYUnit(1, 'km');
 $Plot->setYTicks(1, 10, 0);
