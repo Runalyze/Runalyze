e07d28122960f7d39a5cd21b3751df7a5062093e
laufhannes <laufhannes@users.noreply.github.com>
2012-01-24 18:52:07 +0000

    * FIX: #265, #264

--- inc/class.InstallerUpdate.php
+++ inc/class.InstallerUpdate.php
@@ -27,6 +27,7 @@ class InstallerUpdate extends Installer {
 	 * Constructor
 	 */
 	public function __construct() {
+		$this->definePath();
 		$this->loadConfig();
 		$this->initPossibleUpdates();
 		$this->importUpdateFile();
@@ -40,14 +41,17 @@ class InstallerUpdate extends Installer {
 			'file' => '',
 			'text' => '----- bitte w&auml;hlen');
 		$this->PossibleUpdates[] = array(
+			'file' => 'update-v1.0-to-v1.1.sql',
+			'text' => 'Update zu: v1.1 - vorherige Version v1.0 (von 2012/01)');
+		$this->PossibleUpdates[] = array(
 			'file' => 'update-v0.6-to-v1.0alpha.sql',
-			'text' => 'Update zu: v1.0 alpha - vorherige Version v0.6 (Mitte August)');
+			'text' => 'Update zu: v1.0 alpha - vorherige Version v0.6 (von 2011/08)');
 		$this->PossibleUpdates[] = array(
 			'file' => 'update-v0.5-to-v1.0alpha.sql',
-			'text' => 'Update zu: v1.0 alpha - vorherige Version v0.5 (Mitte Juli)');
+			'text' => 'Update zu: v1.0 alpha - vorherige Version v0.5 (von 2011/07)');
 		$this->PossibleUpdates[] = array(
 			'file' => 'update-v0.5-to-v0.6.sql',
-			'text' => 'Update zu: v0.6 - vorherige Version v0.5 (Mitte Juli)');
+			'text' => 'Update zu: v0.6 - vorherige Version v0.5 (von 2011/07)');
 	}
 
 	/**
---
+++ inc/install/update-rev140.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `runalyze_user` CHANGE `weight` `weight` DECIMAL( 4, 1 ) NOT NULL DEFAULT '0.0';
