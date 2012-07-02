2d9fcbfedd705e8b0195eab584e3f9d629000aaa
mipapo <mipapo@39503437-fbe2-4ff6-820b-4993f4eab67a>
2012-07-02 18:20:36 +0000

    * FIX: #444

--- plugin/RunalyzePluginTool_DbBackup/class.RunalyzePluginTool_DbBackup.php
+++ plugin/RunalyzePluginTool_DbBackup/class.RunalyzePluginTool_DbBackup.php
@@ -83,7 +83,13 @@ class RunalyzePluginTool_DbBackup extends PluginTool {
 
 		echo '<ul>';
 		foreach ($Files as $File)
-			echo '<li><a href="inc/'.$this->BackupPath.$File.'" target="_blank">'.$File.'</a></li>';
+			if(USER_MUST_LOGIN) {
+				if(strpos($File, SessionHandler::getId()."-runalyze-backup-")!==false)
+					echo '<li><a href="inc/'.$this->BackupPath.$File.'" target="_blank">'.$File.'</a></li>';
+			} else 
+				echo '<li><a href="inc/'.$this->BackupPath.$File.'" target="_blank">'.$File.'</a></li>';
+			
+			
 		echo '</ul>';
 	}
 
@@ -93,25 +99,35 @@ class RunalyzePluginTool_DbBackup extends PluginTool {
 	protected function createBackup() {
 		$Backup = '';
 		$Mysql  = Mysql::getInstance();
-		$Tables = $Mysql->fetchAsNumericArray('SHOW TABLES');
-		foreach ($Tables as $Table) {
-			$TableName    = $Table[0];
+		$Tables = $Mysql->untouchedFetchArray('SHOW TABLES');
 
-			$CreateResult = $Mysql->fetchAsNumericArray('SHOW CREATE TABLE '.$TableName);
-			$ArrayOfRows  = $Mysql->fetchAsNumericArray('SELECT * FROM '.$TableName);
+		foreach ($Tables as $ATable) {
+			foreach($ATable as $Table) {
+				$TableName    = $Table;
 
-			$Backup .= 'DROP TABLE IF EXISTS '.$TableName.';'.NL.NL;
-			$Backup .= $CreateResult[0][1].';'.NL.NL;
+				$CreateResult = $Mysql->untouchedFetchArray('SHOW CREATE TABLE '.$TableName);
+				
+				
+				if($TableName == "runalyze_account" && USER_MUST_LOGIN)
+					$ArrayOfRows  = $Mysql->fetchAsNumericArray('SELECT * FROM '.$TableName.' WHERE id = \''.SessionHandler::getId().'\'');
+				else 
+					$ArrayOfRows  = $Mysql->fetchAsNumericArray('SELECT * FROM '.$TableName);
 
-			foreach ($ArrayOfRows as $Row) {
-				$Values = implode(',', array_map('DB_BACKUP_mapperForValues', $Row));
-				$Backup .= 'INSERT INTO '.$TableName.' VALUES('.$Values.');'.NL;
-			} 
+				$Backup .= 'DROP TABLE IF EXISTS '.$TableName.';'.NL.NL;
+				$Backup .= $CreateResult[0]['Create Table'].';'.NL.NL;
 
-			$Backup .= NL.NL.NL;
+				foreach ($ArrayOfRows as $Row) {
+					$Values = implode(',', array_map('DB_BACKUP_mapperForValues', $Row));
+					$Backup .= 'INSERT INTO '.$TableName.' VALUES('.$Values.');'.NL;
+				} 
+
+				$Backup .= NL.NL.NL;
+			}
 		}
-
-		$File = fopen(FRONTEND_PATH.$this->BackupPath.'runalyze-backup-'.date("Ymd-Hi").'.sql.gz', 'w+');
+		if(USER_MUST_LOGIN) 
+			$File = fopen(FRONTEND_PATH.$this->BackupPath.SessionHandler::getId().'-runalyze-backup-'.date("Ymd-Hi").'-'.substr(uniqid(rand()), -4).'.sql.gz', 'w+');
+		else
+			$File = fopen(FRONTEND_PATH.$this->BackupPath.'runalyze-backup-'.date("Ymd-Hi").'-'.substr(uniqid(rand()), -4).'.sql.gz', 'w+');
 		fwrite($File, gzencode($Backup));
 		fclose($File);
 	}
