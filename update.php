64ff2453ccb6e33bc3f7b6295f79bd694f0b02c9
laufhannes <laufhannes@users.noreply.github.com>
2012-07-14 16:42:14 +0000

    * CHANGE: finish Runalyze v1.1

--- update.php
+++ update.php
@@ -12,7 +12,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-	<meta content="text/html; charset=ISO-8859-1" http-equiv="content-type" />
+	<meta content="text/html; charset=UTF-8" http-equiv="content-type" />
 
 	<link rel="stylesheet" type="text/css" href="style.css" />
 	<link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico" />
@@ -20,7 +20,7 @@
 	<title>Update von Runalyze</title>
 
 	<script type="text/javascript" src="lib/jquery-1.7.1.min.js"></script>
-	<script type="text/javascript" src="lib/jQuery.backgroundStretch.js"></script>
+	<script type="text/javascript" src="lib/jquery.backgroundStretch.js"></script>
 </head>
 
 <body id="installer">
