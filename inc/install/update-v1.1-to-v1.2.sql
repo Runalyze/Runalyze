100ea6909895b2594ac7613ea810b77275d26beb
mipapo <mipapo@39503437-fbe2-4ff6-820b-4993f4eab67a>
2012-07-29 11:28:13 +0000

    * ADD (#490) Added Update-SQL File

---
+++ inc/install/update-v1.1-to-v1.2.sql
@@ -0,0 +1,27 @@
+/* Rev280 */
+ALTER TABLE runalyze_clothes ADD FOREIGN KEY (`accountid`) REFERENCES runalyze_account(id) ON DELETE CASCADE;
+ALTER TABLE runalyze_conf ADD FOREIGN KEY (`accountid`) REFERENCES runalyze_account(id) ON DELETE CASCADE;
+ALTER TABLE runalyze_dataset ADD FOREIGN KEY (`accountid`) REFERENCES runalyze_account(id) ON DELETE CASCADE;
+ALTER TABLE runalyze_plugin ADD FOREIGN KEY (`accountid`) REFERENCES runalyze_account(id) ON DELETE CASCADE;
+ALTER TABLE runalyze_shoe ADD FOREIGN KEY (`accountid`) REFERENCES runalyze_account(id) ON DELETE CASCADE;
+ALTER TABLE runalyze_sport ADD FOREIGN KEY (`accountid`) REFERENCES runalyze_account(id) ON DELETE CASCADE;
+ALTER TABLE runalyze_training ADD FOREIGN KEY (`accountid`) REFERENCES runalyze_account(id) ON DELETE CASCADE;
+ALTER TABLE runalyze_type ADD FOREIGN KEY (`accountid`) REFERENCES runalyze_account(id) ON DELETE CASCADE;
+ALTER TABLE runalyze_user ADD FOREIGN KEY (`accountid`) REFERENCES runalyze_account(id) ON DELETE CASCADE;
+
+delimiter |
+CREATE TRIGGER del_tr_train AFTER DELETE ON runalyze_account
+	FOR EACH ROW BEGIN
+		DELETE FROM runalyze_clothes WHERE accountid = OLD.id;
+		DELETE FROM runalyze_conf WHERE accountid = OLD.id;
+		DELETE FROM runalyze_dataset WHERE accountid = OLD.id;
+		DELETE FROM runalyze_plugin WHERE accountid = OLD.id;
+		DELETE FROM runalyze_shoe WHERE accountid = OLD.id;
+		DELETE FROM runalyze_training WHERE accountid = OLD.id;
+		DELETE FROM runalyze_type WHERE accountid = OLD.id;
+		DELETE FROM runalyze_user WHERE accountid = OLD.id;
+	END;
+|
+delimiter ;
+
+ALTER TABLE  `runalyze_account` ADD  `deletion_hash` VARCHAR( 32 ) NOT NULL;
