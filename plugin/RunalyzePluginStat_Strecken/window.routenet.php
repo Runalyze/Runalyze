97c3eba05696b4449574038e3e1a5e6bd4d0da68
laufhannes <laufhannes@users.noreply.github.com>
2012-08-07 14:09:20 +0000

    * FIX: #347

--- plugin/RunalyzePluginStat_Strecken/window.routenet.php
+++ plugin/RunalyzePluginStat_Strecken/window.routenet.php
@@ -2,8 +2,10 @@
 require '../../inc/class.Frontend.php';
 
 new Frontend();
+
+require 'class.RunalyzePluginStat_Strecken.php';
 ?>
-<h1>Streckennetz (max. 50 Strecken)</h1>
+<h1>Streckennetz</h1>
 
 <?php
 $EmptyMap = new Gmap('all', array());
@@ -11,9 +13,14 @@ $EmptyMap->outputHTML();
 
 echo Ajax::wrapJSasFunction( $EmptyMap->getCodeForInit() );
 
-$AllTrainings = Mysql::getInstance()->fetchAsArray('SELECT * FROM `'.PREFIX.'training` WHERE `arr_lat`!="" ORDER BY `id` DESC LIMIT 50');
+$AllTrainings = Mysql::getInstance()->fetchAsArray('SELECT * FROM `'.PREFIX.'training` WHERE `arr_lat`!="" ORDER BY `id` DESC LIMIT '.RunalyzePluginStat_Strecken::$MAX_ROUTES_ON_NET);
 foreach ($AllTrainings as $Training) {
 	$Map = new Gmap('all', new GpsData($Training));
 	echo Ajax::wrapJSasFunction( $Map->getCodeForPolylines(true) );
 }
 ?>
+
+<p class="info small">
+	Das Streckennetz beinhaltet die <?php echo RunalyzePluginStat_Strecken::$MAX_ROUTES_ON_NET; ?> neusten Strecken.
+	Mehr Strecken sind zur Zeit aus Performance-Gr&uuml;nden nicht m&ouml;glich.
+</p>
