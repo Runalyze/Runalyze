e0acf097f59d4c0926f064c5cd1da8174502aa48
laufhannes <laufhannes@users.noreply.github.com>
2012-08-08 14:34:31 +0000

    * FIX: #478, #467

--- inc/import/tpl/tpl.Importer.garminCommunicator.php
+++ inc/import/tpl/tpl.Importer.garminCommunicator.php
@@ -6,5 +6,22 @@
 			<img class="link" style="vertical-align:middle;" src="<?php echo Icon::getSrc(ICON::$REFRESH); ?>" onclick="$('#GCapi').attr('src', 'inc/tpl/tpl.garminCommunicator.php')" />
 		</small>
 
+<?php
+if ($hideGarmin):
+?>
+		<div id="iframe-spacer" style="width:500px;height:210px;">
+			<em>Der Communicator wird gleich geladen.</em>
+		</div>
+<?php
+	echo Ajax::wrapJSasFunction('$("#iframe-spacer").hover(function(){
+			$(\'<iframe src="inc/tpl/tpl.garminCommunicator.php" id="GCapi" width="500px" height="210px"></iframe>\').insertAfter($("#iframe-spacer"));
+			$("#iframe-spacer").remove();
+		});');
+
+else:
+?>
 		<iframe src="inc/tpl/tpl.garminCommunicator.php" id="GCapi" width="500px" height="210px"></iframe>
+<?php
+endif;
+?>
 	</div>
--- inc/training/tpl/tpl.Training.create.php
+++ inc/training/tpl/tpl.Training.create.php
@@ -2,17 +2,16 @@
 /**
  * File displaying the formular for adding a new training, called via TrainingCreator::displayWindow()
  */
-
-$Links = array();
-$Links[] = array('tag' => Ajax::change('Hochladen', 'ajax', 'upload'));
-$Links[] = array('tag' => Ajax::change('GarminCommunicator', 'ajax', 'garmin'));
-$Links[] = array('tag' => Ajax::change('Formular', 'ajax', 'formular'));
-
-echo Ajax::toolbarNavigation($Links, 'right');
-
-$hideUpload   = (CONF_TRAINING_CREATE_MODE != 'upload' || !$showUploader);
+$hideUpload   = ((CONF_TRAINING_CREATE_MODE != 'upload' && CONF_TRAINING_CREATE_MODE != 'tcx') || !$showUploader);
 $hideGarmin   = (CONF_TRAINING_CREATE_MODE != 'garmin' || !$showUploader);
 $hideFormular = (CONF_TRAINING_CREATE_MODE != 'form' && $showUploader);
+
+$Links = array();
+$Links[] = array('tag' => Ajax::change('Hochladen', 'ajax', 'upload', ($hideUpload) ? '' : 'triggered'));
+$Links[] = array('tag' => Ajax::change('GarminCommunicator', 'ajax', 'garmin', ($hideGarmin) ? '' : 'triggered'));
+$Links[] = array('tag' => Ajax::change('Formular', 'ajax', 'formular', ($hideFormular) ? '' : 'triggered'));
+
+echo Ajax::toolbarNavigation($Links, 'right');
 
 if (isset($_GET['sportid'])) {
 	$hideUpload   = true;
@@ -26,7 +25,7 @@ if (isset($_GET['sportid'])) {
 </div>
 
 <div class="change" id="garmin"<?php if ($hideGarmin) echo ' style="display:none;"'; ?>>
-	<?php $Importer->displayGarminCommunicator(); ?>
+	<?php $Importer->displayGarminCommunicator($hideGarmin); ?>
 </div>
 
 <div class="change" id="formular"<?php if ($hideFormular) echo ' style="display:none;"'; ?>>
