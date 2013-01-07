25efd265da98dc5bd42d43167f5232d7902046da
laufhannes <laufhannes@users.noreply.github.com>
2013-01-07 20:18:42 +0000

    * FIX: #679

--- inc/tpl/tpl.garminCommunicator.php
+++ inc/tpl/tpl.garminCommunicator.php
@@ -110,7 +110,7 @@ $Frontend = new Frontend(true);
 				}
 <?php
 if (strlen(CONF_GARMIN_API_KEY) > 10)
-	echo ',pathKeyPairsArray: ["'.(isset($_SERVER['HTTPS'])?'https':'http').'://'.$_SERVER['HTTP_HOST'].'","'.CONF_GARMIN_API_KEY.'"]';
+	echo ',pathKeyPairsArray: ["'.Request::getProtocol().'://'.$_SERVER['HTTP_HOST'].'","'.CONF_GARMIN_API_KEY.'"]';
 ?>
 			});
 		}
