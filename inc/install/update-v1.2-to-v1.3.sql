dd57801d8d2764dcad677a91fffd4205fe57cb4d
mipapo <mipapo@39503437-fbe2-4ff6-820b-4993f4eab67a>
2013-02-17 17:01:49 +0000

    # INSERT and UPDATE Statement for Type for every sports

--- inc/install/update-v1.2-to-v1.3.sql
+++ inc/install/update-v1.2-to-v1.3.sql
@@ -1,2 +1,4 @@
 /* Rev409 */
 ALTER TABLE  `runalyze_training` ADD  `vdot_by_time` DECIMAL( 5, 2 ) NOT NULL DEFAULT  '0' AFTER  `vdot`;
+ALTER TABLE  `runalyze_type` ADD  `sportid` INT( 11 ) NOT NULL DEFAULT  '0' AFTER  `splits`;
+UPDATE `runalyze_type` AS TY LEFT JOIN runalyze_conf AS CO ON TY.accountid = CO.accountid SET TY.sportid = CO.value WHERE CO.key LIKE  'RUNNINGSPORT'
