3c6a05fba4e265e8443773b70aebbe73c00e6dad
mipapo <mipapo@39503437-fbe2-4ff6-820b-4993f4eab67a>
2013-03-31 15:37:39 +0000

    * FIX Upgrade Queries for Sporttypes

--- inc/install/update-v1.2-to-v1.3.sql
+++ inc/install/update-v1.2-to-v1.3.sql
@@ -3,7 +3,7 @@ ALTER TABLE  `runalyze_training` ADD  `vdot_by_time` DECIMAL( 5, 2 ) NOT NULL DE
 
 /* Rev435 */
 ALTER TABLE  `runalyze_type` ADD  `sportid` INT( 11 ) NOT NULL DEFAULT  '0' AFTER  `splits`;
-UPDATE `runalyze_type` AS TY LEFT JOIN runalyze_conf AS CO ON TY.accountid = CO.accountid SET TY.sportid = CO.value WHERE CO.key LIKE  'RUNNINGSPORT';
+UPDATE `runalyze_type` AS TY LEFT JOIN runalyze_conf AS CO ON TY.accountid = CO.accountid SET TY.sportid = CO.value WHERE CO.key = 'RUNNINGSPORT';
 
 /* Rev 437 */
 ALTER TABLE `runalyze_shoe` DROP `brand`;
@@ -21,3 +21,13 @@ DELETE FROM `runalyze_conf` WHERE `key`="DESIGN_TOOLBAR_POSITION";
 DELETE FROM `runalyze_conf` WHERE `key`="PLUGIN_SHOW_MOVE_LINK";
 DELETE FROM `runalyze_conf` WHERE `key`="PLUGIN_SHOW_CONFIG_LINK";
 DELETE FROM `runalyze_conf` WHERE `key`="TRAINING_PLOTS_BELOW";
+
+/* Rev458 */
+INSERT INTO `runalyze_type` (`name`, `abbr`, `RPE`, `splits`, `sportid`, `accountid`)
+SELECT DISTINCT TY.name, TY.abbr, TY.RPE, TY.splits, TR.sportid, TR.accountid FROM `runalyze_training` AS TR 
+INNER JOIN `runalyze_type` AS TY ON TR.typeid = TY.id 
+WHERE TR.sportid IN (SELECT id FROM `runalyze_sport` WHERE `name` != 'Laufen' AND types = 1) AND TR.typeid != 0;
+UPDATE `runalyze_training` AS TR
+INNER JOIN `runalyze_type` AS TY ON TR.sportid = TY.sportid AND TR.accountid = TY.accountid
+SET TR.typeid = TY.id 
+WHERE TR.sportid IN (SELECT id FROM `runalyze_sport` WHERE `name` != 'Laufen' AND types = 1) AND TR.typeid != 0;
