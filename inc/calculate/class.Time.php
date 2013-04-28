5958fac7a24a6b80dad837c53bd916f169d8999f
laufhannes <laufhannes@users.noreply.github.com>
2013-04-28 18:57:32 +0000

    * FIX: #1471

--- inc/calculate/class.Time.php
+++ inc/calculate/class.Time.php
@@ -119,8 +119,8 @@ class Time {
 	 * @return int
 	 */
 	static public function diffOfDates($date1, $date2) {
-		if (function_exists('date_diff'))
-			return (int)date_diff(date_create($date1), date_create($date2))->format('%d');
+		//if (function_exists('date_diff'))
+		//	return (int)date_diff(date_create($date1), date_create($date2))->format('%d');
 
 		return floor(abs(strtotime($date1) - strtotime($date2)) / (3600 * 24));
 	}
