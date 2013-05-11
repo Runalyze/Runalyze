2a528228233ae0b49b0ce6e49f8ad69bc4062ecd
laufhannes <laufhannes@users.noreply.github.com>
2013-05-11 12:03:00 +0000

    * FIX: #279, #572, #590

---
+++ inc/training/plot/class.TrainingPlotCadence.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * This file contains class::TrainingPlotCadence
+ * @package Runalyze\Draw\Training
+ */
+/**
+ * Training plot for cadence
+ * @author Hannes Christiansen
+ * @package Runalyze\Draw\Training
+ */
+class TrainingPlotCadence extends TrainingPlot {
+	/**
+	 * Is this plot visible?
+	 * @return string
+	 */
+	public function isVisible() {
+		return CONF_TRAINING_SHOW_PLOT_CADENCE;
+	}
+
+	/**
+	 * Set key and title for this plot
+	 */
+	protected function setKeyAndTitle() {
+		$this->key   = 'cadence';
+		$this->title = $this->Training->Cadence()->label();
+	}
+
+	/**
+	 * Init data
+	 */
+	protected function initData() {
+		$this->Data = self::getData($this->Training);
+
+		$this->Plot->Data[] = array('label' => $this->title, 'color' => 'rgb(200,100,0)', 'data' => $this->Data);
+	}
+
+	/**
+	 * Set all properties for this plot 
+	 */
+	protected function setProperties() {
+		self::setPropertiesTo($this->Plot, 1, $this->Training, $this->Data);
+	}
+
+	/**
+	 * Get data
+	 * @param TrainingObject $Training
+	 * @return array
+	 */
+	static public function getData(TrainingObject &$Training) {
+		return $Training->GpsData()->getPlotDataForCadence();
+	}
+
+	/**
+	 * Get unit for current power mode
+	 * @return string
+	 */
+	static public function getUnitAsString() {
+		return '/min';
+	}
+
+	/**
+	 * Set properties
+	 * @param Plot $Plot
+	 * @param int $YAxis
+	 * @param TrainingObject $Training
+	 * @param array $Data 
+	 */
+	static public function setPropertiesTo(Plot &$Plot, $YAxis, TrainingObject &$Training, array $Data) {
+		$average = round(array_sum($Data)/count($Data));
+
+		$Plot->addYUnit($YAxis, $Training->Cadence()->unitAsString());
+		$Plot->setYTicks($YAxis, 10, 0);
+		//$Plot->setYLimits($YAxis, 0, Helper::ceilFor(max($Data), 100));
+
+		if ($YAxis == 1) {
+			$Plot->addThreshold('y'.$YAxis, $average, 'rgba(0,0,0,0.5)');
+			$Plot->addAnnotation(0, $average, '&oslash; '.$average.' '.self::getUnitAsString());
+		}
+	}
+}
