cd073bde13a137b24a65a61e89ea3bb2b9555198
laufhannes <laufhannes@39503437-fbe2-4ff6-820b-4993f4eab67a>
2013-05-20 08:35:25 +0000

    * FIX: bug in saving plots

--- call/savePng.php
+++ call/savePng.php
@@ -2,5 +2,6 @@
 header("Content-type: image/png");
 header("Content-Disposition: attachment; filename=".strtolower(str_replace(' ', '_', $_POST['filename'])));
 
-echo file_get_contents($_POST['image']); 
+$encodeData = substr($_POST['image'], strpos($_POST['image'], ',') + 1);
+echo base64_decode($encodeData);
 ?>
