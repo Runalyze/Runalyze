63a8eb1152b79c3a530dc52a7fe169344a12bf94
laufhannes <laufhannes@39503437-fbe2-4ff6-820b-4993f4eab67a>
2013-07-04 06:53:23 +0000

    * FIX: #1539

--- inc/install/update-v1.2-to-v1.3.sql
+++ inc/install/update-v1.2-to-v1.3.sql
@@ -28,8 +28,7 @@ SELECT DISTINCT TY.name, TY.abbr, TY.RPE, TY.splits, TR.sportid, TR.accountid FR
 INNER JOIN `runalyze_type` AS TY ON TR.typeid = TY.id 
 WHERE TR.sportid IN (SELECT id FROM `runalyze_sport` WHERE `name` != 'Laufen' AND types = 1) AND TR.typeid != 0;
 UPDATE `runalyze_training` AS TR
-INNER JOIN `runalyze_type` AS TY ON TR.sportid = TY.sportid AND TR.accountid = TY.accountid
-SET TR.typeid = TY.id 
+INNER JOIN `runalyze_type` AS TY ON TR.sportid = TY.sportid AND TR.accountid = TY.accountid SET TR.typeid = TY.id 
 WHERE TR.sportid IN (SELECT id FROM `runalyze_sport` WHERE `name` != 'Laufen' AND types = 1) AND TR.typeid != 0;
 
 /* Rev490 */
