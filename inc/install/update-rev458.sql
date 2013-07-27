28d0ac51fa12a7d14c680959735bbc298cef97d3
mipapo <mipapo@39503437-fbe2-4ff6-820b-4993f4eab67a>
2013-07-27 11:11:00 +0000

    ** FIX Wrong Type <-> Training

--- inc/install/update-rev458.sql
+++ inc/install/update-rev458.sql
@@ -3,5 +3,6 @@ SELECT DISTINCT TY.name, TY.abbr, TY.RPE, TY.splits, TR.sportid, TR.accountid FR
 INNER JOIN `runalyze_type` AS TY ON TR.typeid = TY.id 
 WHERE TR.sportid IN (SELECT id FROM `runalyze_sport` WHERE `name` != 'Laufen' AND types = 1) AND TR.typeid != 0;
 UPDATE `runalyze_training` AS TR
-INNER JOIN `runalyze_type` AS TY ON TR.sportid = TY.sportid AND TR.accountid = TY.accountid SET TR.typeid = TY.id 
+INNER JOIN `runalyze_type` AS TY ON TR.sportid = TY.sportid AND TR.accountid = TY.accountid AND TY.name = (SELECT name FROM  `runalyze_type` WHERE `id` = TR.typeid)
+SET TR.typeid = TY.id 
 WHERE TR.sportid IN (SELECT id FROM `runalyze_sport` WHERE `name` != 'Laufen' AND types = 1) AND TR.typeid != 0;
