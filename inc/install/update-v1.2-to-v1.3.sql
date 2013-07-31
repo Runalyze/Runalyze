0e59b34ebf179efbad8b4ebadc8d668048adec6f
laufhannes <laufhannes@39503437-fbe2-4ff6-820b-4993f4eab67a>
2013-07-31 17:25:12 +0000

    * FIX: "unknown system variable typeid"

--- inc/install/update-v1.2-to-v1.3.sql
+++ inc/install/update-v1.2-to-v1.3.sql
@@ -29,8 +29,7 @@ INNER JOIN `runalyze_type` AS TY ON TR.typeid = TY.id
 WHERE TR.sportid IN (SELECT id FROM `runalyze_sport` WHERE `name` != 'Laufen' AND types = 1) AND TR.typeid != 0;
 
 UPDATE `runalyze_training` AS TR
-INNER JOIN `runalyze_type` AS TY ON TR.sportid = TY.sportid AND TR.accountid = TY.accountid AND TY.name = (SELECT name FROM  `runalyze_type` WHERE `id` = TR.typeid)
-SET TR.typeid = TY.id 
+INNER JOIN `runalyze_type` AS TY ON TR.sportid = TY.sportid AND TR.accountid = TY.accountid AND TY.name = (SELECT name FROM  `runalyze_type` WHERE `id` = TR.typeid) SET TR.typeid = TY.id
 WHERE TR.sportid IN (SELECT id FROM `runalyze_sport` WHERE `name` != 'Laufen' AND types = 1) AND TR.typeid != 0;
 
 /* Rev490 */
