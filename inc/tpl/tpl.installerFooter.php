9bbae29de8e77640ee13a6c682a67d415cee6bb2
laufhannes <laufhannes@39503437-fbe2-4ff6-820b-4993f4eab67a>
2013-08-09 07:16:55 +0000

    * FIX: mobile version of login window

--- inc/tpl/tpl.installerFooter.php
+++ inc/tpl/tpl.installerFooter.php
@@ -1,4 +1,4 @@
-		<p class="c small" style="border-top:1px solid #ccc; margin-top:20px; padding-top:7px;">
+		<p class="c small installer-footer-links">
 			<a href="http://www.runalyze.de/" title="Runalyze">www.runalyze.de</a>
 			| <a href="http://www.facebook.com/Runalyze" title="Runalyze bei Facebook">bei Facebook</a>
 			| <a href="http://twitter.com/RunalyzeDE" title="Runalyze bei Twitter">bei Twitter</a>
