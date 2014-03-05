1df58fd16fc58845960b0b928568782889c26a52
laufhannes <laufhannes@users.noreply.github.com>
2014-03-05 19:51:13 +0000

    * CHANGE: work on #1707

--- inc/tpl/tpl.installerFooterText.php
+++ inc/tpl/tpl.installerFooterText.php
@@ -1,17 +1,20 @@
 		<div class="small installer-footer-text">
-			<div class="text-badge text-badge-charts">
+			<div class="text-badge">
+				<i class="fa fa-fw fa-2x fa-bar-chart-o"></i>
 				<p>
 					<strong><?php _e('Statistics'); ?></strong>
 					<?php _e('Runalyze analyses your complete training, calculates your fit, allege prognoses and visualzes your training in a chart - as detailed as none other sport diary')?>
 				</p>
 			</div>
-			<div class="text-badge text-badge-tools">
+			<div class="text-badge">
+				<i class="fa fa-fw fa-2x fa-cog"></i>
 				<p>
 					<strong><?php _e('Configuration'); ?></strong>
                                         <?php _e('Runalyze is completely configurable. You can decide by yourself, which information of your training you want to record and which statistics to be shown on which place.'); ?>
 				</p>
 			</div>
-			<div class="text-badge text-badge-heart">
+			<div class="text-badge">
+				<i class="fa fa-fw fa-2x fa-heart"></i>
 				<p>
 					<strong><?php _e('By runners for runners'); ?></strong>
                                         <?php _e('Runalyze is created by runners for runners. Whenever you have comments, problems or wishes: Please contact the Runalyze team. It\'s near and dear to us'); ?>
