a9bbb092fc4e69bf3c62245e62442f960f9dad5f
laufhannes <laufhannes@39503437-fbe2-4ff6-820b-4993f4eab67a>
2014-07-20 14:43:50 +0000

    * FIX: #1822, minor bug

---
+++ call/call.ContentPanels.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * File for displaying panels.
+ * Call:   call.ContentPanels.php
+ */
+require '../inc/class.Frontend.php';
+
+$Frontend = new Frontend();
+
+$Frontend->displayPanels();
