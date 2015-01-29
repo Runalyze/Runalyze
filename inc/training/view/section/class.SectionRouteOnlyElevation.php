a10e67e34afc0941ce0dc9cb5980760982876305
grzeg1 <grzegorz@thulium.pl>
2015-01-29 23:30:05 +0100

    option to show map first in training view

---
+++ inc/training/view/section/class.SectionRouteOnlyElevation.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * This file contains class::SectionRouteOnlyMap
+ * @package Runalyze\DataObjects\Training\View\Section
+ */
+/**
+ * Section: Route
+ * 
+ * @author Hannes Christiansen
+ * @package Runalyze\DataObjects\Training\View\Section
+ */
+class SectionRouteOnlyMap extends SectionRoute {
+	/**
+	 * Set header and rows
+	 */
+	protected function setHeaderAndRows() {
+		$this->Header = __('Course');
+
+		$this->appendRow( new SectionRouteRowMap($this->Context) );
+	}
+
+	/**
+	 * Has the training all required data?
+	 * @return bool
+	 */
+	protected function hasRequiredData() {
+		return $this->Context->hasRoute() && $this->Context->route()->hasPositionData();
+	}
+}
