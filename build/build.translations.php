83d3e908a89989ad2368dd6aa91649c53eb89fa0
grzeg1 <grzegorz@thulium.pl>
2015-02-04 20:33:35 +0100

    Fix: translation build script config path

--- build/build.translations.php
+++ build/build.translations.php
@@ -9,7 +9,7 @@ echo 'Building translations...'.PHP_EOL;
 
 $supportedLanguages=array();
 
-require('../config_lang.php');
+require(__DIR__.'/../config_lang.php');
 
 $LOCALE_DIR=__DIR__.'/../inc/locale';
 
