fbc6e97662cc5ba043c794567ea302ac630f476a
laufhannes <mail@laufhannes.de>
2015-04-12 13:11:16 +0200

    add new sport icons

--- inc/data/class.SportFactory.php
+++ inc/data/class.SportFactory.php
@@ -26,11 +26,30 @@ class SportFactory {
 	 */
 	static public function getIconOptions() {
 		$Files = array(
-			'icons8-sports_mode',
-			'icons8-running',
-			'icons8-regular_biking',
-			'icons8-swimming',
-			'icons8-yoga',
+			'icons8-Sports-Mode',
+			'icons8-Running',
+			'icons8-Regular-Biking',
+			'icons8-Swimming',
+			'icons8-Yoga',
+			// New in v2.1
+			'icons8-Climbing',
+			'icons8-Dancing',
+			'icons8-Exercise',
+			'icons8-Football',
+			'icons8-Guru',
+			'icons8-Handball',
+			'icons8-Mountain-Biking',
+			'icons8-Paddling',
+			'icons8-Pilates',
+			'icons8-Pushups',
+			'icons8-Regular-Biking',
+			'icons8-Roller-Skating',
+			'icons8-Rowing',
+			'icons8-Time-Trial-Biking',
+			'icons8-Trekking',
+			'icons8-Walking',
+			'icons8-Weightlift',
+			'icons8-skiing',
 		);
 
 		$Options = array();
--- inc/install/update-v2.0-to-v2.1.sql
+++ inc/install/update-v2.0-to-v2.1.sql
@@ -8,3 +8,10 @@ UPDATE `runalyze_sport` SET `speed`="km/h" WHERE `speed`="";
 /* 12.04.2015 - remove long run type id */
 UPDATE `runalyze_conf` SET `value`="race" WHERE `key`="TRAINING_MAP_PUBLIC_MODE" AND `value`="race-longjog";
 DELETE FROM `runalyze_conf` WHERE `key`="TYPE_ID_LONGRUN";
+
+/* 12.04.2015 - update sport icons */
+UPDATE runalyze_sport SET img="icons8-Sports-Mode" where `img`="icons8-sports_mode";
+UPDATE runalyze_sport SET img="icons8-Running" where `img`="icons8-running";
+UPDATE runalyze_sport SET img="icons8-Regular-Biking" where `img`="icons8-regular_biking";
+UPDATE runalyze_sport SET img="icons8-Swimming" where `img`="icons8-swimming";
+UPDATE runalyze_sport SET img="icons8-Yoga" where `img`="icons8-yoga";
