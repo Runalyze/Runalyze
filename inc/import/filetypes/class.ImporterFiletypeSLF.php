6b01dbec6a9464072cd3b4285e22be2c779e0da8
mipapo <michael@mipapo.de>
2015-05-14 21:05:12 +0200

    New importer for SLF Files from Sigma Data Center 4

--- inc/import/filetypes/class.ImporterFiletypeSLF.php
+++ inc/import/filetypes/class.ImporterFiletypeSLF.php
@@ -17,6 +17,31 @@ class ImporterFiletypeSLF extends ImporterFiletypeAbstract {
 	 * @param string $String string to parse
 	 */
 	protected function setParserFor($String) {
-		$this->Parser = new ParserSLFMultiple($String);
+            if($this->isFromSigmaDataCenter3($String))
+		$this->Parser = new ParserSLF3Multiple($String);
+            elseif ($this->isFromSigmaDataCenter4($String))
+                $this->Parser = new ParserSLF4Multiple($String);
+            else 
+                $this->throwErrorForUnknownFormat();
+                        
 	}
+        
+	/**
+	 * Is this string from Sigma DataCenter => V4?
+	 * @param string $String
+	 * @return bool
+	 */
+	private function isFromSigmaDataCenter4(&$String) {
+		return strpos($String, '<Entries') !== false;
+	}   
+        
+	/**
+	 * Is this string from Sigma DataCenter <= V3?
+	 * @param string $String
+	 * @return bool
+	 */
+	private function isFromSigmaDataCenter3(&$String) {
+		return strpos($String, '<LogEntries') !== false;
+	}         
+        
 }
