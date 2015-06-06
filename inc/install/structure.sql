a94d820fb667f9fa6fca510792a2eac03558afe2
laufhannes <mail@laufhannes.de>
2015-06-06 14:10:34 +0200

    add `allow_mails` to structure.sql

--- inc/install/structure.sql
+++ inc/install/structure.sql
@@ -35,7 +35,8 @@ CREATE TABLE IF NOT EXISTS `runalyze_account` (
   `changepw_hash` varchar(32) NOT NULL DEFAULT '',
   `changepw_timelimit` int(11) NOT NULL DEFAULT '0',
   `activation_hash` varchar(32) NOT NULL DEFAULT '',
-  `deletion_hash` varchar(32) NOT NULL DEFAULT ''
+  `deletion_hash` varchar(32) NOT NULL DEFAULT '',
+  `allow_mails` tinyint(1) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
