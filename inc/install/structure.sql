f1632ac5bc62f9f26109b373034d1a6c8c5b09f0
mipapo <michael@mipapo.de>
2015-06-27 15:04:07 +0200

    initial structure of equipment feature

--- inc/install/structure.sql
+++ inc/install/structure.sql
@@ -311,6 +311,58 @@ CREATE TABLE IF NOT EXISTS `runalyze_user` (
   `accountid` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `runalyze_equipment_type`
+--
+
+CREATE TABLE IF NOT EXISTS `runalyze_equipment_type` (
+`id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `input`  tinyint(1) NOT NULL DEFAULT '0',
+  `sportid` int(11) NOT NULL DEFAULT '0',
+  `max_km` decimal(6,2) NOT NULL DEFAULT '0.00',
+  `max_time` int(11) NOT NULL DEFAULT '0',
+  `accountid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Tabellenstruktur für Tabelle `runalyze_equipment`
+--
+
+CREATE TABLE IF NOT EXISTS `runalyze_equipment` (
+`id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `typeid` int(11) NOT NULL DEFAULT '0',
+  `notes` text,
+  `distance` decimal(6,2) NOT NULL DEFAULT '0.00',
+  `time` int(11) NOT NULL DEFAULT '0',
+  `additional_km` decimal(6,2) NOT NULL DEFAULT '0.00',
+  `date_start` int(11) NOT NULL DEFAULT '0',
+  `date_end` int(11) NOT NULL DEFAULT '0',
+  `accountid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Tabellenstruktur für Tabelle `runalyze_equipment_sport`
+--
+
+CREATE TABLE IF NOT EXISTS `runalyze_equipment_sport` (
+`sportid` int(11) NOT NULL,
+  `equipment_typeid` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Tabellenstruktur für Tabelle `runalyze_activity_equipment`
+--
+
+CREATE TABLE IF NOT EXISTS `runalyze_activity_equipment` (
+`activityid` int(11) NOT NULL,
+  `equipmentid` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
 --
 -- Indizes der exportierten Tabellen
 --
