Runalyze/Runalyze

commit dd694fce95f19a578c2f11286b8ea32fa7657a93

drop rpe for types
Fixes #1268.

inc/system/schemes/set.emptyValues.php
@@ -92,15 +92,15 @@ $EmptyTables['sport'] = array(
 );
 $EmptyTables['type'] = array(
 	// Sportid will be updated by AccountHandler::setSpecialConfigValuesFor
-	'columns' => array('name', 'abbr', 'RPE', 'sportid'),
+	'columns' => array('name', 'abbr', 'hr_avg', 'quality_session'),
 	'values'  => array(
-		array(__('Jogging'), __('JOG'), 4, 0),
-		array(__('Fartlek'), __('FL'), 5, 0),
-		array(__('Interval training'), __('IT'), 7, 0),
-		array(__('Tempo Run'), __('TR'), 7, 0),
-		array(__('Race'), __('RC'), 10, 0, 'TYPE_ID_RACE'),
-		array(__('Regeneration Run'), __('RG'), 2, 0),
-		array(__('Long Slow Distance'), __('LSD'), 5, 0),
-		array(__('Warm-up'), __('WU'), 1, 0)
+		array(__('Jogging'), __('JOG'), 143, 0),
+		array(__('Fartlek'), __('FL'), 150, 1),
+		array(__('Interval training'), __('IT'), 165, 1),
+		array(__('Tempo Run'), __('TR'), 165, 1),
+		array(__('Race'), __('RC'), 'TYPE_ID_RACE', 190, 1),
+		array(__('Regeneration Run'), __('RG'), 128, 0),
+		array(__('Long Slow Distance'), __('LSD'), 150, 1),
+		array(__('Warm-up'), __('WU'), 128, 0)
 	)
 );

inc/class.Dataset.php
@@ -402,7 +402,7 @@ class Dataset {
 
 			case 'typeid':
 				if (!is_null($this->Type)) {
-					if ($this->Type->rpe() > 4) {
+					if ($this->Type->isQualitySession()) {
 						return '<strong>'.$this->Type->abbreviation().'</strong>';
 					}
 

inc/core/Calculation/Activity/Calculator.php
@@ -167,8 +167,13 @@ class Calculator {
 		} elseif ($this->Activity->hrAvg() > 0) {
 			$data = array($this->Activity->hrAvg() => $this->Activity->duration());
 		} else {
-			$Factory =Context::Factory();
-			$data = array($Factory->sport($this->Activity->sportid())->avgHR() => $this->Activity->duration());
+			$Factory = Context::Factory();
+
+			if ($this->Activity->typeid() > 0) {
+				$data = array($Factory->type($this->Activity->typeid())->hrAvg() => $this->Activity->duration());
+			} else {
+				$data = array($Factory->sport($this->Activity->sportid())->avgHR() => $this->Activity->duration());
+			}
 		}
 
 		$Athlete = Context::Athlete();

inc/core/Model/Type/Object.php
@@ -28,16 +28,22 @@ class Object extends Model\ObjectWithID {
 	const ABBREVIATION = 'abbr';
 
 	/**
-	 * Key: RPE
-	 * @var string
-	 */
-	const RPE = 'RPE';
-
-	/**
 	 * Key: short display
 	 * @var string
 	 */
 	const SPORTID = 'sportid';
+
+	/**
+	 * Key: avg. heart rate
+	 * @var string
+	 */
+	const HR_AVG = 'hr_avg';
+
+	/**
+	 * Key: quality session
+	 * @var string
+	 */
+	const QUALITY_SESSION = 'quality_session';
 
 	/**
 	 * All properties
@@ -47,8 +53,9 @@ class Object extends Model\ObjectWithID {
 		return array(
 			self::NAME,
 			self::ABBREVIATION,
-			self::RPE,
-			self::SPORTID
+			self::SPORTID,
+			self::HR_AVG,
+			self::QUALITY_SESSION
 		);
 	}
 
@@ -77,18 +84,26 @@ class Object extends Model\ObjectWithID {
 	}
 
 	/**
-	 * RPE value
-	 * @return int
-	 */
-	public function rpe() {
-		return $this->Data[self::RPE];
-	}
-
-	/**
 	 * Sportid
 	 * @return int
 	 */
 	public function sportid() {
 		return $this->Data[self::SPORTID];
+	}
+
+	/**
+	 * Avg. heart rate
+	 * @return int
+	 */
+	public function hrAvg() {
+		return $this->Data[self::HR_AVG];
+	}
+
+	/**
+	 * Is quality session?
+	 * @return bool
+	 */
+	public function isQualitySession() {
+		return ($this->Data[self::QUALITY_SESSION] == 1);
 	}
 }

inc/data/class.Type.php
@@ -55,26 +55,24 @@ class Type {
 	 * @return string
 	 */
 	public function formattedAbbr() {
-		if ($this->hasHighRPE())
+		if ($this->isQualitySession())
 			return '<strong>'.$this->data['abbr'].'</strong>';
 
 		return $this->data['abbr'];
 	}
 
 	/**
-	 * Get boolean flag: Is the RPE of this type higher than 4?
-	 * @return bool
+	 * @return int
 	 */
-	public function hasHighRPE() {
-		return ($this->data['RPE'] > 4);
+	public function hrAvg() {
+		return $this->data['hr_avg'];
 	}
 
 	/**
-	 * Get RPE for this type
-	 * @return int
+	 * @return boolean
 	 */
-	public function RPE() {
-		return $this->data['RPE'];
+	public function isQualitySession() {
+		return ($this->data['quality_session'] == 1);
 	}
 
 	/**

inc/data/class.TypeFactory.php
@@ -43,7 +43,8 @@ class TypeFactory {
 		return array(
 			'name' => '?',
 			'abbr' => '?',
-			'RPE' => 0
+			'hr_avg' => 0,
+			'quality_session' => 0
 		);
 	}
 

inc/install/structure.sql
@@ -285,8 +285,9 @@ CREATE TABLE IF NOT EXISTS `runalyze_type` (
 `id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
   `abbr` varchar(5) NOT NULL DEFAULT '',
-  `RPE` smallint(2) NOT NULL DEFAULT '2',
   `sportid` int(11) NOT NULL DEFAULT '0',
+  `hr_avg` tinyint(3) unsigned NOT NULL DEFAULT '100',
+  `quality_session` tinyint(1) unsigned NOT NULL DEFAULT '0',
   `accountid` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 

inc/install/update-v2.0-to-v2.1.sql
@@ -52,3 +52,7 @@ UPDATE `runalyze_training` SET `clothes` = REPLACE(`clothes`, ' ', '');
 
 /* 07.06.2015 - don't use rpe anymore */
 ALTER TABLE `runalyze_sport` DROP `RPE`;
+ALTER TABLE `runalyze_type` ADD `hr_avg` TINYINT(3) UNSIGNED NOT NULL DEFAULT '100' AFTER `sportid`, ADD `quality_session` TINYINT(1) UNSIGNED NOT NULL DEFAULT '0' AFTER `hr_avg`;
+UPDATE `runalyze_type` SET `quality_session` = (`RPE` > 4);
+UPDATE `runalyze_type` SET `hr_avg` = IF(`RPE`>8,90+10*`RPE`,(120 + 7.5 * `RPE`));
+ALTER TABLE `runalyze_type` DROP `RPE`;

commit 3a2547eded5d7c6f7b353dcbbbea9751ce928bfa

fix installer
Fixes bug in installer introduced in
https://github.com/Runalyze/Runalyze/commit/dd694fce95f19a578c2f11286b8e
a32fa7657a93#diff-19c602792798c338fe5d8dcc88dbe8f8R101.

inc/system/schemes/set.emptyValues.php
@@ -98,7 +98,7 @@ $EmptyTables['type'] = array(
 		array(__('Fartlek'), __('FL'), 150, 1),
 		array(__('Interval training'), __('IT'), 165, 1),
 		array(__('Tempo Run'), __('TR'), 165, 1),
-		array(__('Race'), __('RC'), 'TYPE_ID_RACE', 190, 1),
+		array(__('Race'), __('RC'), 190, 1, 'TYPE_ID_RACE'),
 		array(__('Regeneration Run'), __('RG'), 128, 0),
 		array(__('Long Slow Distance'), __('LSD'), 150, 1),
 		array(__('Warm-up'), __('WU'), 128, 0)