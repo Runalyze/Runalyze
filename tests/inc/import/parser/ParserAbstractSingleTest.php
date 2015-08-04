ee37bfc53cbb96a22cf0dd575b4699a8e615aad2
Michael Pohl <michael@mipapo.de>
2015-08-04 19:07:12 +0200

    Update ParserAbstractSingleTest.php

--- tests/inc/import/parser/ParserAbstractSingleTest.php
+++ tests/inc/import/parser/ParserAbstractSingleTest.php
@@ -13,7 +13,6 @@ class ParserAbstractSingle_MockTester extends ParserAbstractSingle {
 					$this->gps[$key][] = $GPSdata[$key][$i];
 				}
 
-				//$this->gps['pace'][] = $this->getCurrentPace();
 			}
 		} else {
 			$this->gps = array_merge($this->gps, $GPSdata);
