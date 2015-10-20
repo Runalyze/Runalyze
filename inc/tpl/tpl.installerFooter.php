2df79347b18675c7e42e724cd96888f28ccc1df9
mipapo <michael@mipapo.de>
2015-10-20 20:38:49 +0200

    added help and support link

--- inc/tpl/tpl.installerFooter.php
+++ inc/tpl/tpl.installerFooter.php
@@ -1,7 +1,9 @@
 
 			<p class="small clearfix">
-				<a class="right" href="http://blog.runalyze.com/" title="Runalyze - <?php _e('run diary'); ?>">Runalyze</a>
-				&copy; <?php echo date('Y'); ?> RUNALYZE Team.
+			<a class="right" href="http://blog.runalyze.com/" title="Runalyze - <?php _e('run diary'); ?>">www.runalyze.de</a>
+                                &copy; <?php echo date('Y'); ?> RUNALYZE Team    &middot;
+<a href="http://help.runalyze.com"  title="<?php _e('Help and support'); ?>"><?php _e('Help and support'); ?></a> 
+
 			</p>
 		</div>
 	</div>
