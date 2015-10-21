a091b856a7fb0dffb1bc8d3a33403c9ebb54a160
Michael Pohl <michael@mipapo.de>
2015-10-21 16:09:30 +0200

    Update tpl.installerFooter.php

--- inc/tpl/tpl.installerFooter.php
+++ inc/tpl/tpl.installerFooter.php
@@ -1,8 +1,8 @@
 
 			<p class="small clearfix">
-			<a class="right" href="http://blog.runalyze.com/" title="Runalyze - <?php _e('run diary'); ?>">www.runalyze.de</a>
+			<a class="right" href="http://blog.runalyze.com/" title="Runalyze - <?php _e('run diary'); ?>">RUNALYZE - Blog</a>
                                 &copy; <?php echo date('Y'); ?> RUNALYZE Team    &middot;
-<a href="http://help.runalyze.com"  title="<?php _e('Help and support'); ?>"><?php _e('Help and support'); ?></a> 
+<a href="http://help.runalyze.com" target="_blank" title="<?php _e('Help and support'); ?>"><?php _e('Help and support'); ?></a> 
 
 			</p>
 		</div>
