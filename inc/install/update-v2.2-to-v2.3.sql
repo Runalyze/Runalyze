4589cbaeb323324e40608026d19099d68d9bdc11
Michael Pohl <pohl@physik.uni-kiel.de>
2015-11-03 07:44:25 +0100

    updated structure & update file

--- inc/install/update-v2.2-to-v2.3.sql
+++ inc/install/update-v2.2-to-v2.3.sql
@@ -1,2 +1,5 @@
 /* 18.10.2015 - drop 'types' flag for sport */
 ALTER TABLE `runalyze_sport` DROP `types`;
+
+/* 02.11.2015 - geohashes for _route table */
+ALTER TABLE `runalyze_route` ADD `geohashes` longtext AFTER `lngs`, ADD `startpoint` char(10) AFTER `startpoint_lng`, ADD `endpoint` char(10) AFTER `endpoint_lng`,  ADD `min` char(10) AFTER `min_lng`, ADD `max` char(10) AFTER `max_lng`;
