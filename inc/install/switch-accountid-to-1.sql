b80f72291978bbd2b32510a95947fac4d80ee4f2
Hannes <mail@laufhannes.de>
2015-11-04 06:38:46 +0100

    fix switch-accountid-to-1.sql
    Fixes #1564.

--- inc/install/switch-accountid-to-1.sql
+++ inc/install/switch-accountid-to-1.sql
@@ -2,10 +2,12 @@
 --
 -- RUNALYZE v2.2+ requires even single-user installations to have a real account
 
+UPDATE `runalyze_clothes` SET `accountid`='1';
 UPDATE `runalyze_conf` SET `accountid`='1';
 UPDATE `runalyze_dataset` SET `accountid`='1';
 UPDATE `runalyze_plugin` SET `accountid`='1';
 UPDATE `runalyze_route` SET `accountid`='1';
+UPDATE `runalyze_shoe` SET `accountid`='1';
 UPDATE `runalyze_sport` SET `accountid`='1';
 UPDATE `runalyze_trackdata` SET `accountid`='1';
 UPDATE `runalyze_training` SET `accountid`='1';
