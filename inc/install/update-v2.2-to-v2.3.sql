101a814ebdae67b8b17c208f4e602a67d5754d34
Michael Pohl <michael@mipapo.de>
2015-11-08 21:20:53 +0100

    Update update-v2.2-to-v2.3.sql

--- inc/install/update-v2.2-to-v2.3.sql
+++ inc/install/update-v2.2-to-v2.3.sql
@@ -2,18 +2,6 @@
 ALTER TABLE `runalyze_sport` DROP `types`;
 
 /* 08.11.2015 -  add tables for tags */
-
-CREATE TABLE IF NOT EXISTS `runalyze_equipment_type` (
-`id` int(10) unsigned NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `input` tinyint(1) NOT NULL DEFAULT '0',
-  `max_km` int(11) NOT NULL DEFAULT '0',
-  `max_time` int(11) NOT NULL DEFAULT '0',
-  `accountid` int(10) unsigned NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
 CREATE TABLE IF NOT EXISTS `runalyze_tag` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `tag` varchar(50) NOT NULL,
