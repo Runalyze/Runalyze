d32499c69c0ad791d369d1437ad6bf2d706caa48
Hannes <mail@laufhannes.de>
2015-12-08 07:46:56 +0000

    fix csv epson import
    Fixes #1575.

--- tests/inc/import/filetypes/ImporterFiletypeCSVTest.php
+++ tests/inc/import/filetypes/ImporterFiletypeCSVTest.php
@@ -13,7 +13,8 @@ class ImporterFiletypeCSVTest extends PHPUnit_Framework_TestCase {
 
 	/**
 	 * Test: standard file
-	 * Filename: "Epson.csv" 
+	 * Filename: "Epson.csv"
+	 * @group epson
 	 */
 	public function testEpsonFile() {
 		$this->object->parseFile('../tests/testfiles/csv/Epson.csv');
@@ -26,7 +27,7 @@ class ImporterFiletypeCSVTest extends PHPUnit_Framework_TestCase {
 
 		$this->assertEquals(14, $this->object->object()->getDistance(), '', 0.01);
 		$this->assertEquals(1123, $this->object->object()->getCalories());
-		$this->assertEquals(168, $this->object->object()->getPulseAvg(), '', 2);
+		$this->assertEquals(171, $this->object->object()->getPulseAvg(), '', 2);
 		$this->assertTrue($this->object->object()->hasArrayAltitude());
 		$this->assertTrue($this->object->object()->hasArrayDistance());
 		$this->assertTrue($this->object->object()->hasArrayHeartrate());
@@ -35,7 +36,7 @@ class ImporterFiletypeCSVTest extends PHPUnit_Framework_TestCase {
 		$this->assertTrue($this->object->object()->hasArrayTime());
 		$this->assertTrue($this->object->object()->hasArrayCadence());
 
-		$this->assertEquals(14.002, $this->object->object()->getArrayDistanceLastPoint());
+		$this->assertEquals(14.002, $this->object->object()->getArrayDistanceLastPoint(), '', 0.01);
 		$this->assertEquals(4648, $this->object->object()->getArrayTimeLastPoint());
 		$this->assertEquals(array(82), array_slice($this->object->object()->getArrayHeartrate(), 0, 1));
 		$this->assertEquals(array(61), array_slice($this->object->object()->getArrayCadence(), 0, 1));
@@ -47,5 +48,28 @@ class ImporterFiletypeCSVTest extends PHPUnit_Framework_TestCase {
 			array(327, 314, 308, 311, 306, 316, 331, 397, 339, 351, 374, 332, 327, 305),
 			$this->object->object()->Splits()->timesAsArray()
 		);
+	}
+
+	/**
+	 * Test: standard file
+	 * Filename: "Epson.csv"
+	 * @group epson
+	 * @see https://github.com/Runalyze/Runalyze/issues/1575
+	 */
+	public function testEpsonFileWithDifferentArraySizes() {
+		$this->object->parseFile('../tests/testfiles/csv/Epson-different-array-sizes.csv');
+
+		$this->assertFalse($this->object->hasMultipleTrainings());
+		$this->assertFalse($this->object->failed());
+
+		$sizeTimeData = count($this->object->object()->getArrayTime());
+
+		$this->assertEquals($sizeTimeData, count($this->object->object()->getArrayHeartrate()), 'Heartrate array has wrong size.');
+		$this->assertEquals($sizeTimeData, count($this->object->object()->getArrayCadence()), 'Cadence array has wrong size.');
+		$this->assertEquals($sizeTimeData, count($this->object->object()->getArrayDistance()), 'Distance array has wrong size.');
+
+		$this->assertEquals($sizeTimeData, count($this->object->object()->getArrayAltitude()), 'Altitude array has wrong size.');
+		$this->assertEquals($sizeTimeData, count($this->object->object()->getArrayLongitude()), 'Latitude array has wrong size.');
+		$this->assertEquals($sizeTimeData, count($this->object->object()->getArrayLongitude()), 'Longitude array has wrong size.');
 	}
 }
