1eaf46beaf05fe0fce9fa6fc746485d2b33c7c2e
laufhannes <mail@laufhannes.de>
2016-01-16 21:42:50 +0100

    fix synchronization of startpoints for routes
    See #1702.

--- tests/inc/core/Model/Route/ObjectTest.php
+++ tests/inc/core/Model/Route/ObjectTest.php
@@ -105,4 +105,16 @@ class ObjectTest extends \PHPUnit_Framework_TestCase {
 		$this->assertEquals(array('7zzzzzzzzzzz'), $Object->get(Object::GEOHASHES));
 	}
 
+	public function testSynchronizationOfStartAndEndpoint() {
+		$Object = new Entity([]);
+		$Object->setLatitudesLongitudes(
+			[0.0, 0.0, 47.7, 47.8, 47.7, 47.8],
+			[0.0, 0.0, 7.8, 7.7, 7.8, 7.7]
+		);
+		$Object->synchronize();
+
+		$this->assertEquals('u0mx37xb9h', $Object->get(Entity::STARTPOINT));
+		$this->assertEquals('u0mrzjwzpj', $Object->get(Entity::ENDPOINT));
+	}
+
 }
