b55ee665a3db3ee08f1ffe051f5f94e81ba04860
Michael Pohl <michael@runalyze.de>
2016-01-21 15:46:39 +0000

    search: group running dynamics

--- inc/training/search/class.SearchFormular.php
+++ inc/training/search/class.SearchFormular.php
@@ -179,12 +179,12 @@ class SearchFormular extends Formular {
 		$this->addBooleanField('is_public', __('Is public'));
 		$this->addNumericConditionField('jd_intensity', __('JD points'), FormularInput::$SIZE_SMALL);
 		$this->addNumericConditionField('groundcontact', __('Ground contact'), FormularInput::$SIZE_SMALL, FormularUnit::$MS);
-		$this->addBooleanField('use_vdot', __('Uses VDOT'));
-		$this->addNumericConditionField('trimp', __('TRIMP'), FormularInput::$SIZE_SMALL);
+		$this->addNumericConditionField('groundcontact_balance', __('Ground Contact Balance'), FormularInput::$SIZE_SMALL, 'L'. FormularUnit::$PERCENT);
 		$this->addNumericConditionField('vertical_oscillation', __('Vertical oscillation'), FormularInput::$SIZE_SMALL, FormularUnit::$CM);
 		$this->addNumericConditionField('vertical_ratio', __('Vertical ratio'), FormularInput::$SIZE_SMALL, FormularUnit::$PERCENT);
-		$this->addNumericConditionField('groundcontact_balance', __('Ground Contact Balance'), FormularInput::$SIZE_SMALL, 'L'. FormularUnit::$PERCENT);
 		$this->addNumericConditionField('stride_length', __('Stride length'), FormularInput::$SIZE_SMALL, Configuration::General()->distanceUnitSystem()->strideLengthUnit());
+		$this->addBooleanField('use_vdot', __('Uses VDOT'));
+		$this->addNumericConditionField('trimp', __('TRIMP'), FormularInput::$SIZE_SMALL);
 	}
 
 	/**
