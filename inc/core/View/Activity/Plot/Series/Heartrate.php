Runalyze/Runalyze

commit 024562499d8ed8217e883b705d3442179ea2bd62

use activity's values for avg in plots
Fixes #1157.

inc/core/View/Activity/Plot/Series/Heartrate.php
@@ -46,6 +46,7 @@ class Heartrate extends ActivitySeries {
 		$this->initOptions();
 		$this->initData($context->trackdata(), Trackdata::HEARTRATE, true);
 		$this->manipulateData();
+		$this->setManualAverage(round(100*$context->activity()->hrAvg()/$this->HRmax));
 	}
 
 	/**
@@ -78,7 +79,7 @@ class Heartrate extends ActivitySeries {
 			if (\Runalyze\Context::Athlete()->knowsRestingHeartRate() && Configuration::General()->heartRateUnit()->isHRreserve()) {
 				$this->Factor = false;
 			} else {
-				$this->Factor = 100/Configuration::Data()->HRmax();
+				$this->Factor = 100/$this->HRmax;
 			}
 		}
 	}

inc/core/View/Activity/Plot/Series/Cadence.php
@@ -42,6 +42,7 @@ class Cadence extends ActivityPointSeries {
 		$this->initData($context->trackdata(), Trackdata::CADENCE);
 		$this->initStrings($cadence);
 		$this->manipulateData($cadence);
+		$this->setManualAverage($cadence->useFactor($context->activity()->cadence()));
 	}
 
 	/**

inc/core/View/Activity/Plot/Series/DistanceSeries.php
@@ -53,13 +53,4 @@ class DistanceSeries extends ActivitySeries {
 		$this->ShowMaximum = false;
 		$this->ShowMinimum = false;
 	}
-	
-	/**
-	 * Average
-	 * @param int $decimals [optional]
-	 * @return int
-	 */
-	protected function avg($decimals = 1) {
-		return parent::avg($decimals);
-	}
 }

inc/core/View/Activity/Plot/Series/GroundContact.php
@@ -30,6 +30,7 @@ class GroundContact extends ActivityPointSeries {
 	public function __construct(Activity\Context $context) {
 		$this->initOptions();
 		$this->initData($context->trackdata(), Trackdata::GROUNDCONTACT);
+		$this->setManualAverage($context->activity()->groundcontact());
 	}
 
 	/**

inc/core/View/Activity/Plot/Series/Power.php
@@ -28,6 +28,7 @@ class Power extends ActivitySeries {
 	public function __construct(Activity\Context $context) {
 		$this->initOptions();
 		$this->initData($context->trackdata(), Trackdata::POWER);
+		$this->setManualAverage($context->activity()->power());
 	}
 
 	/**

inc/core/View/Activity/Plot/Series/StrideLength.php
@@ -31,6 +31,7 @@ class StrideLength extends ActivityPointSeries {
 		$this->initOptions();
 		$this->initData($context->trackdata());
 		$this->manipulateData();
+		$this->setManualAverage($context->activity()->strideLength());
 	}
 
 	/**
@@ -75,14 +76,5 @@ class StrideLength extends ActivityPointSeries {
 				return $value * DistanceUnitSystem::FEET_MULTIPLIER / 1000 / 100;
 			}, $this->Data);
 		}
-	}
-
-	/**
-	 * Average
-	 * @param int $decimals [optional]
-	 * @return int
-	 */
-	protected function avg($decimals = 2) {
-		return parent::avg($decimals);
 	}
 }

inc/core/View/Activity/Plot/Series/Swolf.php
@@ -29,6 +29,7 @@ class Swolf extends ActivityPointSeries {
 	public function __construct(Activity\Context $context) {
 		$this->initOptions();
 		$this->initDataWithSwimdata($context);
+		$this->setManualAverage($context->activity()->swolf());
 	}
 
 	/**

inc/core/View/Activity/Plot/Series/TimeSeries.php
@@ -10,7 +10,7 @@ use Runalyze\Model\Trackdata\Object as Trackdata;
 use Runalyze\View\Activity;
 
 /**
- * Plot for: Temperature
+ * Plot for: Time
  * 
  * @author Hannes Christiansen
  * @package Runalyze\View\Activity\Plot\Series
@@ -46,14 +46,5 @@ class TimeSeries extends ActivitySeries {
 		$this->ShowAverage = false;
 		$this->ShowMaximum = false;
 		$this->ShowMinimum = false;
-	}
-
-	/**
-	 * Average
-	 * @param int $decimals [optional] 
-	 * @return int
-	 */
-	protected function avg($decimals = 1) {
-		return parent::avg($decimals);
 	}
 }

commit 3f73a9065b7c430e235eb1fb2e33610ed14b2e8c

fix avg hr in plot
Fixes a bug introduced in 024562499d8ed8217e883b705d3442179ea2bd62.

inc/core/View/Activity/Plot/Series/Heartrate.php
@@ -46,7 +46,7 @@ class Heartrate extends ActivitySeries {
 		$this->initOptions();
 		$this->initData($context->trackdata(), Trackdata::HEARTRATE, true);
 		$this->manipulateData();
-		$this->setManualAverage(round(100*$context->activity()->hrAvg()/$this->HRmax));
+		$this->setManualAverage(round($this->correctUnit($context->activity()->hrAvg()/$this->HRmax)));
 	}
 
 	/**