Runalyze/Runalyze

commit e1c407577771bd80d1631253646b32131b266caa

add cumulative moving average plot for pace
Fixes #1738.

inc/core/View/Activity/Plot/Series/PaceCumulativeAverage.php
@@ -1,0 +1,62 @@
+<?php
+/**
+ * This file contains class::PaceCumulativeAverage
+ * @package Runalyze\View\Activity\Plot\Series
+ */
+
+namespace Runalyze\View\Activity\Plot\Series;
+
+use Runalyze\Calculation\Math\MovingAverage;
+use Runalyze\Model\Trackdata\Entity as Trackdata;
+use Runalyze\View\Activity;
+
+
+/**
+ * Plot for: Pace (with cumulative moving average)
+ *
+ * @author Hannes Christiansen
+ * @package Runalyze\View\Activity\Plot\Series
+ */
+class PaceCumulativeAverage extends Pace
+{
+	/**
+	 * @var string
+	 */
+	const COLOR = 'rgb(200,200,255)';
+
+	/**
+	 * Create series
+	 * @var \Runalyze\View\Activity\Context $context
+	 */
+	public function __construct(Activity\Context $context)
+	{
+		$internalContext = clone $context;
+
+		$this->adjustTrackdata($internalContext);
+
+		parent::__construct($internalContext);
+	}
+
+	/**
+	 * Init options
+	 */
+	protected function initOptions()
+	{
+		parent::initOptions();
+
+		$this->Label = __('Moving average');
+		$this->Color = self::COLOR;
+	}
+
+	/**
+	 * Calculate cumulative moving average
+	 * @var \Runalyze\View\Activity\Context $context
+	 */
+	protected function adjustTrackdata(Activity\Context $context)
+	{
+		$MovingAverage = new MovingAverage\Cumulative($context->trackdata()->pace(), $context->trackdata()->distance());
+		$MovingAverage->calculate();
+
+		$context->trackdata()->set(Trackdata::PACE, $MovingAverage->movingAverage());
+	}
+}

inc/core/View/Activity/Plot/Pace.php
@@ -27,8 +27,11 @@ class Pace extends ActivityPlot {
 	 * @param \Runalyze\View\Activity\Context $context
 	 */
 	protected function initData(Activity\Context $context) {
-		$this->addSeries(
-			new Series\Pace($context)
-		);
+		$CumulativeSeries = new Series\PaceCumulativeAverage($context);
+
+		$this->addSeries(new Series\Pace($context));
+		$this->addSeries($CumulativeSeries, 1, false);
+
+		$this->Plot->Options['legend']['hidden'] = [$CumulativeSeries->label()];
 	}
 }

build/scripts.js
@@ -9296,6 +9296,7 @@ Licensed under the MIT license.
                 if (off) {
                     s.oldColor = s.color;
                     s.color = "#fff";
+                    options.legend.hidden.splice(options.legend.hidden.indexOf(s.label), 1);
                 }
             }
 

inc/core/Calculation/Math/MovingAverage/Cumulative.php
@@ -26,7 +26,7 @@ class Cumulative extends AbstractMovingAverage
 
         for ($i = 0; $i < $this->Length; ++$i) {
             $delta = $this->IndexData[$i] - $last;
-            $avg = ($avg * $last + $this->Data[$i] * $delta) / ($last + $delta);
+            $avg = ($last + $delta > 0) ? ($avg * $last + $this->Data[$i] * $delta) / ($last + $delta) : 0;
             $last += $delta;
 
             $this->MovingAverage[] = $avg;

inc/core/View/Plot/Series.php
@@ -86,6 +86,13 @@ class Series {
 	}
 
 	/**
+	 * @return string
+	 */
+	public function label() {
+		return $this->Label;
+	}
+
+	/**
 	 * Set color
 	 * @param string $color
 	 */

lib/flot-0.8.3/jquery.flot.hiddengraphs.js
@@ -212,6 +212,7 @@
                 if (off) {
                     s.oldColor = s.color;
                     s.color = "#fff";
+                    options.legend.hidden.splice(options.legend.hidden.indexOf(s.label), 1);
                 }
             }
 

commit b6c465ec461cb58e26a903726c9a2a0580684897

fix y-axis of pace plot
Fixes a bug introduced in e1c407577771bd80d1631253646b32131b266caa.

inc/core/View/Activity/Plot/Series/Pace.php
@@ -47,6 +47,9 @@ class Pace extends ActivitySeries {
 	 */
 	protected $paceUnitEnum;
 
+	/** @var bool */
+	protected $adjustAxis = true;
+
 	/**
 	 * Create series
 	 * @var \Runalyze\View\Activity\Context $context
@@ -114,8 +117,17 @@ class Pace extends ActivitySeries {
 
 		parent::addTo($Plot, $yAxis, $addAnnotations);
 
-		if (!$this->paceUnit->isTimeFormat()) return;
+		if ($this->paceUnit->isTimeFormat() && $this->adjustAxis) {
+			$this->adjustAxis($Plot, $yAxis);
+		}
+	}
 
+	/**
+	 * @param \Plot $Plot
+	 * @param int $yAxis
+	 */
+	protected function adjustAxis(\Plot &$Plot, $yAxis)
+	{
 		$Plot->setYAxisTimeFormat('%M:%S', $yAxis);
 
 		$min = min($this->Data);
@@ -139,14 +151,15 @@ class Pace extends ActivitySeries {
 			$max = 50 * 60 * 1000;
 		}
 
-		if (Configuration::ActivityView()->paceAxisType()->valueAsString() == PaceAxisType::AS_SPEED) {
-			$LimitMin = Configuration::ActivityView()->paceYaxisMinimum();
-			if (!$LimitMin->automatic()) $min = $LimitMin->value() * 1000;
-			$this->setYAxisForReversePace($Plot, $yAxis, $min);
-		} else {
-			$LimitMin = Configuration::ActivityView()->paceYaxisMinimum();
-			$LimitMax = Configuration::ActivityView()->paceYaxisMaximum();
+		$LimitMin = Configuration::ActivityView()->paceYaxisMinimum();
+		$LimitMax = Configuration::ActivityView()->paceYaxisMaximum();
 
+		if (Configuration::ActivityView()->paceAxisType()->valueAsString() == PaceAxisType::AS_SPEED) {
+			$min = $LimitMin->automatic() ? $min : $LimitMin->value() * 1000;
+			$max = $LimitMax->automatic() ? $max : $LimitMax->value() * 1000;
+
+			$this->setYAxisForReversePace($Plot, $yAxis, $min, $max);
+		} else {
 			if (!$LimitMin->automatic() || !$LimitMax->automatic()) {
 				$setLimits = true;
 				$autoscale = false;
@@ -180,46 +193,46 @@ class Pace extends ActivitySeries {
 	}
 
 	/**
-	 * @param Plot $plot
-	 * @param $yAxis
-	 * @param $dataMin
+	 * @param \Plot $plot
+	 * @param int $yAxis
+	 * @param int $dataMin
+	 * @param int $dataMax
 	 */
-	private function setYAxisForReversePace(Plot $plot, $yAxis, $dataMin)
+	private function setYAxisForReversePace(Plot $plot, $yAxis, $dataMin, $dataMax)
 	{
 		if ($this->paceUnitEnum == PaceUnit::MIN_PER_MILE) {
-			$min = 240;
-			$max = 3600;
-			$ticks = [300, 360, 450, 600, 900];
+			$ticks = [240, 300, 360, 450, 600, 900, 3600];
 		} else if ($this->paceUnitEnum == PaceUnit::MIN_PER_100M || $this->paceUnitEnum == PaceUnit::MIN_PER_100Y) {
-			$min = $dataMin < 60*1000*10 ? 10 : 60;
-			$max = 720;
-			$ticks = [60, 120, 180, 240];
+			$ticks = [10, 60, 120, 180, 240, 720];
 		} else if ($this->paceUnitEnum == PaceUnit::MIN_PER_500M || $this->paceUnitEnum == PaceUnit::MIN_PER_500Y) {
-			$min = 60;
-			$max = 720;
-			$ticks = [60, 120, 180, 240];
-		} else { // defaults to min/km
-			if ($dataMin < 180*1000) {
-				$min = 120;
-				$max = 3600;
-				$ticks = [120, 180, 240, 300, 360, 480, 600];
-			} else if ($dataMin < 240*1000) {
-				$min = 180;
-				$max = 3600;
-				$ticks = [180, 240, 300, 360, 480, 600];
-			} else {
-				$min = 240;
-				$max = 3600;
-				$ticks = [240, 300, 360, 480, 600];
-			}
+			$ticks = [60, 120, 180, 240, 720];
+		} else { // defaults to min/km;
+			$ticks = [120, 180, 240, 300, 360, 480, 600, 3600];
 		}
 
 		$ticks = array_map(function($v){
 			return $v*1000;
 		}, $ticks);
 
-		$plot->setYLimits($yAxis, $min*1000, $max*1000, false);
-		$plot->setYAxisLabels($yAxis, $ticks);
+		$firstIndex = 0;
+		$lastIndex = count($ticks) - 1;
 
+		foreach ($ticks as $i => $tick) {
+			if ($tick < $dataMin) {
+				$firstIndex = $i;
+			}
+
+			if ($tick < $dataMax) {
+				$lastIndex = $i + 1;
+			}
+		}
+
+		$lastIndex = min($lastIndex, count($ticks) - 1);
+		$min = $ticks[$firstIndex];
+		$max = $ticks[$lastIndex];
+		$ticks = array_slice($ticks, $firstIndex, max(1, $lastIndex - $firstIndex));
+
+		$plot->setYLimits($yAxis, $min, $max, false);
+		$plot->setYAxisLabels($yAxis, $ticks);
 	}
 }

inc/core/View/Activity/Plot/Series/PaceCumulativeAverage.php
@@ -24,6 +24,9 @@ class PaceCumulativeAverage extends Pace
 	 */
 	const COLOR = 'rgb(200,200,255)';
 
+	/** @var bool */
+	protected $adjustAxis = false;
+
 	/**
 	 * Create series
 	 * @var \Runalyze\View\Activity\Context $context