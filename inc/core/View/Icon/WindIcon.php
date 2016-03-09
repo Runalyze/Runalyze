3b95046e53b1f677dee3806f4c271d4db0e37e54
mipapo <michael@mipapo.de>
2016-03-09 11:11:13 +0100

    Beautfort Scala

--- inc/core/View/Icon/WindIcon.php
+++ inc/core/View/Icon/WindIcon.php
@@ -8,6 +8,7 @@ namespace Runalyze\View\Icon;
 
 use Runalyze\Data\Weather\WindDegree;
 use Runalyze\Data\Weather\WindSpeed;
+use Runalyze\Data\Weather\BeautfortScala;
 
 /**
  * Wind icon
@@ -27,6 +28,9 @@ class WindIcon extends \Runalyze\View\Icon
 
     /** @var \Runalyze\Data\Weather\WindDegree */
     protected $WindDegree;
+    
+    /** @var \Runalyze\Data\Weather\BeautfortScala */
+    protected $BeautfortScala;
 
     /**
      * WindIcon constructor.
@@ -37,6 +41,7 @@ class WindIcon extends \Runalyze\View\Icon
     {
         $this->WindSpeed = $windSpeed;
         $this->WindDegree = $windDegree;
+	$this->BeautfortScala = new BeautfortScala($windSpeed);
 
         $this->setDefaultTooltip();
     }
@@ -50,6 +55,7 @@ class WindIcon extends \Runalyze\View\Icon
 
         if (!$this->WindSpeed->isUnknown()) {
             $strings[] = $this->WindSpeed->string();
+	    $strings[] = $this->BeautfortScala->shortString();
         }
 
         if (!$this->WindDegree->isUnknown()) {
@@ -77,7 +83,7 @@ class WindIcon extends \Runalyze\View\Icon
             $code .= '<span class="'.self::DIRECTION_CLASS.'" style="transform:rotate('.$this->WindDegree->value().'deg);"></span> ';
         }
 
-        $code .= $this->WindSpeed->isUnknown() ? '?' : $this->WindSpeed->string(false);
+        $code .= $this->WindSpeed->isUnknown() ? '?' : $this->BeautfortScala->string(false);
         $code .= '</span>';
 
         return $code;
