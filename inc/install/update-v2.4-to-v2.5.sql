36f634dccf5d9ea8d6d28fcf4bcda9da08c09bba
mipapo <michael@mipapo.de>
2016-04-03 19:16:24 +0200

    wind_speed update query fix #1767

--- inc/install/update-v2.4-to-v2.5.sql
+++ inc/install/update-v2.4-to-v2.5.sql
@@ -10,7 +10,7 @@ INSERT INTO `runalyze_dataset` (`keyid`, `active`, `position`, `accountid`) SELE
 DELETE FROM `runalyze_conf` WHERE `key` = 'DB_SHOW_DIRECT_EDIT_LINK';
 
 /* 10.03.2016 - fix conversion of wind speed */
-UPDATE `runalyze_training` SET wind_speed=2.25*wind_speed WHERE wind_speed IS NOT NULL AND wind_deg IS NOT NULL AND humidity IS NOT NULL AND pressure IS NOT NULL AND weatherid IS NOT NULL AND temperature IS NOT NULL;
+UPDATE `runalyze_training` SET wind_speed=2.25*wind_speed WHERE wind_speed IS NOT NULL AND wind_speed > 0 AND wind_speed <=102 AND wind_deg IS NOT NULL AND humidity IS NOT NULL AND pressure IS NOT NULL AND weatherid IS NOT NULL AND temperature IS NOT NULL;
 
 /* 20.03.2016 - originally in feature/timezone on 20.12.2015 */
 ALTER TABLE `runalyze_account` ADD `timezone` SMALLINT(5) unsigned NOT NULL DEFAULT '0' AFTER `language`;
