b9fc9b60f9688d4a646c132543f9ceb14335a5dd
mipapo <michael@mipapo.de>
2016-04-26 19:22:39 +0200

    rename localisation files

--- build/build.translations.php
+++ build/build.translations.php
@@ -15,9 +15,8 @@ $LOCALE_DIR=__DIR__.'/../inc/locale';
 
 foreach ($supportedLanguages as $lang => $larr){
     $lang_dir=$LOCALE_DIR.'/'.$lang.'/LC_MESSAGES/';
-    if (is_file($lang_dir.'runalyze.po')){
-        echo $lang.' ';
-        system ('msgfmt -v '.$lang_dir.'runalyze.po -o '.$lang_dir.'runalyze.mo');
+    if (is_file($lang_dir.'messages.'.$lang.'.po')){
+	system ('msgfmt -v '.$lang_dir.'messages.'.$lang.'.po -o '.$lang_dir.'runalyze.mo');
     }
 }
 
