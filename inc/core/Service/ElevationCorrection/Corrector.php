Runalyze/Runalyze

commit 7044af41ebaaefbf786c05f997a3d12ba769e8d5

move elevation classes
See #1659.

inc/core/Service/ElevationCorrection/Corrector.php
@@ -1,21 +1,22 @@
 <?php
 /**
  * This file contains class::Corrector
- * @package Runalyze\Data\Elevation\Correction
+ * @package Runalyze\Service\ElevationCorrection
  */
 
-namespace Runalyze\Data\Elevation\Correction;
+namespace Runalyze\Service\ElevationCorrection;
 
 /**
  * Elevation corrector
  * 
  * @author Hannes Christiansen
- * @package Runalyze\Data\Elevation\Correction
+ * @package Runalyze\Service\ElevationCorrection
  */
-class Corrector {
+class Corrector
+{
 	/**
 	 * Strategy
-	 * @var \Runalyze\Data\Elevation\Correction\Strategy
+	 * @var null|\Runalyze\Service\ElevationCorrection\Strategy\AbstractStrategy
 	 */
 	protected $Strategy = null;
 
@@ -36,10 +37,11 @@ class Corrector {
 	 * @param array $latitude
 	 * @param array $longitude
 	 * @param string $strategyName
-	 * @throws \NoValidStrategyException
-	 * @throws \Runalyze\Data\Elevation\Correction\InvalidResponseException
+	 * @throws \Runalyze\Service\ElevationCorrection\NoValidStrategyException
+	 * @throws \Runalyze\Service\ElevationCorrection\Strategy\InvalidResponseException
 	 */
-	public function correctElevation(array $latitude, array $longitude, $strategyName = '') {
+	public function correctElevation(array $latitude, array $longitude, $strategyName = '')
+	{
 		$this->LatitudePoints = $latitude;
 		$this->LongitudePoints = $longitude;
 
@@ -54,17 +56,19 @@ class Corrector {
 
 	/**
 	 * Is a valid strategy set?
-	 * @return boolean
+	 * @return bool
 	 */
-	final protected function hasNoValidStrategy() {
+	final protected function hasNoValidStrategy()
+	{
 		return !($this->Strategy instanceof Strategy);
 	}
 
 	/**
 	 * @param string $strategyName
 	 */
-	protected function tryToUse($strategyName) {
-		$strategyName = 'Runalyze\\Data\\Elevation\\Correction\\'.$strategyName;
+	protected function tryToUse($strategyName)
+	{
+		$strategyName = 'Runalyze\\Strategy\\ElevationCorrection\\Strategy\\'.$strategyName;
 
 		if (class_exists($strategyName)) {
 			$this->Strategy = new $strategyName($this->LatitudePoints, $this->LongitudePoints);
@@ -78,7 +82,8 @@ class Corrector {
 	/**
 	 * Choose strategy
 	 */
-	protected function chooseStrategy() {
+	protected function chooseStrategy()
+	{
 		$this->tryToUseGeoTIFF();
 
 		if ($this->hasNoValidStrategy()) {
@@ -92,10 +97,10 @@ class Corrector {
 
 	/**
 	 * Apply strategy
-	 * @throws \NoValidStrategyException
-	 * @throws \RuntimeException
+	 * @throws \Runalyze\Service\ElevationCorrection\NoValidStrategyException
 	 */
-	protected function applyStrategy() {
+	protected function applyStrategy()
+	{
 		if ($this->hasNoValidStrategy()) {
 			throw new NoValidStrategyException('No elevation correction strategy is able to handle the data. Maybe all query limits are reached.');
 		} else {
@@ -107,7 +112,8 @@ class Corrector {
 	 * Get used strategy
 	 * @return string
 	 */
-	public function getNameOfUsedStrategy() {
+	public function getNameOfUsedStrategy()
+	{
 		$strategyName = get_class($this->Strategy);
 
 		return substr($strategyName, strrpos($strategyName, '\\')+1);
@@ -117,7 +123,8 @@ class Corrector {
 	 * Get corrected elevation
 	 * @return array
 	 */
-	public function getCorrectedElevation() {
+	public function getCorrectedElevation()
+	{
 		if ($this->hasNoValidStrategy()) {
 			return array();
 		} else {
@@ -128,8 +135,9 @@ class Corrector {
 	/**
 	 * Try to use GeoTIFF
 	 */
-	protected function tryToUseGeoTIFF() {
-		$this->Strategy = new GeoTIFF($this->LatitudePoints, $this->LongitudePoints);
+	protected function tryToUseGeoTIFF()
+	{
+		$this->Strategy = new Strategy\GeoTIFF($this->LatitudePoints, $this->LongitudePoints);
 
 		if (!$this->Strategy->canHandleData()) {
 			$this->Strategy = null;
@@ -139,8 +147,9 @@ class Corrector {
 	/**
 	 * Try to use Geonames
 	 */
-	protected function tryToUseGeonames() {
-		$this->Strategy = new Geonames($this->LatitudePoints, $this->LongitudePoints);
+	protected function tryToUseGeonames()
+	{
+		$this->Strategy = new Strategy\Geonames($this->LatitudePoints, $this->LongitudePoints);
 
 		if (!$this->Strategy->canHandleData()) {
 			$this->Strategy = null;
@@ -156,8 +165,9 @@ class Corrector {
 	 * 
 	 * @see https://developers.google.com/maps/terms?hl=de#section_10_12
 	 */
-	protected function tryToUseGoogleAPI() {
-		$this->Strategy = new GoogleMaps($this->LatitudePoints, $this->LongitudePoints);
+	protected function tryToUseGoogleAPI()
+	{
+		$this->Strategy = new Strategy\GoogleMaps($this->LatitudePoints, $this->LongitudePoints);
 
 		if (!$this->Strategy->canHandleData()) {
 			$this->Strategy = null;

call/call.Training.elevationCorrection.php
@@ -10,7 +10,7 @@ use Runalyze\Model\Activity;
 use Runalyze\Model\Route;
 use Runalyze\Calculation\Route\Calculator;
 use Runalyze\View\Activity\Linker;
-use Runalyze\Data\Elevation\Correction\NoValidStrategyException;
+use Runalyze\Service\ElevationCorrection\NoValidStrategyException;
 
 $Frontend = new Frontend();
 

inc/core/Calculation/Elevation/Calculator.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * This file contains class::Calculator
- * @package Runalyze\Data\Elevation\Calculation
+ * @package Runalyze\Calculation\Elevation
  */
 
-namespace Runalyze\Data\Elevation\Calculation;
+namespace Runalyze\Calculation\Elevation;
 
 use Runalyze\Configuration;
 use Runalyze\Parameter\Application\ElevationMethod;
@@ -13,9 +13,10 @@ use Runalyze\Parameter\Application\ElevationMethod;
  * Elevation calculator
  * 
  * @author Hannes Christiansen
- * @package Runalyze\Data\Elevation\Calculation
+ * @package Runalyze\Calculation\Elevation
  */
-class Calculator {
+class Calculator
+{
 	/**
 	 * Elevation points
 	 * @var array
@@ -36,7 +37,7 @@ class Calculator {
 
 	/**
 	 * Smoothing strategy
-	 * @var \Runalyze\Data\Elevation\Calculation\Strategy
+	 * @var \Runalyze\Calculation\Elevation\Strategy\AbstractStrategy
 	 */
 	protected $Strategy;
 
@@ -64,10 +65,11 @@ class Calculator {
 	 * If no options are set, the current configuration settings are used.
 	 * 
 	 * @param array $ElevationPoints
-	 * @param \Runalyze\Parameter\Application\ElevationMethod $Method [optional]
-	 * @param int $Threshold [optional]
+	 * @param \Runalyze\Parameter\Application\ElevationMethod|null $Method [optional]
+	 * @param int|null $Threshold [optional]
 	 */
-	public function __construct($ElevationPoints, ElevationMethod $Method = null, $Threshold = null) {
+	public function __construct(array $ElevationPoints, ElevationMethod $Method = null, $Threshold = null)
+	{
 		$this->ElevationPoints = $ElevationPoints;
 		$this->Method = !is_null($Method) ? $Method : Configuration::ActivityView()->elevationMethod();
 		$this->Threshold = !is_null($Threshold) ? $Threshold : Configuration::ActivityView()->elevationMinDiff();
@@ -77,7 +79,8 @@ class Calculator {
 	 * Set threshold
 	 * @param int $threshold
 	 */
-	public function setThreshold($threshold) {
+	public function setThreshold($threshold)
+	{
 		$this->Threshold = $threshold;
 	}
 
@@ -85,7 +88,8 @@ class Calculator {
 	 * Set method
 	 * @param \Runalyze\Parameter\Application\ElevationMethod $Method
 	 */
-	public function setMethod(ElevationMethod $Method) {
+	public function setMethod(ElevationMethod $Method)
+	{
 		$this->Method = $Method;
 	}
 
@@ -93,7 +97,8 @@ class Calculator {
 	 * Get elevation
 	 * @return int
 	 */
-	public function totalElevation() {
+	public function totalElevation()
+	{
 		return max($this->Up, $this->Down);
 	}
 
@@ -101,7 +106,8 @@ class Calculator {
 	 * Get elevation up
 	 * @return int
 	 */
-	public function elevationUp() {
+	public function elevationUp()
+	{
 		return $this->Up;
 	}
 
@@ -109,14 +115,16 @@ class Calculator {
 	 * Get elevation up
 	 * @return int
 	 */
-	public function elevationDown() {
+	public function elevationDown()
+	{
 		return $this->Down;
 	}
 
 	/**
 	 * Calculate
 	 */
-	public function calculate() {
+	public function calculate()
+	{
 		$this->chooseStrategy();
 
 		if (!empty($this->ElevationPoints)) {
@@ -130,23 +138,25 @@ class Calculator {
 	/**
 	 * Choose strategy
 	 */
-	protected function chooseStrategy() {
+	protected function chooseStrategy()
+	{
 		if ($this->Method->usesThreshold()) {
-			$this->Strategy = new Threshold($this->ElevationPoints, $this->Threshold);
+			$this->Strategy = new Strategy\Threshold($this->ElevationPoints, $this->Threshold);
 		} elseif ($this->Method->usesDouglasPeucker()) {
-			$this->Strategy = new DouglasPeucker($this->ElevationPoints, $this->Threshold);
+			$this->Strategy = new Strategy\DouglasPeucker($this->ElevationPoints, $this->Threshold);
 		} elseif ($this->Method->usesReumannWitkam()) {
-			$this->Strategy = new ReumannWitkam($this->ElevationPoints);
+			$this->Strategy = new Strategy\ReumannWitkam($this->ElevationPoints);
 		} else {
-			$this->Strategy = new NoSmoothing($this->ElevationPoints);
+			$this->Strategy = new Strategy\NoSmoothing($this->ElevationPoints);
 		}
 	}
 
 	/**
 	 * Run strategy
 	 */
-	protected function runStrategy() {
-		if ($this->Strategy instanceof Strategy) {
+	protected function runStrategy()
+	{
+		if ($this->Strategy instanceof Strategy\AbstractStrategy) {
 			$this->Strategy->runSmoothing();
 			$smoothedData = $this->Strategy->smoothedData();
 		} else {
@@ -163,9 +173,10 @@ class Calculator {
 
 	/**
 	 * Smoothing strategy
-	 * @return \Runalyze\Data\Elevation\Calculation\Strategy
+	 * @return \Runalyze\Calculation\Elevation\Strategy\AbstractStrategy
 	 */
-	public function strategy() {
+	public function strategy()
+	{
 		return $this->Strategy;
 	}
 }

inc/core/Calculation/Elevation/Strategy/AbstractStrategy.php
@@ -1,18 +1,19 @@
 <?php
 /**
  * This file contains class::Strategy
- * @package Runalyze\Data\Elevation\Calculation
+ * @package Runalyze\Calculation\Elevation\Strategy
  */
 
-namespace Runalyze\Data\Elevation\Calculation;
+namespace Runalyze\Calculation\Elevation\Strategy;
 
 /**
  * Abstract strategy to smooth elevation data for calculation
  *
  * @author Hannes Christiansen
- * @package Runalyze\Data\Elevation\Calculation
+ * @package Runalyze\Calculation\Elevation\Strategy
  */
-abstract class Strategy {
+abstract class AbstractStrategy
+{
 	/**
 	 * Original elevation data
 	 * @var array
@@ -35,7 +36,8 @@ abstract class Strategy {
 	 * Construct
 	 * @param array $elevation
 	 */
-	public function __construct(array $elevation) {
+	public function __construct(array $elevation)
+	{
 		$this->ElevationData = $elevation;
 	}
 
@@ -48,7 +50,8 @@ abstract class Strategy {
 	 * Get smoothed data
 	 * @return array
 	 */
-	final public function smoothedData() {
+	final public function smoothedData()
+	{
 		return $this->SmoothedData;
 	}
 
@@ -56,7 +59,8 @@ abstract class Strategy {
 	 * Get used indices
 	 * @return array
 	 */
-	final public function smoothingIndices() {
+	final public function smoothingIndices()
+	{
 		return $this->SmoothingIndices;
 	}
 
@@ -70,7 +74,8 @@ abstract class Strategy {
 	 * @param float $line2y
 	 * @return float
 	 */
-	final protected function perpendicularDistance($pointX, $pointY, $line1x, $line1y, $line2x, $line2y) {
+	final protected function perpendicularDistance($pointX, $pointY, $line1x, $line1y, $line2x, $line2y)
+	{
 		if ($line2x == $line1x) {
 			return abs($pointX - $line2x);
 		}

inc/core/Calculation/Elevation/Strategy/DouglasPeucker.php
@@ -1,19 +1,20 @@
 <?php
 /**
  * This file contains class::DouglasPeucker
- * @package Runalyze\Data\Elevation\Calculation
+ * @package Runalyze\Calculation\Elevation\Strategy
  */
 
-namespace Runalyze\Data\Elevation\Calculation;
+namespace Runalyze\Calculation\Elevation\Strategy;
 
 /**
  * Smoothing strategy: Douglas-Peucker
  *
  * @author Hannes Christiansen
- * @package Runalyze\Data\Elevation\Calculation
+ * @package Runalyze\Calculation\Elevation\Strategy
  * @see http://en.wikipedia.org/wiki/Ramer%E2%80%93Douglas%E2%80%93Peucker_algorithm
  */
-class DouglasPeucker extends Strategy {
+class DouglasPeucker extends AbstractStrategy
+{
 	/**
 	 * Distance (in x direction) between two points
 	 * @var int
@@ -22,16 +23,17 @@ class DouglasPeucker extends Strategy {
 
 	/**
 	 * Epsilon
-	 * @var float
+	 * @var float|int
 	 */
 	protected $Epsilon = 0;
 
 	/**
 	 * Construct
 	 * @param array $elevation
-	 * @param float $epsilon [optional]
+	 * @param float|int $epsilon [optional]
 	 */
-	public function __construct(array $elevation, $epsilon = 0) {
+	public function __construct(array $elevation, $epsilon = 0)
+	{
 		parent::__construct($elevation);
 
 		$this->setEpsilon($epsilon);
@@ -41,14 +43,16 @@ class DouglasPeucker extends Strategy {
 	 * Set epsilon
 	 * @param float $epsilon
 	 */
-	public function setEpsilon($epsilon) {
+	public function setEpsilon($epsilon)
+	{
 		$this->Epsilon = $epsilon;
 	}
 
 	/**
 	 * Smooth data
 	 */
-	public function runSmoothing() {
+	public function runSmoothing()
+	{
 		$this->SmoothingIndices = array(0, count($this->ElevationData)-1);
 		$this->SmoothedData = $this->smoothPart($this->ElevationData);
 
@@ -61,7 +65,8 @@ class DouglasPeucker extends Strategy {
 	 * @param int $offset
 	 * @return array
 	 */
-	protected function smoothPart(array $data, $offset = 0) {
+	protected function smoothPart(array $data, $offset = 0)
+	{
 		$maxDist = 0;
 		$index = 0;
 		$num = count($data);

inc/core/Calculation/Elevation/Strategy/NoSmoothing.php
@@ -1,22 +1,24 @@
 <?php
 /**
  * This file contains class::NoSmoothing
- * @package Runalyze\Data\Elevation\Calculation
+ * @package Runalyze\Calculation\Elevation\Strategy
  */
 
-namespace Runalyze\Data\Elevation\Calculation;
+namespace Runalyze\Calculation\Elevation\Strategy;
 
 /**
  * Smoothing strategy: no smoothing
  *
  * @author Hannes Christiansen
- * @package Runalyze\Data\Elevation\Calculation
+ * @package Runalyze\Calculation\Elevation\Strategy
  */
-class NoSmoothing extends Strategy {
+class NoSmoothing extends AbstractStrategy
+{
 	/**
 	 * Smooth data
 	 */
-	public function runSmoothing() {
+	public function runSmoothing()
+	{
 		$this->SmoothedData = $this->ElevationData;
 		$this->SmoothingIndices = array_keys($this->SmoothedData);
 	}

inc/core/Calculation/Elevation/Strategy/ReumannWitkam.php
@@ -1,23 +1,25 @@
 <?php
 /**
  * This file contains class::ReumannWitkamm
- * @package Runalyze\Data\Elevation\Calculation
+ * @package Runalyze\Calculation\Elevation\Strategy
  */
 
-namespace Runalyze\Data\Elevation\Calculation;
+namespace Runalyze\Calculation\Elevation\Strategy;
 
 /**
  * Smoothing strategy: Reumann-Witkamm
  *
  * @author Hannes Christiansen
- * @package Runalyze\Data\Elevation\Calculation
+ * @package Runalyze\Calculation\Elevation\Strategy
  * @see http://psimpl.sourceforge.net/reumann-witkam.html
  */
-class ReumannWitkam extends Strategy {
+class ReumannWitkam extends AbstractStrategy
+{
 	/**
 	 * Smooth data
 	 */
-	public function runSmoothing() {
+	public function runSmoothing()
+	{
 		throw new \RuntimeException('Not implemented.');
 	}
 }

inc/core/Calculation/Elevation/Strategy/Threshold.php
@@ -1,30 +1,32 @@
 <?php
 /**
  * This file contains class::Threshold
- * @package Runalyze\Data\Elevation\Calculation
+ * @package Runalyze\Calculation\Elevation\Strategy
  */
 
-namespace Runalyze\Data\Elevation\Calculation;
+namespace Runalyze\Calculation\Elevation\Strategy;
 
 /**
  * Smoothing strategy: Threshold
  *
  * @author Hannes Christiansen
- * @package Runalyze\Data\Elevation\Calculation
+ * @package Runalyze\Calculation\Elevation\Strategy
  */
-class Threshold extends Strategy {
+class Threshold extends AbstractStrategy
+{
 	/**
 	 * Epsilon
-	 * @var float
+	 * @var int
 	 */
 	protected $Epsilon = 0;
 
 	/**
 	 * Construct
 	 * @param array $elevation
-	 * @param float $epsilon [optional]
+	 * @param int $epsilon [optional]
 	 */
-	public function __construct(array $elevation, $epsilon = 0) {
+	public function __construct(array $elevation, $epsilon = 0)
+	{
 		parent::__construct($elevation);
 
 		$this->setEpsilon($epsilon);
@@ -34,13 +36,16 @@ class Threshold extends Strategy {
 	 * Set epsilon
 	 * @param float $epsilon
 	 */
-	public function setEpsilon($epsilon) {
+	public function setEpsilon($epsilon)
+	{
 		$this->Epsilon = $epsilon;
 	}
+
 	/**
 	 * Smooth data
 	 */
-	public function runSmoothing() {
+	public function runSmoothing()
+	{
 		$i = 0;
 		$max = count($this->ElevationData);
 		$this->SmoothedData = array($this->ElevationData[0]);

commit a0dca0d9cc8b32c24cf70c19d999312747d0dc77

fix elevation correction
Fixes two bugs introduced with 7044af4.

inc/core/Service/ElevationCorrection/Corrector.php
@@ -60,7 +60,7 @@ class Corrector
 	 */
 	final protected function hasNoValidStrategy()
 	{
-		return !($this->Strategy instanceof Strategy);
+		return !($this->Strategy instanceof Strategy\AbstractStrategy);
 	}
 
 	/**
@@ -68,7 +68,7 @@ class Corrector
 	 */
 	protected function tryToUse($strategyName)
 	{
-		$strategyName = 'Runalyze\\Strategy\\ElevationCorrection\\Strategy\\'.$strategyName;
+		$strategyName = 'Runalyze\\Service\\ElevationCorrection\\Strategy\\'.$strategyName;
 
 		if (class_exists($strategyName)) {
 			$this->Strategy = new $strategyName($this->LatitudePoints, $this->LongitudePoints);