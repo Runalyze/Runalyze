7754c87150111ada4547ae7c34ebf74ed460fbc3
laufhannes <mail@laufhannes.de>
2016-05-22 15:03:04 +0200

    fix search and send to multi editor
    Fixes #1838.

--- inc/training/search/class.SearchResults.php
+++ inc/training/search/class.SearchResults.php
@@ -438,7 +438,7 @@ class SearchResults {
 	 * Display
 	 */
 	public function display() {
-		if ($this->multiEditorRequested()) {
+		if ($this->multiEditorRequested() && !empty($this->Trainings)) {
 			$this->sendResultsToMultiEditor();
 		} else {
 			echo '<div id="'.DATA_BROWSER_SEARCHRESULT_ID.'">';
