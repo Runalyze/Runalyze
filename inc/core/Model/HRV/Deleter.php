2ecd53bb8c3ed01f336e17f265680292c3fc0b28
laufhannes <mail@laufhannes.de>
2016-08-20 13:30:20 +0200

    add option to remove hrv data
    Fixes #1908.

---
+++ inc/core/Model/HRV/Deleter.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * This file contains class::Deleter
+ * @package Runalyze\Model\HRV
+ */
+
+namespace Runalyze\Model\HRV;
+
+use Cache;
+use Runalyze\Model\DeleterWithAccountID;
+
+/**
+ * Delete object in database
+ *
+ * @author Hannes Christiansen
+ * @package Runalyze\Model\HRV
+ */
+class Deleter extends DeleterWithAccountID
+{
+	/**
+	 * Construct deleter
+	 * @param \PDO $connection
+	 * @param \Runalyze\Model\HRV\Entity $object [optional]
+	 */
+	public function __construct(\PDO $connection, Entity $object = null) {
+		parent::__construct($connection, $object);
+	}
+
+	/**
+	 * Tablename without prefix
+	 * @return string
+	 */
+	protected function table() {
+		return 'hrv';
+	}
+
+    /**
+     * Where clause
+     * @return string
+     */
+    protected function where() {
+        return '`activityid`='.$this->Object->get(Entity::ACTIVITYID).' AND '.parent::where();
+    }
+
+    /**
+     * Tasks before delete
+     */
+    protected function before() {
+        parent::before();
+
+        if (!$this->Object->get(Entity::ACTIVITYID)) {
+            throw new \RuntimeException('Provided object does not have any activityid.');
+        }
+
+        if (Cache::is($this->table().$this->Object->get(Entity::ACTIVITYID))) {
+            Cache::delete($this->table().$this->Object->get(Entity::ACTIVITYID));
+        }
+    }
+}
