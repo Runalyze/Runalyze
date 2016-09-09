2707d577de1b47d45b75602e6a1f33d5838632d9
Hannes <mail@laufhannes.de>
2016-09-09 11:13:29 +0200

    revert add language path for start page

--- src/CoreBundle/Controller/DefaultController.php
+++ src/CoreBundle/Controller/DefaultController.php
@@ -36,7 +36,6 @@ class DefaultController extends Controller
     /**
      * @Route("/dashboard", name="dashboard")
      * @Route("/", name="base_url")
-     * @Route("/{_locale}")
      * @Security("has_role('ROLE_USER')")
      */
     public function indexAction()
