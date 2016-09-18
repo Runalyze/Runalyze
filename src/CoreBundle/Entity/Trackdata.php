1da2fb9df43f0cc0ebb77161616d071afa2573f8
Michael Pohl <michael@runalyze.com>
2016-09-18 22:04:49 +0200

    add full trackdata entity

--- src/CoreBundle/Entity/Trackdata.php
+++ src/CoreBundle/Entity/Trackdata.php
@@ -1,6 +1,6 @@
 <?php
-namespace Runalyze\Bundle\CoreBundle\Entity;
 
+namespace Runalyze\Bundle\CoreBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 
@@ -8,7 +8,7 @@ use Doctrine\ORM\Mapping as ORM;
  * Trackdata
  *
  * @ORM\Table(name="trackdata", indexes={@ORM\Index(name="accountid", columns={"accountid"})})
- * @ORM\Entity
+ * @ORM\Entity(repositoryClass="Runalyze\Bundle\CoreBundle\Entity\TrackdataRepository")
  */
 class Trackdata
 {
@@ -78,32 +78,441 @@ class Trackdata
     /**
      * @var string
      *
+     * @ORM\Column(name="smo2_0", type="text", nullable=true)
+     */
+    private $smo20;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="smo2_1", type="text", nullable=true)
+     */
+    private $smo21;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="thb_0", type="text", nullable=true)
+     */
+    private $thb0;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="thb_1", type="text", nullable=true)
+     */
+    private $thb1;
+
+    /**
+     * @var string
+     *
      * @ORM\Column(name="pauses", type="text", length=65535, nullable=true)
      */
     private $pauses;
 
     /**
-     * @var \Account
+     * @var \Runalyze\Bundle\CoreBundle\Entity\Training
      *
-     * @ORM\ManyToOne(targetEntity="Account")
+     * @ORM\OneToOne(targetEntity="Runalyze\Bundle\CoreBundle\Entity\Training")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="activityid", referencedColumnName="id", unique=true)
+     * })
+     */
+    private $activityid;
+
+    /**
+     * @var \Runalyze\Bundle\CoreBundle\Entity\Account
+     *
+     * @ORM\ManyToOne(targetEntity="Runalyze\Bundle\CoreBundle\Entity\Account")
      * @ORM\JoinColumns({
      *   @ORM\JoinColumn(name="accountid", referencedColumnName="id")
      * })
      */
     private $accountid;
 
+
+
     /**
-     * @var \Training
+     * Set time
      *
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="NONE")
-     * @ORM\OneToOne(targetEntity="Training")
-     * @ORM\JoinColumns({
-     *   @ORM\JoinColumn(name="activityid", referencedColumnName="id")
-     * })
+     * @param string $time
+     *
+     * @return Trackdata
      */
-    private $activityid;
+    public function setTime($time)
+    {
+        $this->time = $time;
 
+        return $this;
+    }
 
+    /**
+     * Get time
+     *
+     * @return string
+     */
+    public function getTime()
+    {
+        return $this->time;
+    }
+
+    /**
+     * Set distance
+     *
+     * @param string $distance
+     *
+     * @return Trackdata
+     */
+    public function setDistance($distance)
+    {
+        $this->distance = $distance;
+
+        return $this;
+    }
+
+    /**
+     * Get distance
+     *
+     * @return string
+     */
+    public function getDistance()
+    {
+        return $this->distance;
+    }
+
+    /**
+     * Set heartrate
+     *
+     * @param string $heartrate
+     *
+     * @return Trackdata
+     */
+    public function setHeartrate($heartrate)
+    {
+        $this->heartrate = $heartrate;
+
+        return $this;
+    }
+
+    /**
+     * Get heartrate
+     *
+     * @return string
+     */
+    public function getHeartrate()
+    {
+        return $this->heartrate;
+    }
+
+    /**
+     * Set cadence
+     *
+     * @param string $cadence
+     *
+     * @return Trackdata
+     */
+    public function setCadence($cadence)
+    {
+        $this->cadence = $cadence;
+
+        return $this;
+    }
+
+    /**
+     * Get cadence
+     *
+     * @return string
+     */
+    public function getCadence()
+    {
+        return $this->cadence;
+    }
+
+    /**
+     * Set power
+     *
+     * @param string $power
+     *
+     * @return Trackdata
+     */
+    public function setPower($power)
+    {
+        $this->power = $power;
+
+        return $this;
+    }
+
+    /**
+     * Get power
+     *
+     * @return string
+     */
+    public function getPower()
+    {
+        return $this->power;
+    }
+
+    /**
+     * Set temperature
+     *
+     * @param string $temperature
+     *
+     * @return Trackdata
+     */
+    public function setTemperature($temperature)
+    {
+        $this->temperature = $temperature;
+
+        return $this;
+    }
+
+    /**
+     * Get temperature
+     *
+     * @return string
+     */
+    public function getTemperature()
+    {
+        return $this->temperature;
+    }
+
+    /**
+     * Set groundcontact
+     *
+     * @param string $groundcontact
+     *
+     * @return Trackdata
+     */
+    public function setGroundcontact($groundcontact)
+    {
+        $this->groundcontact = $groundcontact;
+
+        return $this;
+    }
+
+    /**
+     * Get groundcontact
+     *
+     * @return string
+     */
+    public function getGroundcontact()
+    {
+        return $this->groundcontact;
+    }
+
+    /**
+     * Set verticalOscillation
+     *
+     * @param string $verticalOscillation
+     *
+     * @return Trackdata
+     */
+    public function setVerticalOscillation($verticalOscillation)
+    {
+        $this->verticalOscillation = $verticalOscillation;
+
+        return $this;
+    }
+
+    /**
+     * Get verticalOscillation
+     *
+     * @return string
+     */
+    public function getVerticalOscillation()
+    {
+        return $this->verticalOscillation;
+    }
+
+    /**
+     * Set groundcontactBalance
+     *
+     * @param string $groundcontactBalance
+     *
+     * @return Trackdata
+     */
+    public function setGroundcontactBalance($groundcontactBalance)
+    {
+        $this->groundcontactBalance = $groundcontactBalance;
+
+        return $this;
+    }
+
+    /**
+     * Get groundcontactBalance
+     *
+     * @return string
+     */
+    public function getGroundcontactBalance()
+    {
+        return $this->groundcontactBalance;
+    }
+
+    /**
+     * Set smo20
+     *
+     * @param string $smo20
+     *
+     * @return Trackdata
+     */
+    public function setSmo20($smo20)
+    {
+        $this->smo20 = $smo20;
+
+        return $this;
+    }
+
+    /**
+     * Get smo20
+     *
+     * @return string
+     */
+    public function getSmo20()
+    {
+        return $this->smo20;
+    }
+
+    /**
+     * Set smo21
+     *
+     * @param string $smo21
+     *
+     * @return Trackdata
+     */
+    public function setSmo21($smo21)
+    {
+        $this->smo21 = $smo21;
+
+        return $this;
+    }
+
+    /**
+     * Get smo21
+     *
+     * @return string
+     */
+    public function getSmo21()
+    {
+        return $this->smo21;
+    }
+
+    /**
+     * Set thb0
+     *
+     * @param string $thb0
+     *
+     * @return Trackdata
+     */
+    public function setThb0($thb0)
+    {
+        $this->thb0 = $thb0;
+
+        return $this;
+    }
+
+    /**
+     * Get thb0
+     *
+     * @return string
+     */
+    public function getThb0()
+    {
+        return $this->thb0;
+    }
+
+    /**
+     * Set thb1
+     *
+     * @param string $thb1
+     *
+     * @return Trackdata
+     */
+    public function setThb1($thb1)
+    {
+        $this->thb1 = $thb1;
+
+        return $this;
+    }
+
+    /**
+     * Get thb1
+     *
+     * @return string
+     */
+    public function getThb1()
+    {
+        return $this->thb1;
+    }
+
+    /**
+     * Set pauses
+     *
+     * @param string $pauses
+     *
+     * @return Trackdata
+     */
+    public function setPauses($pauses)
+    {
+        $this->pauses = $pauses;
+
+        return $this;
+    }
+
+    /**
+     * Get pauses
+     *
+     * @return string
+     */
+    public function getPauses()
+    {
+        return $this->pauses;
+    }
+
+    /**
+     * Set activityid
+     *
+     * @param \Runalyze\Bundle\CoreBundle\Entity\Training $activityid
+     *
+     * @return Trackdata
+     */
+    public function setActivityid(\Runalyze\Bundle\CoreBundle\Entity\Training $activityid = null)
+    {
+        $this->activityid = $activityid;
+
+        return $this;
+    }
+
+    /**
+     * Get activityid
+     *
+     * @return \Runalyze\Bundle\CoreBundle\Entity\Training
+     */
+    public function getActivityid()
+    {
+        return $this->activityid;
+    }
+
+    /**
+     * Set accountid
+     *
+     * @param \Runalyze\Bundle\CoreBundle\Entity\Account $accountid
+     *
+     * @return Trackdata
+     */
+    public function setAccountid(\Runalyze\Bundle\CoreBundle\Entity\Account $accountid = null)
+    {
+        $this->accountid = $accountid;
+
+        return $this;
+    }
+
+    /**
+     * Get accountid
+     *
+     * @return \Runalyze\Bundle\CoreBundle\Entity\Account
+     */
+    public function getAccountid()
+    {
+        return $this->accountid;
+    }
 }
-
---
+++ src/CoreBundle/Entity/TrackdataRepository.php
@@ -0,0 +1,8 @@
+<?php
+namespace Runalyze\Bundle\CoreBundle\Entity;
+
+use Doctrine\ORM\EntityRepository;
+
+class TrackdataRepository extends EntityRepository
+{
+}
