abdf56ba69e42c2b3361ef054fa0922c2a65e1f0
Matthias Glaub <magl@magl.net>
2016-10-08 10:16:31 +0200

    prepare the database

--- data/vagrant/bootstrap.sh
+++ data/vagrant/bootstrap.sh
@@ -30,7 +30,6 @@ debconf-set-selections <<< 'mysql-server mysql-server/root_password password def
 debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password default'
 apt-get install -y mysql-server
 mysql -uroot -pdefault -e "SET PASSWORD FOR root@localhost=PASSWORD('');"
-mysql -uroot -e "CREATE DATABASE IF NOT EXISTS runalyze;"
 cp /vagrant/data/vagrant/mysql-runalyze.cnf /etc/mysql/mysql.conf.d/
 service mysql restart
 
@@ -42,4 +41,23 @@ service mysql restart
 if [ ! -f /vagrant/data/config.yml ]; then
     echo "copying default config file to data folder"
     cp /vagrant/app/config/default_config.yml /vagrant/data/config.yml
+fi
+
+# check if we can access the database
+mysql -uroot -e "SELECT @@VERSION;" > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+    # we have access, so we can check if we need to create a database
+    mysql -uroot -e "USE runalyze;" > /dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        mysql -uroot -e "CREATE DATABASE runalyze;" > /dev/null 2>&1
+        if [ $? -eq 0 ]; then
+            echo "Runalyze database created successfully"
+        else
+            echo "Error Creating the Database"
+        fi
+    else
+        echo "Runalyze database already exists."
+    fi
+else
+    echo "Could not access the database server, no database will be created"
 fi
