ea4ab557af7c642fdc51d375e84e9ad62f9433c9
Michael Pohl <michael@runalyze.com>
2016-10-17 22:34:00 +0200

    remove session table from db if exists

---
+++ app/DoctrineMigrations/Version20161017223115.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Runalyze\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+
+/**
+ * delete runalyze_session (if exists)
+ */
+class Version20161017223115 extends AbstractMigration implements ContainerAwareInterface
+{
+    /** @var ContainerInterface|null */
+    private $container;
+
+    /**
+     * @param ContainerInterface|null $container
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $prefix = $this->container->getParameter('database_prefix');
+        $this->addSql('DROP TABLE IF EXISTS `'.$prefix.'sessions`');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+
+    }
+}
