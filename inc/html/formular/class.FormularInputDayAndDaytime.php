Runalyze/Runalyze

commit 366d6b8c0ccfc4e657fd9db9c7e39c7f3d8d65a6

fix training form for bad date input
Fixes #1934.

inc/html/formular/class.FormularInputDayAndDaytime.php
@@ -57,21 +57,30 @@ class FormularInputDayAndDaytime extends FormularField {
 
 	/**
 	 * Validate value
-	 * @return boolean
+	 * @return bool
 	 */
 	public function validate() {
 		$this->setFields();
 
 		if (!isset($_POST[$this->getFieldDayName()]) || !isset($_POST[$this->getFieldDaytimeName()]))
-			return;
+			return false;
+
+		$dateString = $_POST[$this->getFieldDayName()];
+		$timeString = $_POST[$this->getFieldDaytimeName()];
+
+		$validateDay = $this->FieldDay->validate();
+		$validateDaytime = $this->FieldDaytime->validate();
 
 		$date = $_POST[$this->getFieldDayName()];
 		$time = $_POST[$this->getFieldDaytimeName()];
 
-		$this->FieldDay->validate();
-		$this->FieldDaytime->validate();
+		if (is_numeric($date) && is_numeric($time)) {
+			$_POST[$this->name] = $date + $time;
+		} else {
+			$_POST[$this->name] = strtotime($dateString.' '.$timeString);
+		}
 
-		$_POST[$this->name] = strtotime($date.' '.$time);
+		return $validateDay && $validateDaytime && false !== $_POST[$this->name];
 	}
 
 	/**

inc/html/formular/class.FormularField.php
@@ -11,7 +11,7 @@
 abstract class FormularField extends HtmlTag {
 	/**
 	 * CSS-class if validation failed
-	 * @var string 
+	 * @var string
 	 */
 	public static $CSS_VALIDATION_FAILED = 'validation-failed';
 
@@ -29,7 +29,7 @@ abstract class FormularField extends HtmlTag {
 
 	/**
 	 * Name
-	 * @var string 
+	 * @var string
 	 */
 	protected $name = '';
 
@@ -41,13 +41,13 @@ abstract class FormularField extends HtmlTag {
 
 	/**
 	 * Label
-	 * @var string 
+	 * @var string
 	 */
 	protected $label = '';
 
 	/**
 	 * Layout
-	 * @var string 
+	 * @var string
 	 */
 	protected $layout = '';
 
@@ -65,7 +65,7 @@ abstract class FormularField extends HtmlTag {
 
 	/**
 	 * Array with options for parser
-	 * @var array 
+	 * @var array
 	 */
 	protected $parserOptions = array();
 
@@ -77,7 +77,7 @@ abstract class FormularField extends HtmlTag {
 
 	/**
 	 * Set key as failed
-	 * @param string $key 
+	 * @param string $key
 	 */
 	public static function setKeyAsFailed($key) {
 		self::$FAILED_KEYS[] = $key;
@@ -136,7 +136,7 @@ abstract class FormularField extends HtmlTag {
 
 	/**
 	 * Set default value if empty
-	 * @param string $value 
+	 * @param string $value
 	 */
 	public function defaultValue($value) {
 		if (empty($this->value))
@@ -145,7 +145,7 @@ abstract class FormularField extends HtmlTag {
 
 	/**
 	 * Set layout, used as css-class for a surrounding div
-	 * @param string $layout 
+	 * @param string $layout
 	 */
 	public function setLayout($layout) {
 		$this->layout = $layout;
@@ -153,7 +153,7 @@ abstract class FormularField extends HtmlTag {
 
 	/**
 	 * Set layout if no layout is set
-	 * @param string $layout 
+	 * @param string $layout
 	 */
 	public function setLayoutIfEmpty($layout) {
 		if (empty($this->layout)) {
@@ -163,7 +163,7 @@ abstract class FormularField extends HtmlTag {
 
 	/**
 	 * Add layout, used as additional css-class for a surrounding div
-	 * @param string $layout 
+	 * @param string $layout
 	 */
 	public function addLayout($layout) {
 		$this->layout .= ' '.$layout;
@@ -171,7 +171,7 @@ abstract class FormularField extends HtmlTag {
 
 	/**
 	 * Add CSS class for layout
-	 * @param string $layout 
+	 * @param string $layout
 	 */
 	public function addLayoutClass($layout) {
 		$this->layoutClasses[] = $layout;
@@ -180,7 +180,7 @@ abstract class FormularField extends HtmlTag {
 	/**
 	 * Set parser
 	 * @param string $parser
-	 * @param array $options 
+	 * @param array $options
 	 */
 	public function setParser($parser, $options = array()) {
 		$this->parser = $parser;
@@ -204,6 +204,7 @@ abstract class FormularField extends HtmlTag {
 
 	/**
 	 * Validate value
+	 * @return bool
 	 */
 	public function validate() {
 		$validation = FormularValueParser::validatePost($this->name, $this->parser, $this->parserOptions);
@@ -212,10 +213,12 @@ abstract class FormularField extends HtmlTag {
 			self::setKeyAsFailed($this->name);
 			self::addValidationFailure(is_string($validation) ? $validation : __('Your input is not allowed.').' ('.$this->name.')');
 		}
+
+		return $validation;
 	}
 
 	/**
-	 * Display this field 
+	 * Display this field
 	 */
 	public function display() {
 		echo $this->getCode();
@@ -223,7 +226,7 @@ abstract class FormularField extends HtmlTag {
 
 	/**
 	 * Get code for displaying this field with layout
-	 * @return string 
+	 * @return string
 	 */
 	final public function getCode() {
 		$this->prepare();
@@ -244,7 +247,7 @@ abstract class FormularField extends HtmlTag {
 	}
 
 	/**
-	 * Prepare field for being displayed 
+	 * Prepare field for being displayed
 	 */
 	private function prepare() {
 		if (!$this->prepared) {

inc/html/formular/class.FormularInputUTCDayAndDaytime.php
@@ -25,15 +25,19 @@ class FormularInputUTCDayAndDaytime extends FormularInputDayAndDaytime {
 			$this->value = (new LocalTime($this->value))->toServerTime()->getTimestamp();
 		}
 	}
-	
+
 	/**
 	 * Validate value
-	 * @return boolean
+	 * @return bool
 	 */
 	public function validate() {
-		parent::validate();
+		$parentValidation = parent::validate();
 
-		$_POST[$this->name] = LocalTime::fromServerTime($_POST[$this->name])->getTimestamp();
+		if (true === $parentValidation) {
+			$_POST[$this->name] = LocalTime::fromServerTime($_POST[$this->name])->getTimestamp();
+		}
+
+		return $parentValidation;
 	}
 
 }

inc/html/formular/class.FormularValueParser.php
@@ -345,9 +345,9 @@ class FormularValueParser {
 		$numParts  = count($dateParts);
 
 		if ($numParts == 3) {
-			$_POST[$key] = mktime(0, 0, 0, $dateParts[1], $dateParts[0], $dateParts[2]);
+			$_POST[$key] = mktime(0, 0, 0, (int)$dateParts[1], (int)$dateParts[0], (int)$dateParts[2]);
 		} elseif ($numParts == 2) {
-			$_POST[$key] = mktime(0, 0, 0, $dateParts[1], $dateParts[0], date('Y'));
+			$_POST[$key] = mktime(0, 0, 0, (int)$dateParts[1], (int)$dateParts[0], date('Y'));
 		} else {
 			//$_POST[$key] = time();
 			return __('The date could not be parsed.');

commit 96b8a791777d57c88282d993598e3603318ee652

fix bug on dst change dates only
Validating date and time separately causes date's timestamp to be in summertime.

Originally caused by 366d6b8c0ccfc4e657fd9db9c7e39c7f3d8d65a6, see #1934.

inc/html/formular/class.FormularInputDayAndDaytime.php
@@ -74,13 +74,17 @@ class FormularInputDayAndDaytime extends FormularField {
 		$date = $_POST[$this->getFieldDayName()];
 		$time = $_POST[$this->getFieldDaytimeName()];
 
-		if (is_numeric($date) && is_numeric($time)) {
-			$_POST[$this->name] = $date + $time;
-		} else {
+		if ($validateDay && $validateDaytime) {
 			$_POST[$this->name] = strtotime($dateString.' '.$timeString);
+
+			if (false === $_POST[$this->name]) { // i.e. FormularValueParser "ignored/fixed" some bad input
+				$_POST[$this->name] = $date + $time;
+			}
+
+			return true;
 		}
 
-		return $validateDay && $validateDaytime && false !== $_POST[$this->name];
+		return false;
 	}
 
 	/**