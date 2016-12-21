b0d643fea73ea247ea43af8b220d88736550816b
Felix Gertz <dev@felixgertz.de>
2016-12-21 10:35:32 +0100

    Enable sport type selection

--- plugin/RunalyzePluginPanel_TagsSummary/class.RunalyzePluginPanel_TagsSummary.php
+++ plugin/RunalyzePluginPanel_TagsSummary/class.RunalyzePluginPanel_TagsSummary.php
@@ -23,14 +23,14 @@ class RunalyzePluginPanel_TagsSummary extends PluginPanel {
 	/**
 	 * @var array
 	 */
-	protected $AllTypes = array();
+	protected $AllSports = array();
 
 	/**
 	 * Name
 	 * @return string
 	 */
 	final public function name() {
-		return __('TagsSummary');
+		return __('Tags summary');
 	}
 
 	/**
@@ -41,4 +41,84 @@ class RunalyzePluginPanel_TagsSummary extends PluginPanel {
 		return __('Summarises your tags for the selected sport.');
 	}
 
+	/**
+	 * Init configuration
+	 */
+	protected function initConfiguration() {
+		$this->AllSports = DB::getInstance()
+			->query('SELECT `id`, `name` FROM `'.PREFIX.'sport` WHERE `accountid`="'.SessionAccountHandler::getId().'" ORDER BY `id` ASC')
+			->fetchAll();
+		$Options = array();
+
+		foreach ($this->AllSports as $data) {
+			$Options[$data['id']] = $data['name'];
+		}
+
+		$Sports = new PluginConfigurationValueSelect('sport', __('Sport to summarise tags for'));
+		$Sports->setOptions($Options);
+
+		$Configuration = new PluginConfiguration($this->id());
+		$Configuration->addValue($Sports);
+
+		if (isset($_GET['sport']) && isset($Options[$_GET['sport']])) {
+			$Configuration->object('sport')->setValue($_GET['sport']);
+			$Configuration->update('sport');
+			Cache::delete(PluginConfiguration::CACHE_KEY);
+		}
+
+		$this->setConfiguration($Configuration);
+	}
+
+	/**
+	 * Method for getting the right symbol(s)
+	 */
+	protected function getRightSymbol() {
+		$CurrentSport = '';
+		$SportLinks = [];
+
+		foreach ($this->AllSports as $Sport) {
+			$active = $Sport['id'] == (int)$this->Configuration()->value('sport');
+			$SportLinks[] = '<li'.($active ? ' class="active"' : '').'>'.Ajax::link($Sport['name'], 'panel-'.$this->id(), Plugin::$DISPLAY_URL.'/'.$this->id().'?sport='.$Sport['id']).'</li>';
+
+			if ($active) {
+				$CurrentSport = $Sport['name'];
+			}
+		}
+
+		$Links = '<li class="with-submenu"><span class="link">'.$CurrentSport.'</span>';
+		$Links .= '<ul class="submenu">'.implode('', $SportLinks).'</ul>';
+		$Links .= '</li>';
+
+		return '<ul>'.$Links.'</ul>';
+	}
+
+	/**
+	 * Display the content
+	 * @see PluginPanel::displayContent()
+	 */
+	protected function displayContent() {
+		echo $this->getStyle();
+		echo '<div id="equipment">';
+
+		echo '</div>';
+
+		echo HTML::clearBreak();
+	}
+
+	/**
+	 * Get style
+	 * @return string
+	 */
+	protected function getStyle() {
+		return '<style type="text/css">.equipment-usage { position: absolute; bottom: 0; left: 0; background-image:url(assets/images//damage.png); background-position:left center; height: 2px; max-width: 100%; }</style>';
+	}
+
+	/**
+	 * Get shoe usage image
+	 * @param float $percentage [0.0 .. 1.0]
+	 * @return string
+	 */
+	protected function getUsageImage($percentage) {
+		return '<span class="equipment-usage" style="width:'.round($percentage * 330).'px;"></span>';
+	}
 }
