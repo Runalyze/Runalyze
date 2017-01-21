Runalyze/Runalyze

commit 376f2ad3fc02022a57a5e11ec8990012a9260f09

minor refactoring - database cleanup in symfony FormType - using new Configuration Manager and use Account injection

src/CoreBundle/Controller/ActivityController.php
@@ -69,7 +69,7 @@ class ActivityController extends Controller
      * @Route("/call/call.Training.display.php")
      * @Route("/activity/{id}", name="ActivityShow", requirements={"id" = "\d+"})
      */
-    public function displayAction($id = null)
+    public function displayAction($id = null, Account $account)
     {
         $Frontend = new \Frontend(true, $this->get('security.token_storage'));
 
@@ -77,20 +77,20 @@ class ActivityController extends Controller
             $id = Request::createFromGlobals()->query->get('id');
         }
 
-        $Context = new Context($id, \SessionAccountHandler::getId());
+        $Context = new Context($id, $account->getId());
 
         switch (Request::createFromGlobals()->query->get('action')) {
             case 'changePrivacy':
                 $oldActivity = clone $Context->activity();
                 $Context->activity()->set(Activity\Entity::IS_PUBLIC, !$Context->activity()->isPublic());
                 $Updater = new Activity\Updater(\DB::getInstance(), $Context->activity(), $oldActivity);
-                $Updater->setAccountID(\SessionAccountHandler::getId());
+                $Updater->setAccountID($account->getId());
                 $Updater->update();
                 break;
             case 'delete':
                 $Factory = \Runalyze\Context::Factory();
                 $Deleter = new Activity\Deleter(\DB::getInstance(), $Context->activity());
-                $Deleter->setAccountID(\SessionAccountHandler::getId());
+                $Deleter->setAccountID($account->getId());
                 $Deleter->setEquipmentIDs($Factory->equipmentForActivity($id, true));
                 $Deleter->delete();
 
@@ -180,13 +180,13 @@ class ActivityController extends Controller
     * @Route("/activity/{id}/delete", name="ActivityDelete")
     * @Security("has_role('ROLE_USER')")
     */
-   public function deleteAction($id)
+   public function deleteAction($id, Account $account)
    {
         $Frontend = new \Frontend(true, $this->get('security.token_storage'));
 
         $Factory = \Runalyze\Context::Factory();
         $Deleter = new Activity\Deleter(\DB::getInstance(), $Factory->activity($id));
-        $Deleter->setAccountID(\SessionAccountHandler::getId());
+        $Deleter->setAccountID($account->getId());
         $Deleter->setEquipmentIDs($Factory->equipmentForActivity($id, true));
         $Deleter->delete();
 
@@ -199,13 +199,13 @@ class ActivityController extends Controller
      * @Route("/activity/{id}/vdot-info")
      * @Security("has_role('ROLE_USER')")
      */
-    public function vdotInfoAction($id)
+    public function vdotInfoAction($id, Account $account)
     {
         $Frontend = new \Frontend(true, $this->get('security.token_storage'));
 
         $VdotInfo = new VdotInfo();
-        $VdotInfo->setContext(new Context($id, \SessionAccountHandler::getId()));
-        $VdotInfo->setConfiguration(Configuration::Data(), Configuration::Vdot());
+        $VdotInfo->setContext(new Context($id, $account->getId()));
+        $VdotInfo->setConfiguration($this->get('app.configuration_manager')->getList()->getCurrentVdot());
 
         return $this->render(':activity:vdot_info.html.twig', [
             'title' => $VdotInfo->getTitle(),
@@ -222,7 +222,7 @@ class ActivityController extends Controller
      * @Route("/activity/{id}/elevation-correction")
      * @Security("has_role('ROLE_USER')")
      */
-    public function elevationCorrectionAction($id)
+    public function elevationCorrectionAction($id, Account $account)
     {
         $Frontend = new \Frontend(false, $this->get('security.token_storage'));
 
@@ -244,11 +244,11 @@ class ActivityController extends Controller
         	$Activity->set(Activity\Entity::ELEVATION, $Route->elevation());
 
         	$UpdaterRoute = new \Runalyze\Model\Route\Updater(\DB::getInstance(), $Route, $RouteOld);
-        	$UpdaterRoute->setAccountID(\SessionAccountHandler::getId());
+        	$UpdaterRoute->setAccountID($account->getId());
         	$UpdaterRoute->update();
 
         	$UpdaterActivity = new Activity\Updater(\DB::getInstance(), $Activity, $ActivityOld);
-        	$UpdaterActivity->setAccountID(\SessionAccountHandler::getId());
+        	$UpdaterActivity->setAccountID($account->getId());
         	$UpdaterActivity->update();
 
         	if (Request::createFromGlobals()->query->get('strategy') == 'none') {
@@ -277,10 +277,10 @@ class ActivityController extends Controller
      * @Route("/activity/{id}/splits-info", requirements={"id" = "\d+"})
      * @Security("has_role('ROLE_USER')")
      */
-    public function splitsInfoAction($id)
+    public function splitsInfoAction($id, Account $account)
     {
         $Frontend = new \Frontend(false, $this->get('security.token_storage'));
-        $Window = new Window(new Context($id, \SessionAccountHandler::getId()));
+        $Window = new Window(new Context($id, $account->getId()));
         $Window->display();
 
         return new Response();
@@ -291,10 +291,10 @@ class ActivityController extends Controller
      * @Route("/activity/{id}/elevation-info", requirements={"id" = "\d+"})
      * @Security("has_role('ROLE_USER')")
      */
-    public function elevationInfoAction($id)
+    public function elevationInfoAction($id, Account $account)
     {
         $Frontend = new \Frontend(false, $this->get('security.token_storage'));
-        $ElevationInfo = new \ElevationInfo(new Context($id, \SessionAccountHandler::getId()));
+        $ElevationInfo = new \ElevationInfo(new Context($id, $account->getId()));
         $ElevationInfo->display();
 
         return new Response();
@@ -361,18 +361,18 @@ class ActivityController extends Controller
      * @Route("/activity/{id}/export/{type}/{typeid}", requirements={"id" = "\d+"})
      * @Security("has_role('ROLE_USER')")
      */
-    public function exporterExportAction($id, $type, $typeid) {
+    public function exporterExportAction($id, $type, $typeid, Account $account) {
         $Frontend = new \Frontend(true, $this->get('security.token_storage'));
 
         if ($type == 'social' && Share\Types::isValidValue((int)$typeid)) {
-            $Context = new Context((int)$id, \SessionAccountHandler::getId());
+            $Context = new Context((int)$id, $account->getId());
             $Exporter = Share\Types::get((int)$typeid, $Context);
 
             if ($Exporter instanceof Share\AbstractSnippetSharer) {
                 $Exporter->display();
             }
         } elseif ($type == 'file' && File\Types::isValidValue((int)$typeid)) {
-            $Context = new Context((int)$id, \SessionAccountHandler::getId());
+            $Context = new Context((int)$id, $account->getId());
             $Exporter = File\Types::get((int)$typeid, $Context);
 
             if ($Exporter instanceof File\AbstractFileExporter) {
@@ -389,7 +389,7 @@ class ActivityController extends Controller
      * @Route("/activity/matcher", name="activityMatcher")
      * @Security("has_role('ROLE_USER')")
      */
-    public function ajaxActivityMatcher()
+    public function ajaxActivityMatcher(Account $account)
     {
         $Frontend = new \Frontend(true, $this->get('security.token_storage'));
 
@@ -402,7 +402,7 @@ class ActivityController extends Controller
         }
 
         $IgnoreIDs = \Runalyze\Configuration::ActivityForm()->ignoredActivityIDs();
-        $DuplicateFinder = new DuplicateFinder(\DB::getInstance(), \SessionAccountHandler::getId());
+        $DuplicateFinder = new DuplicateFinder(\DB::getInstance(), $account->getId());
 
         $IgnoreIDs = array_map(function($v){
         	try {

app/Resources/views/tools/database_cleanup/form.html.twig
@@ -1,67 +1,58 @@
 {% extends 'tools/database_cleanup/base.html.twig' %}
 
 {% block toolContent %}
-    <form action="{{ url('tools-cleanup') }}" method="post" id="cleanup" class="ajax no-automatic-reload">
+    {{ form_start(form, {'attr': {'novalidate': 'novalidate', 'class': 'ajax no-automatic-reloads'}, 'action': url('tools-cleanup')}) }}
 
         <div class="padding-5">
             <label>
-                <input type="radio" name="mode" value="general" checked>
-                <strong>{% trans %}General cleanup{% endtrans %}</strong>
+                {{ form_widget(form.mode[0]) }}
+                {{ form_label(form.mode[0]) }}
             </label>
 
             <div class="radio-content" data-radio="general">
                 <div class="margin-10 padding-5">
                     <div class="checkbox">
-                        <label>
-                            <input type="checkbox" name="internals">
-                            {% trans %}Refresh internal constants{% endtrans %}
-                        </label>
+                        {{ form_widget(form.internals) }}
+                        {{ form_label(form.internals, 'Refresh internal constants') }}
                     </div>
+
                     <div class="checkbox">
-                        <label>
-                            <input type="checkbox" name="equipment">
-                            {% trans %}Recalculate your equipment statistics{% endtrans %}
-                        </label>
+                        {{ form_widget(form.equipment) }}
+                        {{ form_label(form.equipment, 'Recalculate your equipment statistics') }}
                     </div>
+
                     <div class="checkbox">
-                        <label>
-                            <input type="checkbox" name="vdot-corrector">
-                            {% trans %}Recalculate your VDOT correction factor{% endtrans %}
-                        </label>
+                        {{ form_widget(form.vdotCorrector) }}
+                        {{ form_label(form.vdotCorrector, 'Recalculate your VDOT correction factor') }}
                     </div>
+
                     <div class="checkbox">
-                        <label>
-                            <input type="checkbox" name="vdot">
-                            {% trans %}Recalculate your VDOT shape{% endtrans %}
-                        </label>
+                        {{ form_widget(form.vdot) }}
+                        {{ form_label(form.vdot, 'Recalculate your VDOT shape') }}
                     </div>
+
                     <div class="checkbox">
-                        <label>
-                            <input type="checkbox" name="endurance">
-                            {% trans %}Recalculate your basic endurance{% endtrans %}
-                        </label>
+                        {{ form_widget(form.endurance) }}
+                        {{ form_label(form.endurance, 'Recalculate your basic endurance') }}
                     </div>
+
                     <div class="checkbox">
-                        <label>
-                            <input type="checkbox" name="trimp">
-                            {% trans %}Recalculate maximal TRIMP values{% endtrans %}
-                        </label>
+                        {{ form_widget(form.trimp) }}
+                        {{ form_label(form.trimp, 'Recalculate maximal TRIMP values') }}
                     </div>
+
                     <div class="checkbox">
-                        <label>
-                            <input type="checkbox" name="cacheclean">
-                            {% trans %}Clear your personal cache{% endtrans %}
-                        </label>
+                        {{ form_widget(form.cacheclean) }}
+                        {{ form_label(form.cacheclean, 'Clear your personal cache') }}
                     </div>
+
                 </div>
             </div>
         </div>
 
         <div class="padding-5">
-            <label>
-                <input type="radio" name="mode" value="loop">
-                <strong>{% trans %}Loop through activities{% endtrans %}</strong>
-            </label>
+                {{ form_widget(form.mode[1]) }}
+                {{ form_label(form.mode[1]) }}
 
             <div class="radio-content transparent-70" data-radio="loop">
                 <div class="margin-10">
@@ -73,43 +64,34 @@
 
                 <div class="margin-10 padding-5">
                     <div class="checkbox">
-                        <label>
-                            <input type="checkbox" name="activity-elevation" disabled>
-                            {% trans %}Recalculate elevation{% endtrans %}
-                        </label>
+                        {{ form_widget(form.activityElevation) }}
+                        {{ form_label(form.activityElevation, 'Recalculate elevation') }}
                         <div class="checkbox">
-                            <label>
-                                <input type="checkbox" name="activity-elevation-overwrite" disabled>
-                                {% trans %}Overwrite manual values{% endtrans %}
-                            </label>
+                            {{ form_widget(form.activityElevationOverwrite) }}
+                            {{ form_label(form.activityElevationOverwrite, 'Overwrite manual values') }}
                         </div>
                     </div>
                     <div class="checkbox">
-                        <label>
-                            <input type="checkbox" name="activity-vdot" disabled>
-                            {% trans %}Recalculate VDOT values{% endtrans %}
-                        </label>
+                            {{ form_widget(form.activityVdot) }}
+                            {{ form_label(form.activityVdot, 'Recalculate VDOT values') }}
                     </div>
                     <div class="checkbox">
-                        <label>
-                            <input type="checkbox" name="activity-jdpoints" disabled>
-                            {% trans %}Recalculate JD training points{% endtrans %}
-                        </label>
+                            {{ form_widget(form.activityJdpoints) }}
+                            {{ form_label(form.activityJdpoints, 'Recalculate JD training points') }}
                     </div>
                     <div class="checkbox">
-                        <label>
-                            <input type="checkbox" name="activity-trimp" disabled>
-                            {% trans %}Recalculate TRIMP values{% endtrans %}
-                        </label>
+                            {{ form_widget(form.activityTrimp) }}
+                            {{ form_label(form.activityTrimp, 'Recalculate TRIMP values') }}
                     </div>
                 </div>
             </div>
         </div>
 
-        <input class="transparent-70" type="submit" name="submit" value="{% trans %}Start cleanup{% endtrans %}" disabled>
-
-    </form>
-
+    <div class="c">
+        <input type="submit" class="submit" value="{% trans %}Start cleanup{% endtrans %}">
+    </div>
+    {{ form_rest(form) }}
+    {{ form_end(form) }}
     <script>
         $(function(){
             var $cleanup = $("#cleanup");

src/CoreBundle/Component/Tool/DatabaseCleanup/JobGeneral.php
@@ -30,7 +30,7 @@ class JobGeneral extends Job
 	 * Task key: vdot corrector
 	 * @var string
 	 */
-	const VDOT_CORRECTOR = 'vdot-corrector';
+	const VDOT_CORRECTOR = 'vdotCorrector';
 
 	/**
 	 * Task key: basic endurance

src/CoreBundle/Component/Tool/DatabaseCleanup/JobLoop.php
@@ -14,31 +14,31 @@ class JobLoop extends Job
 	 * Task key: elevation
 	 * @var string
 	 */
-	const ELEVATION = 'activity-elevation';
+	const ELEVATION = 'activityElevation';
 
 	/**
 	 * Task key: overwrite elevation
 	 * @var string
 	 */
-	const ELEVATION_OVERWRITE = 'activity-elevation-overwrite';
+	const ELEVATION_OVERWRITE = 'activityElevationOverwrite';
 
 	/**
 	 * Task key: vdot
 	 * @var string
 	 */
-	const VDOT = 'activity-vdot';
+	const VDOT = 'activityVdot';
 
 	/**
 	 * Task key: jd points
 	 * @var string
 	 */
-	const JD_POINTS = 'activity-jdpoints';
+	const JD_POINTS = 'activityJdpoints';
 
 	/**
 	 * Task key: trimp
 	 * @var string
 	 */
-	const TRIMP = 'activity-trimp';
+	const TRIMP = 'activityTrimp';
 
 	/**
 	 * Calculated elevations

src/CoreBundle/Controller/JsonImportToolController.php
@@ -10,6 +10,8 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -30,19 +32,22 @@ class JsonImportToolController extends Controller
      * @Route("/upload", name="tools-backup-json-upload")
      * @Security("has_role('ROLE_USER')")
      *
-     * @TODO use symfony to move uploaded file
      */
-    public function backupUploadAction()
+    public function backupUploadAction(Request $request)
     {
-        if (!FilenameHandler::validateImportFileExtension($_FILES['qqfile']['name'])) {
+
+        $backupFile = $request->files->get('qqfile');
+        if (!FilenameHandler::validateImportFileExtension($backupFile->getClientOriginalName())) {
             return $this->json(['error' => 'Wrong file extension.']);
         }
 
-        if (!move_uploaded_file($_FILES['qqfile']['tmp_name'], $this->getImportFilePath().$_FILES['qqfile']['name'])) {
+        try {
+            $backupFile->move($this->getImportFilePath(), $backupFile->getClientOriginalName());
+        } catch (FileException $e) {
             return $this->json(['error' => 'Moving file did not work. Set chmod 777 for /data/backup-tool/import/']);
         }
 
-        $this->get('session')->getFlashBag()->set('json-import.file', $_FILES['qqfile']['name']);
+        $this->get('session')->getFlashBag()->set('json-import.file', $backupFile->getClientOriginalName());
 
         return $this->json(['success' => true]);
     }

src/CoreBundle/Controller/ToolsController.php
@@ -11,6 +11,7 @@ use Runalyze\Bundle\CoreBundle\Component\Tool\Table\VdotRaceResultsTable;
 use Runalyze\Bundle\CoreBundle\Component\Tool\Table\VdotPaceTable;
 use Runalyze\Bundle\CoreBundle\Component\Tool\VdotAnalysis\VdotAnalysis;
 use Runalyze\Bundle\CoreBundle\Entity\Account;
+use Runalyze\Bundle\CoreBundle\Form\Tools\DatabaseCleanupType;
 use Runalyze\Bundle\CoreBundle\Form\Tools\Anova\AnovaData;
 use Runalyze\Bundle\CoreBundle\Form\Tools\Anova\AnovaType;
 use Runalyze\Configuration;
@@ -27,20 +28,22 @@ class ToolsController extends Controller
      * @Route("/my/tools/cleanup", name="tools-cleanup")
      * @Security("has_role('ROLE_USER')")
      *
-     * @TODO use symfony form
      */
     public function cleanupAction(Request $request, Account $account)
     {
-        $Frontend = new \Frontend(true, $this->get('security.token_storage'));
-
-        $mode = $request->request->get('mode');
         $prefix = $this->getParameter('database_prefix');
 
-        if (null !== $mode) {
-            if ('general' === $mode) {
-                $job = new JobGeneral($request->request->all(), \DB::getInstance(), $account->getId(), $prefix);
+        $defaultData = array();
+        $form = $this->createForm(DatabaseCleanupType::class, $defaultData);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid() && null !== $form->getData()['mode']) {
+            $Frontend = new \Frontend(true, $this->get('security.token_storage'));
+
+            if ('general' === $form->getData()['mode']) {
+                $job = new JobGeneral($form->getData(), \DB::getInstance(), $account->getId(), $prefix);
             } else {
-                $job = new JobLoop($request->request->all(), \DB::getInstance(), $account->getId(), $prefix);
+                $job = new JobLoop($form->getData(), \DB::getInstance(), $account->getId(), $prefix);
             }
 
             $job->run();
@@ -50,7 +53,9 @@ class ToolsController extends Controller
             ]);
         }
 
-        return $this->render('tools/database_cleanup/form.html.twig');
+        return $this->render('tools/database_cleanup/form.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 
     /**
@@ -59,10 +64,8 @@ class ToolsController extends Controller
      */
     public function tableVdotPaceAction()
     {
-        $Frontend = new \Frontend(true, $this->get('security.token_storage'));
-
         return $this->render('tools/tables/vdot_paces.html.twig', [
-            'currentVdot' => Configuration::Data()->vdot(),
+            'currentVdot' => $this->get('app.configuration_manager')->getList()->getCurrentVdot(),
             'vdots' => (new VdotPaceTable())->getVdotPaces(range(30, 80))
         ]);
     }
@@ -73,8 +76,6 @@ class ToolsController extends Controller
      */
     public function tableGeneralPaceAction()
     {
-        $Frontend = new \Frontend(true, $this->get('security.token_storage'));
-
         $distances = [0.2, 0.4, 1, 3, 5, 10, 21.1, 42.2, 50];
 
         return $this->render('tools/tables/general_paces.html.twig', [
@@ -90,12 +91,9 @@ class ToolsController extends Controller
      */
     public function tableVdotRaceResultAction()
     {
-        $Frontend = new \Frontend(true, $this->get('security.token_storage'));
-
         $distances = [1, 3, 5, 10, 21.1, 42.2, 50];
-
         return $this->render('tools/tables/vdot.html.twig', [
-            'currentVdot' => Configuration::Data()->vdot(),
+            'currentVdot' => $this->get('app.configuration_manager')->getList()->getCurrentVdot(),
             'distances' => array_map(function($km) { return (new Distance($km))->stringAuto(); }, $distances),
             'vdots' => (new VdotRaceResultsTable())->getVdotRaceResults($distances, range(30, 80))
         ]);

src/CoreBundle/Form/Tools/DatabaseCleanupType.php
@@ -1,0 +1,44 @@
+<?php
+
+namespace Runalyze\Bundle\CoreBundle\Form\Tools;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+class DatabaseCleanupType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('mode', ChoiceType::class, array(
+                'expanded' => true,
+                'data' => 'general',
+                'choices' => array(
+                    'General cleanup' => 'general',
+                    'Loop through activities' => 'loop')
+            ))
+            ->add('internals', CheckboxType::class, array('required' => false))
+            ->add('equipment', CheckboxType::class, array('required' => false))
+            ->add('vdotCorrector', CheckboxType::class, array('required' => false))
+            ->add('vdot', CheckboxType::class, array('required' => false))
+            ->add('endurance', CheckboxType::class, array('required' => false))
+            ->add('trimp', CheckboxType::class, array('required' => false))
+            ->add('cacheclean', CheckboxType::class, array('required' => false))
+
+            ->add('activityElevation', CheckboxType::class, array('required' => false))
+            ->add('activityElevationOverwrite', CheckboxType::class, array('required' => false))
+            ->add('activityVdot', CheckboxType::class, array('required' => false))
+            ->add('activityJdpoints', CheckboxType::class, array('required' => false))
+            ->add('activityTrimp', CheckboxType::class, array('required' => false))
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+        ));
+    }
+}

commit 10d93d731d85b6017f6fb11e5401f0570681a710

fix minor dev bug
Introduced in 376f2ad3fc02022a57a5e11ec8990012a9260f09#diff-26143f3962bb8273b13ffd8870645dc2.

src/CoreBundle/Controller/ActivityController.php
@@ -202,10 +202,11 @@ class ActivityController extends Controller
     public function vdotInfoAction($id, Account $account)
     {
         $Frontend = new \Frontend(true, $this->get('security.token_storage'));
+        $configList = $this->get('app.configuration_manager')->getList();
 
         $VdotInfo = new VdotInfo();
         $VdotInfo->setContext(new Context($id, $account->getId()));
-        $VdotInfo->setConfiguration($this->get('app.configuration_manager')->getList()->getCurrentVdot());
+        $VdotInfo->setConfiguration($configList->getData()->getLegacyCategory(), $configList->getVdot()->getLegacyCategory());
 
         return $this->render(':activity:vdot_info.html.twig', [
             'title' => $VdotInfo->getTitle(),