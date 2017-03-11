2b4aab8c860f8a3db06444e8e49cc1511b2c42eb
laufhannes <mail@laufhannes.de>
2017-03-11 17:06:17 +0100

    fix import command for laps
    In theory, this should fix #2097.

--- src/CoreBundle/Command/ActivityBulkImportCommand.php
+++ src/CoreBundle/Command/ActivityBulkImportCommand.php
@@ -2,26 +2,27 @@
 
 namespace Runalyze\Bundle\CoreBundle\Command;
 
+use ImporterFactory;
+use Runalyze\Activity\DuplicateFinder;
 use Runalyze\Error;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
-use ImporterFactory;
-use Runalyze\Activity\DuplicateFinder;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 class ActivityBulkImportCommand extends ContainerAwareCommand
 {
-    protected $failedImports = array();
+    /** @var array */
+    protected $FailedImports = array();
+
     protected function configure()
     {
         $this
             ->setName('runalyze:activity:bulk-import')
             ->setDescription('Bulk import of activity files')
             ->addArgument('username', InputArgument::REQUIRED, 'username')
-            ->addArgument('path', InputArgument::REQUIRED, 'Path to files')
-        ;
+            ->addArgument('path', InputArgument::REQUIRED, 'Path to files');
     }
 
     /**
@@ -32,74 +33,77 @@ class ActivityBulkImportCommand extends ContainerAwareCommand
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $repository = $this->getContainer()->get('doctrine')->getRepository('CoreBundle:Account');
-
         Error::$MAX_NUM_OF_ERRORS = 10000;
 
-        /** @var Account|null $account */
+        $repository = $this->getContainer()->get('doctrine')->getRepository('CoreBundle:Account');
         $user = $repository->loadUserByUsername($input->getArgument('username'));
-        if (NULL === $user) {
+
+        if (null === $user) {
             $output->writeln('<fg=red>Unknown User</>');
-            return;
+
+            return 1;
         }
 
-        $token = new UsernamePasswordToken(
-            $user,
-            null,
-            'main',
-            $user->getRoles());
+        $token = new UsernamePasswordToken($user, null, 'main', $user->getRoles());
         $this->getContainer()->get('security.token_storage')->setToken($token);
-        $path = $input->getArgument('path');
+
         new \Frontend(true, $this->getContainer()->get('security.token_storage'));
         $DuplicateFinder = new DuplicateFinder(\DB::getInstance(), $user->getId());
 
+        $path = $input->getArgument('path');
         $it = new \FilesystemIterator($path);
+
         foreach ($it as $fileinfo) {
             $file = $fileinfo->getFilename();
-            if (! is_file($path.'/'.$file))
-                break;
 
-            $output->writeln('<info>File: '.$file.'</info>');
+            if (!is_file($path.'/'.$file)) {
+                break;
+            }
+
+            $output->writeln('<info>'.$file.'</info>');
             $filename = 'bulk-import'.uniqid().$file;
             copy($path.'/'.$file, FRONTEND_PATH.'../data/import/'.$filename);
 
             try {
                 $Factory = new ImporterFactory($filename);
-            } catch(\Exception $e) {
-                $output->writeln('<fg=red>Failed</>');
+            } catch (\Exception $e) {
+                $output->writeln('<fg=red> ... failed</>');
                 $this->addFailedFile($file, $e->getMessage());
                 break;
             }
 
             foreach ($Factory->trainingObjects() as $training) {
                 try {
-                    if (! $DuplicateFinder->checkForDuplicate($training->getActivityid())) {
-                                $training->setWeatherForecast();
-                                $training->insert();
-                                $output->writeln('<fg=green>Successfully imported</>');
-                    } else {
-                                $output->writeln('<fg=red>' . $file . ' is a duplicate</>');
-                    }
+                    if (!$DuplicateFinder->checkForDuplicate($training->getActivityid())) {
+                        $training->setWeatherForecast();
+                        $training->updateAfterParsing();
+                        $training->insert();
 
+                        $output->writeln('<fg=green> ... successfully imported</>');
+                    } else {
+                        $output->writeln('<fg=red> ... is a duplicate</>');
+                    }
                 } catch (\Exception $e) {
                     $this->addFailedFile($file, $e->getMessage());
-                    $output->writeln('<fg=red>'.$e->getMessage().' </>');
                 }
             }
         }
 
-        if (!empty($this->failedImports)) {
-            $output->writeln('<fg=red>Failed imports</>');
-            foreach ($this->failedImports as $fileName => $message) {
-                $output->writeln('<info>'.$fileName.' - '.$message.'</info>');
+        if (!empty($this->FailedImports)) {
+            $output->writeln('');
+            $output->writeln('<fg=red>Failed imports:</>');
+
+            foreach ($this->FailedImports as $fileName => $message) {
+                $output->writeln('<fg=red> - '.$fileName.': '.$message.'</>');
             }
         }
 
-        $output->writeln('Done');
+        $output->writeln('');
+        $output->writeln('Done.');
     }
 
-    private function addFailedFile($fileName, $error) {
-        $this->failedImports[$fileName] = $error;
+    private function addFailedFile($fileName, $error)
+    {
+        $this->FailedImports[$fileName] = $error;
     }
-
 }
