26d52cb9abdb873df5ed2c0347f58332268ad178
laufhannes <mail@laufhannes.de>
2017-03-21 22:38:52 +0100

    restructure links in headline

--- inc/tpl/tpl.Frontend.header.php
+++ inc/tpl/tpl.Frontend.header.php
@@ -26,16 +26,56 @@
 	<span id="menu-link" onclick="$('#headline').toggleClass('menu-expanded');"><i class="fa fa-fw fa-bars"></i></span>
 	<a class="tab logo" href="<?php echo System::getFullDomain(); ?>" title="Runalyze">Runalyze</a>
 
-	<?php if ($this instanceof \Symfony\Component\DependencyInjection\ContainerAwareInterface): ?>
-	<?php if ($this->get('security.authorization_checker')->isGranted('ROLE_USER')): ?>
-		<a class="tab right" href="<?php echo $this->get('router')->generate('logout'); ?>" title="<?php _e('Logout'); ?>"><i class="fa fa-fw fa-lg fa-sign-out"></i>&nbsp;<?php _e('Logout'); ?></a>
-		<?php echo Ajax::window('<a class="tab right b" href="'.$this->get('router')->generate('settings-account').'"><i class="fa fa-fw fa-lg fa-user"></i>'.NBSP.$this->get('security.token_storage')->getToken()->getUser()->getUsername().'</a>'); ?>
-	<?php endif; ?>
+	<?php if ($this instanceof \Symfony\Component\DependencyInjection\ContainerAwareInterface && $this->get('security.authorization_checker')->isGranted('ROLE_USER')): ?>
+        <div class="headline-menu right">
+            <div class="submenu-label">
+                <?php echo $this->get('security.token_storage')->getToken()->getUser()->getUsername() ?>
+                &nbsp;<i class="fa fa-fw fa-lg fa-user"></i>
+            </div>
+            <ul class="submenu right-oriented">
+                <li><a class="window" href="<?php echo $this->get('router')->generate('settings-account'); ?>"><i class="fa fa-fw fa-cogs"></i>&nbsp;<?php _e('Account settings'); ?></a></li>
+                <li><a class="window" href="<?php echo $this->get('router')->generate('logout'); ?>"><i class="fa fa-fw fa-sign-out"></i>&nbsp;<?php _e('Logout'); ?></a></li>
+            </ul>
+        </div>
 
-	<span class="left b">
-		<?php echo Ajax::window('<a class="tab" href="'.$this->get('router')->generate('settings').'"><i class="fa fa-fw fa-lg fa-cog"></i>'.NBSP.__('Configuration').'</a>'); ?>
-		<?php echo Ajax::window('<a class="tab" href="'.$this->get('router')->generate('tools').'"><i class="fa fa-fw fa-lg fa-dashboard"></i>'.NBSP.__('Tools').'</a>'); ?>
-		<?php echo Ajax::window('<a class="tab" href="'.$this->get('router')->generate('help').'"><i class="fa fa-fw fa-lg fa-question-circle"></i>'.NBSP.__('Help').'</a>'); ?>
-	</span>
+        <div class="headline-menu right">
+            <div class="submenu-label only-icon">
+                <i class="fa fa-fw fa-plus"></i><i class="fa fa-fw fa-caret-down"></i>
+            </div>
+            <ul class="submenu">
+                <li><a class="window" href="<?php echo $this->get('router')->generate('activity-add'); ?>?upload" data-size="small"><i class="fa fa-fw fa-chevron-right small"></i>&nbsp;<?php _e('Activity upload'); ?></a></li>
+                <li><a class="window" href="<?php echo $this->get('router')->generate('activity-add'); ?>?date" data-size="small"><i class="fa fa-fw fa-chevron-right small"></i>&nbsp;<?php _e('Manual activity'); ?></a></li>
+                <li><a class="window" href="<?php echo $this->get('router')->generate('settings'); ?>?key=config_tab_equipment"><i class="fa fa-fw fa-chevron-right small"></i>&nbsp;<?php _e('New equipment'); ?></a></li>
+                <li><a class="window" href="<?php echo $this->get('router')->generate('body-values-add'); ?>"><i class="fa fa-fw fa-chevron-right small"></i>&nbsp;<?php _e('New body values'); ?></a></li>
+            </ul>
+        </div>
+
+        <div class="headline-menu left">
+            <div class="submenu-label b">
+                <i class="fa fa-fw fa-lg fa-cog"></i>&nbsp;<?php _e('Configuration'); ?>
+            </div>
+            <ul class="submenu">
+                <li><a class="window" href="<?php echo $this->get('router')->generate('settings'); ?>?key=config_tab_general"><i class="fa fa-fw fa-chevron-right small"></i>&nbsp;<?php _e('General settings'); ?></a></li>
+                <li><a class="window" href="<?php echo $this->get('router')->generate('settings'); ?>?key=config_tab_plugins"><i class="fa fa-fw fa-chevron-right small"></i>&nbsp;<?php _e('Plugins'); ?></a></li>
+                <li><a class="window" href="<?php echo $this->get('router')->generate('settings'); ?>?key=config_tab_dataset"><i class="fa fa-fw fa-chevron-right small"></i>&nbsp;<?php _e('Dataset'); ?></a></li>
+                <li><a class="window" href="<?php echo $this->get('router')->generate('settings'); ?>?key=config_tab_sports"><i class="fa fa-fw fa-chevron-right small"></i>&nbsp;<?php _e('Sport types'); ?></a></li>
+                <li><a class="window" href="<?php echo $this->get('router')->generate('settings'); ?>?key=config_tab_types"><i class="fa fa-fw fa-chevron-right small"></i>&nbsp;<?php _e('Activity types'); ?></a></li>
+                <li><a class="window" href="<?php echo $this->get('router')->generate('settings'); ?>?key=config_tab_equipment"><i class="fa fa-fw fa-chevron-right small"></i>&nbsp;<?php _e('Equipment'); ?></a></li>
+            </ul>
+        </div>
+
+        <div class="headline-menu left">
+            <div class="submenu-label b">
+                <a class="window" href="<?php echo $this->get('router')->generate('tools'); ?>"><i class="fa fa-fw fa-lg fa-dashboard"></i>&nbsp;<?php _e('Tools'); ?></a>
+            </div>
+            <ul class="submenu">
+                <li><a class="window" href="<?php echo $this->get('router')->generate('tools-anova'); ?>"><i class="fa fa-fw fa-chevron-right small"></i>&nbsp;<?php _e('ANOVA'); ?></a></li>
+                <li><a class="window" href="<?php echo $this->get('router')->generate('tools-vo2max-analysis'); ?>"><i class="fa fa-fw fa-chevron-right small"></i>&nbsp;<?php _e('Analyze your VO2max'); ?></a></li>
+                <li><a class="window" href="<?php echo $this->get('router')->generate('tools-tables'); ?>"><i class="fa fa-fw fa-chevron-right small"></i>&nbsp;<?php _e('Running tables'); ?></a></li>
+                <li><a class="window" href="<?php echo $this->get('router')->generate('poster'); ?>"><i class="fa fa-fw fa-chevron-right small"></i>&nbsp;<?php _e('Poster generator'); ?></a></li>
+            </ul>
+        </div>
+
+        <a class="tab left b" href="<?php echo $this->get('router')->generate('help') ?>"><i class="fa fa-fw fa-lg fa-question-circle"></i>&nbsp;<?php _e('Help'); ?></a>
 	<?php endif; ?>
 </div>
