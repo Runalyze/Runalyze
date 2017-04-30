2926b7698a7b58e0c0f3b915c7256ce65751c6be
laufhannes <mail@laufhannes.de>
2017-04-30 23:05:25 +0200

    start refactoring plugins to symfony world
    Remove uninstall feature.
    
    Refactor moving and toggling panels to symfony world.

---
+++ src/CoreBundle/Entity/PluginRepository.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Runalyze\Bundle\CoreBundle\Entity;
+
+use Doctrine\DBAL\Driver\Statement;
+use Doctrine\ORM\EntityRepository;
+
+class NotificationRepository extends EntityRepository
+{
+    /**
+     * @param Account|null $account
+     * @return Notification[]
+     */
+    public function findAll(Account $account = null)
+    {
+        if (null === $account) {
+            throw new \LogicException('Finding all notifications is only possible for a specific account.');
+        }
+
+        return $this->findAllSince(null, $account);
+    }
+
+    /**
+     * @param null|int $timestamp
+     * @param Account $account
+     * @return Notification[]
+     */
+    public function findAllSince($timestamp, Account $account)
+    {
+        $queryBuilder = $this->createQueryBuilder('n')
+            ->where('n.account = :account')
+            ->andWhere('n.wasRead = 0') // Only necessary as long as headline menu is filled via request with $timestamp = 0
+            ->setParameter('account', $account);
+
+        if (null !== $timestamp && 0 !== $timestamp) {
+            $queryBuilder
+                ->andWhere('n.createdAt > :time')
+                ->setParameter('time', $timestamp)
+                ->orderBy('n.createdAt', 'DESC');
+        }
+
+        return $queryBuilder->getQuery()->getResult();
+    }
+
+    public function markAsRead(Notification $notification)
+    {
+        $this->save($notification->setRead());
+    }
+
+    public function markAllAsRead(Account $account)
+    {
+        $this->createQueryBuilder('n')
+            ->update()
+            ->set('n.wasRead', true)
+            ->where('n.account = :account')
+            ->setParameter('account', $account)
+            ->getQuery()
+            ->execute();
+
+        $this->_em->clear(Notification::class);
+    }
+
+    /**
+     * @return int number of removed notifications
+     */
+    public function removeExpiredNotifications()
+    {
+        $numDeleted = $this->createQueryBuilder('n')
+            ->delete()
+            ->where('n.expirationAt < :time')
+            ->setParameter('time', time())
+            ->getQuery()
+            ->execute();
+
+        $this->_em->clear(Notification::class);
+
+        return $numDeleted;
+    }
+
+    public function save(Notification $notification)
+    {
+        $this->_em->persist($notification);
+        $this->_em->flush();
+    }
+}
---
+++ tests/CoreBundle/Entity/PluginRepositoryTest.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace Runalyze\Bundle\CoreBundle\Tests\Entity;
+
+use Runalyze\Bundle\CoreBundle\Entity\Account;
+use Runalyze\Bundle\CoreBundle\Entity\Sport;
+use Runalyze\Bundle\CoreBundle\Entity\SportRepository;
+use Runalyze\Bundle\CoreBundle\Entity\Training;
+use Runalyze\Bundle\CoreBundle\Entity\TrainingRepository;
+use Runalyze\Profile\Sport\SportProfile;
+
+class SportRepositoryTest extends AbstractRepositoryTestCase
+{
+    /** @var SportRepository */
+    protected $SportRepository;
+
+    /** @var TrainingRepository */
+    protected $TrainingRepository;
+
+    /** @var Account */
+    protected $Account;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->SportRepository = $this->EntityManager->getRepository('CoreBundle:Sport');
+        $this->TrainingRepository = $this->EntityManager->getRepository('CoreBundle:Training');
+        $this->Account = $this->getDefaultAccount();
+    }
+
+    /**
+     * @param int|null $timestamp
+     * @param int|float $duration
+     * @param float|int|null $distance
+     * @param Sport|null $sport
+     * @return Training
+     */
+    protected function insertActivityForDefaultAccount(
+        $timestamp = null,
+        $duration = 3600,
+        $distance = null,
+        Sport $sport = null
+    )
+    {
+        $activity = $this->getActivitiyForDefaultAccount($timestamp, $duration, $distance, $sport);
+
+        $this->TrainingRepository->save($activity);
+
+        return $activity;
+    }
+
+    public function testEmptyAccount()
+    {
+        $account = $this->getEmptyAccount();
+
+        $this->assertEmpty($this->SportRepository->findAllFor($account));
+        $this->assertEmpty($this->SportRepository->findWithDistancesFor($account));
+        $this->assertEmpty($this->SportRepository->getUsedInternalSportIdsFor($account));
+
+        $this->assertTrue($this->SportRepository->isInternalTypeFree(SportProfile::RUNNING, $account));
+        $this->assertTrue($this->SportRepository->isInternalTypeFree(SportProfile::CYCLING, $account));
+        $this->assertTrue($this->SportRepository->isInternalTypeFree(SportProfile::SWIMMING, $account));
+
+        $this->assertNull($this->SportRepository->findRunningFor($account, true));
+        $this->assertNull($this->SportRepository->findRunningFor($account)->getId());
+    }
+
+    public function testDefaultAccount()
+    {
+        $account = $this->getDefaultAccount();
+
+        $this->assertFalse($this->SportRepository->isInternalTypeFree(SportProfile::RUNNING, $account));
+        $this->assertFalse($this->SportRepository->isInternalTypeFree(SportProfile::CYCLING, $account));
+        $this->assertFalse($this->SportRepository->isInternalTypeFree(SportProfile::SWIMMING, $account));
+
+        $this->assertEquals($this->getDefaultAccountsRunningSport()->getId(), $this->SportRepository->findRunningFor($account)->getId());
+    }
+
+    public function testSportStatisticsWithoutData()
+    {
+        $statistics = $this->SportRepository->getSportStatisticsSince(null, $this->Account);
+
+        $this->assertEquals(0, $statistics->getCount());
+        $this->assertEmpty($statistics->getStatistics());
+    }
+
+    public function testSportStatisticsWithData()
+    {
+        $this->insertActivityForDefaultAccount(1400000000, 3600, 10.0, $this->getDefaultAccountsRunningSport());
+        $this->insertActivityForDefaultAccount(1500000000, 3400, 12.0, $this->getDefaultAccountsRunningSport());
+        $this->insertActivityForDefaultAccount(1400000000, 7600, 63.5, $this->getDefaultAccountsCyclingSport());
+
+        $allTimeStatistics = $this->SportRepository->getSportStatisticsSince(null, $this->Account);
+        $allTimeStatisticsRunning = $allTimeStatistics->getStatisticFor($this->getDefaultAccountsRunningSport());
+        $allTimeStatisticsCycling = $allTimeStatistics->getStatisticFor($this->getDefaultAccountsCyclingSport());
+
+        $this->assertEquals(2, $allTimeStatistics->getCount());
+
+        $this->assertEquals(7000.0, $allTimeStatisticsRunning->getTotalDuration());
+        $this->assertEquals(22.0, $allTimeStatisticsRunning->getTotalDistance());
+        $this->assertEquals(2, $allTimeStatisticsRunning->getNumberOfActivities());
+
+        $this->assertEquals(7600.0, $allTimeStatisticsCycling->getTotalDuration());
+        $this->assertEquals(63.5, $allTimeStatisticsCycling->getTotalDistance());
+        $this->assertEquals(1, $allTimeStatisticsCycling->getNumberOfActivities());
+    }
+}
