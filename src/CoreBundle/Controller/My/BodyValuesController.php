595f907000090c4da8dde86c88a75fbaa44dd991
Hannes <mail@laufhannes.de>
2017-05-16 14:50:28 +0200

    fix body values controller

--- src/CoreBundle/Controller/My/BodyValuesController.php
+++ src/CoreBundle/Controller/My/BodyValuesController.php
@@ -10,6 +10,7 @@ use Runalyze\Bundle\CoreBundle\Services\AutomaticReloadFlagSetter;
 use Runalyze\Metrics\HeartRate\Unit\BeatsPerMinute;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Form\Extension\Core\Type\PercentType;
