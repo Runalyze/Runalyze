36b34dbedb9815e284a4459771b6f40da23696f2
Michael Pohl <michael@runalyze.com>
2017-05-18 08:24:30 +0200

    fix missing security annotation in body value controller

--- src/CoreBundle/Controller/My/BodyValuesController.php
+++ src/CoreBundle/Controller/My/BodyValuesController.php
@@ -10,12 +10,14 @@ use Runalyze\Bundle\CoreBundle\Services\AutomaticReloadFlagSetter;
 use Runalyze\Metrics\HeartRate\Unit\BeatsPerMinute;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Form\Extension\Core\Type\PercentType;
 
 /**
  * @Route("/my/body-values")
+ * @Security("has_role('ROLE_USER')")
  */
 class BodyValuesController extends Controller
 {
