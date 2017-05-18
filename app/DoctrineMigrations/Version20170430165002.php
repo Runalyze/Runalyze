0f6c928c659c35171b87fa515c635c20c9a2812a
Michael Pohl <michael@mipapo.de>
2017-05-18 08:13:15 +0200

    Update Version20170430165002.php

--- app/DoctrineMigrations/Version20170430165002.php
+++ app/DoctrineMigrations/Version20170430165002.php
@@ -25,7 +25,7 @@ class Version20170430165002 extends AbstractMigration implements ContainerAwareI
         $prefix = $this->container->getParameter('database_prefix');
 
         $this->addSql('ALTER TABLE `'.$prefix.'notification` DROP foreign key `FK_F99B51889B6B5FBA`');
-        $this->addSql('ALTER TABLE `'.$prefix.'notification` ADD CONSTRAINT FK_F99B51889B6B5FBA FOREIGN KEY (account_id) REFERENCES `'.$prefix.'account (id) ON DELETE CASCADE ON UPDATE CASCADE');
+        $this->addSql('ALTER TABLE `'.$prefix.'notification` ADD CONSTRAINT FK_F99B51889B6B5FBA FOREIGN KEY (account_id) REFERENCES `'.$prefix.'account` (id) ON DELETE CASCADE ON UPDATE CASCADE');
 
     }
 
