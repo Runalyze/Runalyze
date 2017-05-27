575bcfbc2bb014786b3cd8b1755dd078d286dfec
laufhannes <mail@laufhannes.de>
2017-05-27 16:20:15 +0200

    try to fix travis builds for pull requests

--- bin/travis_build.sh
+++ bin/travis_build.sh
@@ -1,27 +1,35 @@
 #!/bin/bash
 
-if [[ "$BUILD_ARCHIVE" == "true" ]] ; then
-    mkdir runalyze
-    git archive HEAD | tar -x -C runalyze/
-    cp -r vendor/ runalyze/vendor/
+openssl aes-256-cbc -K $encrypted_d056b0ac7927_key -iv $encrypted_d056b0ac7927_iv -in bin/deploy_rsa.enc -out /tmp/deploy_rsa -d
 
-    cd runalyze
+eval "$(ssh-agent -s)"
 
-    if [[ `npm -v` != 3* ]]; then npm i -g npm@3; fi
-    npm install -g gulpjs/gulp-cli
-    npm install
-    gulp
+chmod 600 /tmp/deploy_rsa
+ssh-add /tmp/deploy_rsa
 
-    sudo rm -r node_modules
-    sudo rm -r vendor/willdurand/geocoder/tests
+sudo apt-get install gettext -y
 
-    cd ..
+mkdir runalyze
+git archive HEAD | tar -x -C runalyze/
+cp -r vendor/ runalyze/vendor/
 
-    tar -czf runalyze.tar.gz runalyze/ --exclude-vcs
-    zip -r -q --exclude='*.git*' runalyze.zip runalyze/
+cd runalyze
 
-    ssh-keyscan $PHOST >> ~/.ssh/known_hosts
+if [[ `npm -v` != 3* ]]; then npm i -g npm@3; fi
+npm install -g gulpjs/gulp-cli
+npm install
+gulp
 
-    scp runalyze.tar.gz ${PUSERNAME}@${PHOST}:branches/${TRAVIS_BRANCH}/runalyze.tar.gz
-    scp runalyze.zip ${PUSERNAME}@${PHOST}:branches/${TRAVIS_BRANCH}/runalyze.zip
-fi
+sudo rm -r node_modules
+sudo rm -r vendor/willdurand/geocoder/tests
+
+cd ..
+
+tar -czf runalyze.tar.gz runalyze/ --exclude-vcs
+zip -r -q --exclude='*.git*' runalyze.zip runalyze/
+
+ssh-keyscan $PHOST >> ~/.ssh/known_hosts
+
+ssh ${PUSERNAME}@${PHOST} "mkdir -p branches/${TRAVIS_BRANCH}/"
+scp runalyze.tar.gz ${PUSERNAME}@${PHOST}:branches/${TRAVIS_BRANCH}/runalyze.tar.gz
+scp runalyze.zip ${PUSERNAME}@${PHOST}:branches/${TRAVIS_BRANCH}/runalyze.zip
