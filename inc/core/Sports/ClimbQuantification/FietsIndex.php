3f9dcaac39aaa72b3d97277f23e36628551d056e
laufhannes <mail@laufhannes.de>
2017-06-10 18:22:37 +0200

    add classes for climb category

--- inc/core/Sports/ClimbQuantification/FietsIndex.php
+++ inc/core/Sports/ClimbQuantification/FietsIndex.php
@@ -5,10 +5,15 @@ namespace Runalyze\Sports\ClimbQuantification;
 /**
  * @see https://www.pjammcycling.com/fiets-index.html
  */
-class FietsIndex extends AbstractAdditiveClimbQuantification
+class FietsIndex extends AbstractAdditiveClimbQuantification implements CategorizableInterface
 {
     public function getScoreFor($distance, $elevation, $altitudeAtTop = 0)
     {
         return $elevation * $elevation / ($distance * 10000) + max(0, ($altitudeAtTop - 1000) / 1000);
+    }
+
+    public function getLowerLimitsForCategorization()
+    {
+        return [6.5, 5.0, 3.5, 2.0, 0.5, 0.25];
     }
 }
