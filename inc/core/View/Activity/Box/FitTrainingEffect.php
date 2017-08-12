0c3ab79ec223b12fcb8e5ed37bddc1ae72449e26
laufhannes <mail@laufhannes.de>
2017-08-12 17:18:32 +0200

    minor adjustment
    Add glossary link to some boxes.

--- inc/core/View/Activity/Box/FitTrainingEffect.php
+++ inc/core/View/Activity/Box/FitTrainingEffect.php
@@ -1,31 +1,18 @@
 <?php
-/**
- * This file contains class::FitTrainingEffect
- * @package Runalyze\View\Activity\Box
- */
 
 namespace Runalyze\View\Activity\Box;
 
 use Runalyze\Activity;
 use Runalyze\View\Activity\Context;
 
-/**
- * Boxed value for FitTrainingEffect
- * 
- * @author Michael Pohl
- * @author Hannes Christiansen
- * @package Runalyze\View\Activity\Box
- */
 class FitTrainingEffect extends ValueBox
 {
-	/**
-	 * Constructor
-	 * @param \Runalyze\View\Activity\Context $Context
-	 */
 	public function __construct(Context $Context)
 	{
 		parent::__construct(
-			new Activity\TrainingEffect($Context->activity()->fitTrainingEffect())
+			new Activity\TrainingEffect($Context->activity()->fitTrainingEffect()),
+            '',
+            'training-effect'
 		);
 	}
 }
