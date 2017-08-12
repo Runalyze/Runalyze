3864c589f2e87800ca0caf9b44e185fb97559dcf
laufhannes <mail@laufhannes.de>
2017-08-12 16:47:58 +0200

    allow comma in form for percentage type
    Fixes #2204.

--- src/CoreBundle/Form/Type/PercentageType.php
+++ src/CoreBundle/Form/Type/PercentageType.php
@@ -3,18 +3,42 @@
 namespace Runalyze\Bundle\CoreBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\DataTransformerInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 
-class PercentageType extends AbstractType
+class PercentageType extends AbstractType implements DataTransformerInterface
 {
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->addModelTransformer($this);
+    }
 
     public function buildView(FormView $view, FormInterface $form, array $options)
     {
         $view->vars['input_unit'] = '%';
+    }
+
+    /**
+     * @param  mixed $value
+     * @return string
+     */
+    public function transform($value)
+    {
+        return null === $value ? '' : (string)$value;
+    }
+
+    /**
+     * @param  null|string $value
+     * @return float|null
+     */
+    public function reverseTransform($value)
+    {
+        return null === $value ? null : (float)str_replace(',', '.', $value);
     }
 
     /**
