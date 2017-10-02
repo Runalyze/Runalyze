45c8037f605b661b6a25d95b07743c69f84c59e7
laufhannes <mail@laufhannes.de>
2017-10-02 18:34:59 +0200

    refactor slf parser

---
+++ inc/core/Parser/Activity/FileType/Slf3.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace Runalyze\Parser\Activity\FileType;
+
+use Runalyze\Import\Exception\UnsupportedFileException;
+use Runalyze\Metrics\Common\Unit\Simple;
+use Runalyze\Parser\Activity\Common\AbstractSingleParser;
+use Runalyze\Parser\Activity\Common\Data\Pause\Pause;
+use Runalyze\Parser\Activity\Common\Data\Round\Round;
+use Runalyze\Parser\Common\FileContentAwareParserInterface;
+use Runalyze\Parser\Common\XmlParserTrait;
+use SimpleXMLElement;
+
+class Slf3 extends AbstractSingleParser implements FileContentAwareParserInterface
+{
+    use XmlParserTrait;
+
+    /** @var int [s] */
+    protected $PauseInSeconds = 0;
+
+    public function parse()
+    {
+        $this->checkThatXmlIsValid();
+
+        $this->parseGeneralValues();
+        $this->parseLogEntries();
+        $this->parseLaps();
+    }
+
+    protected function checkThatXmlIsValid()
+    {
+        if (!property_exists($this->Xml, 'LogEntries')) {
+            throw new UnsupportedFileException('Given XML object is not from Sigma. &lt;LogEntries&gt;-tag could not be located.');
+        }
+    }
+
+    protected function parseGeneralValues()
+    {
+        $this->Container->Metadata->setTimestampAndTimezoneOffsetWithUtcFixFrom((string)$this->Xml->GeneralInformation->StartDate);
+        $this->Container->Metadata->setCreator('Sigma', $this->findCreator());
+    }
+
+    protected function parseLogEntries()
+    {
+        if (empty($this->Xml->LogEntries->LogEntry)) {
+            throw new UnsupportedFileException('This file does not contain any data.');
+        } else {
+            foreach ($this->Xml->LogEntries->LogEntry as $log) {
+                $this->parseLogEntry($log);
+            }
+        }
+    }
+
+    protected function parseLogEntry(SimpleXMLElement $log)
+    {
+        if ((int)$log->Time == 0 || (string)$log->IsPause == 'true') {
+            if ((int)$log->PauseTime > 0 && !empty($this->Container->ContinuousData->Time)) {
+                $this->Container->Pauses->add(new Pause(
+                    end($this->Container->ContinuousData->Time),
+                    (int)$log->PauseTime
+                ));
+            }
+
+            return;
+        }
+
+        $this->Container->ContinuousData->Time[] = (int)$log->TimeAbsolute;
+        $this->Container->ContinuousData->Distance[] = (float)$log->DistanceAbsolute / 1000;
+        $this->Container->ContinuousData->HeartRate[] = (!empty($log->Heartrate)) ? round((float)$log->Heartrate) : null;
+    }
+
+    protected function parseLaps()
+    {
+        if (!empty($this->Xml->Laps)) {
+            $readEnergyConsumption = null === $this->Container->ActivityData->EnergyConsumption;
+
+            foreach ($this->Xml->Laps->Lap as $lap) {
+                $this->parseLap($lap, $readEnergyConsumption);
+            }
+        }
+    }
+
+    protected function parseLap(SimpleXMLElement $lap, $readEnergyConsumption = true)
+    {
+        if ($readEnergyConsumption && !empty($lap->Calories)) {
+            $this->Container->ActivityData->EnergyConsumption += (int)$lap->Calories;
+        }
+
+        $this->Container->Rounds->add(new Round(
+            round((int)$lap->Distance) / 1000,
+            round((float)$lap->Time),
+            ((string)$lap->FastLap != 'false')
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    protected function findCreator()
+    {
+        $string = '';
+
+        if (!empty($this->Xml->GeneralInformation)) {
+            foreach ($this->Xml->GeneralInformation->attributes() as $key => $value) {
+                $string .= (string)$key.': '.((string)$value)."\n";
+            }
+        }
+
+        return $string;
+    }
+}
---
+++ inc/core/Parser/Activity/FileType/Slf4.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Runalyze\Parser\Activity\FileType;
+
+use Runalyze\Import\Exception\UnsupportedFileException;
+use Runalyze\Metrics\Common\Unit\Simple;
+use Runalyze\Parser\Activity\Common\AbstractSingleParser;
+use Runalyze\Parser\Activity\Common\Data\Pause\Pause;
+use Runalyze\Parser\Activity\Common\Data\Round\Round;
+use Runalyze\Parser\Common\XmlParserTrait;
+use SimpleXMLElement;
+
+class Slf4 extends Slf3
+{
+    protected function checkThatXmlIsValid()
+    {
+        if (!property_exists($this->Xml, 'Entries')) {
+            throw new UnsupportedFileException('Given XML object is not from Sigma. &lt;Entries&gt;-tag could not be located.');
+        }
+    }
+
+    protected function parseGeneralValues()
+    {
+        $this->Container->Metadata->setTimestampAndTimezoneOffsetWithUtcFixFrom((string)$this->Xml->GeneralInformation->startDate);
+        $this->Container->Metadata->setCreator('Sigma', $this->findCreator());
+        $this->Container->Metadata->setDescription((string)$this->Xml->GeneralInformation->name);
+
+        $this->Container->ActivityData->EnergyConsumption = (int)$this->Xml->GeneralInformation->calories;
+    }
+
+    protected function parseLogEntries()
+    {
+        if (!isset($this->Xml->Entries->Entry) ) {
+            if ($this->Xml->GeneralInformation->trainingTime) {
+                $this->Container->ActivityData->Duration = (int)$this->Xml->GeneralInformation->trainingTime / 100;
+                $this->Container->ActivityData->Distance = (float)$this->Xml->GeneralInformation->distance / 1000;
+                $this->Container->ActivityData->MaxHeartRate = (int)$this->Xml->GeneralInformation->maximumHeartrate;
+                $this->Container->ActivityData->AvgHeartRate = (int)$this->Xml->GeneralInformation->averageHeartrate;
+            } else {
+                throw new UnsupportedFileException('This file does not contain any data.');
+            }
+        } else {
+            foreach ($this->Xml->Entries->Entry as $log) {
+                $this->parseLogEntry($log);
+            }
+        }
+    }
+
+    protected function parseLogEntry(SimpleXMLElement $log)
+    {
+        $log = $log->attributes();
+
+        if ((int)$log['trainingTime'] == 0) {
+            return;
+        }
+
+        $this->Container->ContinuousData->Time[] = (int)$log['trainingTimeAbsolute'] / 100;
+        $this->Container->ContinuousData->Distance[] = (float)$log['distanceAbsolute'] / 1000;
+        $this->Container->ContinuousData->HeartRate[] = (!empty($log['heartrate'])) ? round((float)$log['heartrate']) : null;
+    }
+
+    protected function parseLaps()
+    {
+        if (!empty($this->Xml->Markers)) {
+            $readEnergyConsumption = null === $this->Container->ActivityData->EnergyConsumption;
+
+            foreach ($this->Xml->Markers->Marker as $lap) {
+                $this->parseLap($lap, $readEnergyConsumption);
+            }
+        }
+    }
+
+    protected function parseLap(SimpleXMLElement $lap, $readEnergyConsumption = true)
+    {
+        $lap = $lap->attributes();
+
+        if ($readEnergyConsumption && !empty($lap['calories'])) {
+            $this->Container->ActivityData->EnergyConsumption += (int)$lap['calories'];
+        }
+
+        $this->Container->Rounds->add(new Round(
+            round((int)$lap['distance']) / 1000,
+            round((float)$lap['time']),
+            ((string)$lap['FastLap'] != 'false')
+        ));
+    }
+}
