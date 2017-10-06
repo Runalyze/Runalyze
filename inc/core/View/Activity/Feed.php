6a4e84457f63428d2a8a7e41a73bd2c518944538
Michael Pohl <michael@runalyze.com>
2017-10-06 23:11:40 +0200

    initial rss athlete feed

---
+++ inc/core/View/Activity/Feed.php
@@ -0,0 +1,160 @@
+<?php
+namespace Runalyze\View\Activity;
+
+use PicoFeed\Syndication\Rss20FeedBuilder;
+use PicoFeed\Syndication\Rss20ItemBuilder;
+use Runalyze\Bundle\CoreBundle\Component\Activity\ActivityContext;
+use Runalyze\Bundle\CoreBundle\Component\Activity\ActivityDecorator;
+use Runalyze\Bundle\CoreBundle\Services\Activity\ActivityContextFactory;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Runalyze\Util\LocalTime;
+use Symfony\Component\Translation\TranslatorInterface;
+use Runalyze\Bundle\CoreBundle\Entity\Training;
+
+class Feed {
+
+    /** @var array $Activities */
+    protected $Activities;
+
+    /** @var Rss20FeedBuilder */
+    protected $FeedBuilder;
+
+    /** @var TranslatorInterface */
+    protected $Translator;
+
+    /** @var ActivityContextFactory */
+    protected $ActivityContextFactory;
+
+    /**
+     * Feed constructor.
+     * @param TranslatorInterface $translator
+     */
+    public function __construct(TranslatorInterface $translator, ActivityContextFactory $activityContextFactory)
+    {
+        $this->FeedBuilder = new Rss20FeedBuilder();
+        $this->ActivityContextFactory = $activityContextFactory;
+        $this->Translator = $translator;
+        $this->FeedBuilder->withDate(new \DateTime());
+    }
+
+    /**
+     * @param TranslatorInterface $translator
+     */
+    public function setTranslator(TranslatorInterface $translator) {
+        $this->Translator = $translator;
+    }
+
+    /**
+     * @return string
+     */
+    public function buildFeed()
+    {
+        $this->createItems();
+        return $this->FeedBuilder->build();
+    }
+
+    /**
+     * @param $activities
+     * @return $this
+     */
+    public function setActivities($activities)
+    {
+        $this->Activities = $activities;
+        return $this;
+    }
+
+    /**
+     * @param $author
+     * @return $this
+     */
+    public function setFeedAuthor($author)
+    {
+        $this->FeedBuilder->withAuthor($author);
+        return $this;
+    }
+
+    /**
+     * @param $title
+     * @return $this
+     */
+    public function setFeedTitle($title)
+    {
+        $this->FeedBuilder->withTitle($title);
+        return $this;
+    }
+
+    /**
+     * @param $feed
+     * @return $this
+     */
+    public function setFeedUrl($feed)
+    {
+        $this->FeedBuilder->withFeedUrl($feed);
+        return $this;
+    }
+
+    /**
+     * @param $site
+     * @return $this
+     */
+    public function setSiteUrl($site)
+    {
+        $this->FeedBuilder->withSiteUrl($site);
+        return $this;
+    }
+
+    /**
+     * @param \DateTime $date
+     * @return $this
+     */
+    public function setFeedDate(\DateTime $date)
+    {
+        $this->FeedBuilder->withDate($date);
+        return $this;
+    }
+
+    /**
+     * @param ActivityContext $activityContext
+     * @return string
+     */
+    private function createItemContent(ActivityContext $activityContext)
+    {
+        $content = '<h1>'.$this->Translator->trans('Sport') . ': ' . $activityContext->getActivity()->getSport()->getName().'</h1>';
+        $content .= '<br><b>'.$this->Translator->trans('Duration') . '</b>: ' . $activityContext->getActivity()->getElapsedTime();
+
+        if ($activityContext->getActivity()->getType() !== null) {
+            $content .= '<br><b>'.$this->Translator->trans('Activity type') . '</b>: ' . $activityContext->getActivity()->getType()->getName();
+        }
+
+        return $content;
+    }
+
+    /**
+     * @param Training $activity
+     */
+    private function createItem(Training $activity)
+    {
+        $item = new Rss20ItemBuilder($this->FeedBuilder);
+        $activityContext = $this->ActivityContextFactory->getContext($activity);
+        $time = (new LocalTime($activityContext->getActivity()->getTime()))->format('d.m.Y H:i');
+
+        $item->withTitle($time.' '.$activityContext->getActivity()->getDistance(). ' ' .$activityContext->getDecorator()->getTitle());
+        $item->withPublishedDate(new LocalTime($activityContext->getActivity()->getTime()));
+        $item->withContent($this->createItemContent($activity, $activityContext));
+        $item->withAuthor($activity->getAccount()->getUsername());
+        if ($activityContext->getActivity()->isPublic()) {
+            //$item->withUrl($this->generateUrl('shared-activity', array('activityHash' => $activityContext->getActivity()->getId()), UrlGeneratorInterface::ABSOLUTE_URL));
+        }
+        $this->FeedBuilder->withItem($item);
+
+        //$account = $this->getDoctrine()->getRepository('CoreBundle:Account')->findByUsername($username);
+        //$privacy = $this->get('app.configuration_manager')->getList($account)->getPrivacy();
+    }
+    private function createItems()
+    {
+        foreach($this->Activities as $activity) {
+            $this->createItem($activity);
+        }
+    }
+
+}
--- src/CoreBundle/Controller/SharedController.php
+++ src/CoreBundle/Controller/SharedController.php
@@ -5,19 +5,35 @@ namespace Runalyze\Bundle\CoreBundle\Controller;
 use Runalyze\Bundle\CoreBundle\Entity\Account;
 use Runalyze\Bundle\CoreBundle\Entity\Training;
 use Runalyze\Export\Share\Facebook;
+use Runalyze\Util\LocalTime;
 use Runalyze\View\Activity\Context;
+use Runalyze\View\Activity\Feed;
 use Runalyze\View\Activity\Linker;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use PicoFeed\Syndication\Rss20FeedBuilder;
+use PicoFeed\Syndication\Rss20ItemBuilder;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Runalyze\Bundle\CoreBundle\Component\Activity\ActivityDecorator;
+
 
 class SharedController extends Controller
 {
     /**
+     * @return TrainingRepository
+     */
+    protected function getTrainingRepository()
+    {
+        return $this->getDoctrine()->getRepository('CoreBundle:Training');
+    }
+
+    /**
      * @Route("/shared/{activityHash}&{foo}&{bar}&{baz}", requirements={"activityHash": "[a-zA-Z0-9]+"})
      * @Route("/shared/{activityHash}&{foo}&{bar}", requirements={"activityHash": "[a-zA-Z0-9]+"})
      * @Route("/shared/{activityHash}&{foo}", requirements={"activityHash": "[a-zA-Z0-9]+"})
-     * @Route("/shared/{activityHash}", requirements={"activityHash": "[a-zA-Z0-9]+"})
+     * @Route("/shared/{activityHash}", requirements={"activityHash": "[a-zA-Z0-9]+"}, name="shared-activity")
      */
     public function sharedTrainingAction($activityHash, Request $request)
     {
@@ -108,6 +124,39 @@ class SharedController extends Controller
             'legacyStatistics' => $legacyStatistics,
             'dataBrowser' => new \DataBrowserShared()
         ]);
+    }
+
+    /**
+     * @Route("/athlete/{username}/feed", name="shared-athlete-feed")
+     */
+    public function publicUserFeedAction($username, Request $request) {
+        /** @var null|Account $account */
+        $account = $this->getDoctrine()->getRepository('CoreBundle:Account')->findByUsername($username);
+        $privacy = $this->get('app.configuration_manager')->getList($account)->getPrivacy();
+
+        $feed = $this->get('app.activity.feed');
+        $feed->setFeedTitle('RUNALYZE athlete '.$username)
+                ->setFeedUrl($this->generateUrl('shared-athlete-feed', array('username' => $username), UrlGeneratorInterface::ABSOLUTE_URL));
+        $feed->setSiteUrl($this->generateUrl('shared-athlete', array('username' => $username), UrlGeneratorInterface::ABSOLUTE_URL));
+        $feed->setFeedAuthor($username);
+
+        if (null === $account || !$privacy->isListPublic()) {
+            return new Response(
+                $feed->buildFeed(),
+                Response::HTTP_OK,
+                array('content-type' => 'text/xml')
+            );
+        }
+
+        $activities = $this->getTrainingRepository()->latestActivities($account);
+        $feed->setActivities($activities);
+
+        return new Response(
+            $feed->buildFeed(),
+            Response::HTTP_OK,
+            array('content-type' => 'text/xml')
+        );
+
     }
 
     /**
