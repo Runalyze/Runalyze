cf4db993a57864c8af6f0c4466be8b1fd3781706
laufhannes <mail@laufhannes.de>
2017-10-18 22:49:00 +0200

    try to fight google maps responses in unit tests

---
+++ inc/core/Service/ElevationCorrection/Strategy/NoResponseException.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Runalyze\Service\ElevationCorrection\Strategy;
+
+class NoResponseException extends InvalidResponseException
+{
+}
--- tests/inc/core/Service/ElevationCorrection/Strategy/GoogleMapsTest.php
+++ tests/inc/core/Service/ElevationCorrection/Strategy/GoogleMapsTest.php
@@ -9,17 +9,22 @@ namespace Runalyze\Service\ElevationCorrection\Strategy;
  */
 class GoogleMapsTest extends \PHPUnit_Framework_TestCase
 {
-
 	public function testSimpleData()
 	{
-		$Corrector = new GoogleMaps(
-			array(49.444722),
-			array(7.768889)
+		$corrector = new GoogleMaps(
+			[49.444722],
+			[7.768889]
 		);
 
-		if ($Corrector->canHandleData()) {
-			$Corrector->correctElevation();
-			$this->assertEquals( array(237), $Corrector->getCorrectedElevation() );
+		if ($corrector->canHandleData()) {
+		    try {
+                $corrector->correctElevation();
+                $this->assertEquals([237], $corrector->getCorrectedElevation());
+            } catch (NoResponseException $e) {
+                $this->markTestSkipped('Google Maps was not available.');
+            } catch (OverQueryLimitException $e) {
+                $this->markTestSkipped('Google Maps query limit reached.');
+            }
 		} else {
 			$this->markTestSkipped('Google Maps was not available.');
 		}
@@ -27,14 +32,23 @@ class GoogleMapsTest extends \PHPUnit_Framework_TestCase
 
 	public function testSimplePath()
 	{
-		$Corrector = new GoogleMaps(
-			array(49.440, 49.441, 49.442, 49.443, 49.444, 49.445, 49.446, 49.447, 49.448, 49.449, 49.450),
-			array(7.760, 7.761, 7.762, 7.763, 7.764, 7.765, 7.766, 7.767, 7.768, 7.769, 7.770)
+		$corrector = new GoogleMaps(
+			[49.440, 49.441, 49.442, 49.443, 49.444, 49.445, 49.446, 49.447, 49.448, 49.449, 49.450],
+			[7.760, 7.761, 7.762, 7.763, 7.764, 7.765, 7.766, 7.767, 7.768, 7.769, 7.770]
 		);
 
-		if ($Corrector->canHandleData()) {
-			$Corrector->correctElevation();
-			$this->assertEquals( array(238, 238, 238, 238, 238, 237, 237, 237, 237, 237, 263), $Corrector->getCorrectedElevation() );
+		if ($corrector->canHandleData()) {
+            try {
+			    $corrector->correctElevation();
+			    $this->assertEquals(
+			        [238, 238, 238, 238, 238, 237, 237, 237, 237, 237, 263],
+                    $corrector->getCorrectedElevation()
+                );
+            } catch (NoResponseException $e) {
+                $this->markTestSkipped('Google Maps was not available.');
+            } catch (OverQueryLimitException $e) {
+                $this->markTestSkipped('Google Maps query limit reached.');
+            }
 		} else {
 			$this->markTestSkipped('Google Maps was not available.');
 		}
@@ -42,14 +56,21 @@ class GoogleMapsTest extends \PHPUnit_Framework_TestCase
 
 	public function testUnknown()
 	{
-		$Corrector = new GoogleMaps(
-			array( 0, 49.440, 49.441, 49.442, 49.443, 49.444, 49.445, 0, 0, 0, 0, 0, 0, 49.446, 49.447, 49.448, 49.449, 49.450),
-			array( 0,  7.760,  7.761,  7.762,  7.763,  7.764,  7.765, 0, 0, 0, 0, 0, 0,  7.766,  7.767,  7.768,  7.769,  7.770)
+		$corrector = new GoogleMaps(
+			[0, 49.440, 49.441, 49.442, 49.443, 49.444, 49.445, 0, 0, 0, 0, 0, 0, 49.446, 49.447, 49.448, 49.449, 49.450],
+			[0,  7.760,  7.761,  7.762,  7.763,  7.764,  7.765, 0, 0, 0, 0, 0, 0,  7.766,  7.767,  7.768,  7.769,  7.770]
 		);
 
-		if ($Corrector->canHandleData()) {
-			$Corrector->correctElevation();
-			$this->assertEquals( array(238, 238, 238, 238, 238, 238, 238, 238, 238, 238, 238, 238, 238, 238, 238, 245, 245, 245), $Corrector->getCorrectedElevation() );
+		if ($corrector->canHandleData()) {
+            try {
+    			$corrector->correctElevation();
+	    		$this->assertEquals(
+	    		    [238, 238, 238, 238, 238, 238, 238, 238, 238, 238, 238, 238, 238, 238, 238, 245, 245, 245],
+                    $corrector->getCorrectedElevation()
+                );
+            } catch (NoResponseException $e) {
+                $this->markTestSkipped('Google Maps was not available.');
+            }
 		} else {
 			$this->markTestSkipped('Google Maps was not available.');
 		}
