e7006f0aba849369a9677b01bfcab72f34ad3b7f
Michael Pohl <michael@runalyze.com>
2017-11-05 19:57:10 +0100

    Fix pace in RSS feed & change title layout (fix #2232)

--- inc/core/View/Activity/Feed.php
+++ inc/core/View/Activity/Feed.php
@@ -4,6 +4,7 @@ namespace Runalyze\View\Activity;
 use PicoFeed\Syndication\Rss20FeedBuilder;
 use PicoFeed\Syndication\Rss20ItemBuilder;
 use Runalyze\Bundle\CoreBundle\Component\Activity\ActivityContext;
+use Runalyze\Bundle\CoreBundle\Component\Activity\ActivityDecorator;
 use Runalyze\Bundle\CoreBundle\Services\Activity\ActivityContextFactory;
 use Runalyze\Bundle\CoreBundle\Services\Configuration\ConfigurationManager;
 use Runalyze\Data\Cadence\Unit;
@@ -13,6 +14,8 @@ use Symfony\Component\Translation\TranslatorInterface;
 use Runalyze\Bundle\CoreBundle\Entity\Training;
 use Runalyze\Activity\PaceUnit;
 use Runalyze\Bundle\CoreBundle\Twig\ValueExtension;
+use Runalyze\Bundle\CoreBundle\Component\Configuration\UnitSystem;
+use Runalyze\Bundle\CoreBundle\Twig\DisplayableValue;
 
 class Feed {
 
@@ -34,6 +37,9 @@ class Feed {
     /** @var ConfigurationManager */
     protected $ConfigurationManager;
 
+    /** @var UnitSystem */
+    protected $UnitSystem;
+
     /**
      * Feed constructor.
      * @param TranslatorInterface $translator
@@ -49,6 +55,7 @@ class Feed {
         $this->UrlGenerator = $urlGenerator;
         $this->ConfigurationManager = $configurationManager;
         $this->FeedBuilder->withDate(new \DateTime());
+        $this->UnitSystem = new UnitSystem($configurationManager->getList());
     }
 
     /**
@@ -139,11 +146,13 @@ class Feed {
         if ($activity->getType() !== null) {
             $content .= '<br><b>'.$this->Translator->trans('Activity type') . '</b>: ' . $activity->getType()->getName();
         }
+        $content .= '<br><b>'.$this->Translator->trans('Date') . '</b>: '.(new LocalTime($activityContext->getActivity()->getTime()))->format('d.m.Y');
+
         $content .= '<br><b>'.$this->Translator->trans('Duration') . '</b>: '.(new \DateTime())->setTimezone(new \DateTimeZone("UTC"))->setTimestamp($activityContext->getActivity()->getS())->format('H:i:s');
 
         if ($activity->getDistance()) {
             $content .= '<br><b>' . $this->Translator->trans('Distance') . '</b>: ' . $valueDecorator->distance($activity->getDistance());
-            $content .= '<br><b>' . $this->Translator->trans('Pace') . '</b>: ' . $valueDecorator->pace($activity->getElapsedTime() / $activity->getDistance(), $activity->getSport()->getSpeedUnit());
+            $content .= '<br><b>' . $this->Translator->trans('Pace') . '</b>: ' . $valueDecorator->pace($activity->getS() / $activity->getDistance(), $activity->getSport()->getSpeedUnit());
         }
 
         if ($activityContext->getActivity()->getNotes()) {
@@ -163,22 +172,47 @@ class Feed {
      */
     private function createItem(Training $activity)
     {
-
         $item = new Rss20ItemBuilder($this->FeedBuilder);
         $activityContext = $this->ActivityContextFactory->getContext($activity);
-        $time = (new LocalTime($activityContext->getActivity()->getTime()))->format('d.m.Y');
+        $activity = $activityContext->getActivity();
+        $time = (new LocalTime($activity->getTime()))->format('d.m.Y');
         $account = $activity->getAccount();
         $valueDecorator = new ValueExtension($this->ConfigurationManager);
-        $item->withTitle($time.' - '.$activityContext->getSport()->getName().' - '.$valueDecorator->distance($activity->getDistance()));
 
+        $item->withTitle($this->getFeedTitle($activityContext, $valueDecorator));
 
-        $item->withPublishedDate(new LocalTime($activityContext->getActivity()->getTime()));
+        $item->withPublishedDate(new LocalTime($activity->getTime()));
         $item->withContent($this->createItemContent($activityContext, $valueDecorator));
         $item->withAuthor($account->getUsername());
-        if ($activityContext->getActivity()->isPublic()) {
-            $item->withUrl($this->UrlGenerator->generate('shared-activity', array('activityHash' => base_convert((int)$activityContext->getActivity()->getId(), 10, 35)), UrlGeneratorInterface::ABSOLUTE_URL));
+        if ($activity->isPublic()) {
+            $item->withUrl($this->UrlGenerator->generate('shared-activity', array('activityHash' => base_convert((int)$activity->getId(), 10, 35)), UrlGeneratorInterface::ABSOLUTE_URL));
         }
         $this->FeedBuilder->withItem($item);
+    }
+
+    /**
+     * @param ActivityContext $activityContext
+     * @param ValueExtension $valueDecorator
+     * @return string
+     */
+    private function getFeedTitle(ActivityContext $activityContext, ValueExtension $valueDecorator)
+    {
+        $title = $activityContext->getSport()->getName();
+
+        if (null !== $activityContext->getActivity()->getType()) {
+            $title .= ' ('. $activityContext->getActivity()->getType()->getName().')';
+        }
+        $title .= ': '. (new \DateTime())->setTimezone(new \DateTimeZone("UTC"))->setTimestamp($activityContext->getActivity()->getS())->format('H:i:s').'h - ';
+
+        if ($activityContext->getActivity()->getDistance() > 0) {
+            $title .= $valueDecorator->distance($activityContext->getActivity()->getDistance()). '';
+        }
+
+        if ('' != $activityContext->getActivity()->getTitle()) {
+            $title .= ' - '.$activityContext->getActivity()->getTitle();
+        }
+
+        return $title;
     }
 
     private function createItems()
