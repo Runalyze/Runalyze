58c95b2a675f88a3c37afbb532ed4b5f02fc4bc4
laufhannes <mail@laufhannes.de>
2018-02-04 10:06:02 +0100

    fix default start date for equipment

--- src/CoreBundle/Form/EquipmentType.php
+++ src/CoreBundle/Form/EquipmentType.php
@@ -66,7 +66,6 @@ class EquipmentType extends AbstractType
                 'format' => 'dd.MM.yyyy',
                 'html5' => false,
                 'required' => false,
-                'data' => new \DateTime("now"),
                 'attr' => ['class' => 'pick-a-date small-size', 'placeholder' => 'dd.mm.YYYY']
             ])
             ->add('dateEnd', DateType::class, [
