250b941494e6a224abeb188a6f0cd65c0f4df728
laufhannes <mail@laufhannes.de>
2018-02-06 11:58:56 +0000

    fix tcx parser for indoor activities
    + fix isPossibleDuplicate() if more than one activity with that id exists

--- tests/inc/core/Parser/Activity/Common/Data/ContinuousDataTest.php
+++ tests/inc/core/Parser/Activity/Common/Data/ContinuousDataTest.php
@@ -20,4 +20,31 @@ class ContinuousDataTest extends \PHPUnit_Framework_TestCase
             $this->assertTrue(is_array($this->Data->$name), 'Can\'t access property "'.$name.'".');
         }
     }
+
+    public function testTotalDurationAndDistance()
+    {
+        $this->Data->Time = [1, 2, 3, 4, 5, 6];
+        $this->Data->Distance = [0.005, 0.011, 0.016, 0.020, 0.024, 0.026];
+
+        $this->assertEquals(6, $this->Data->getTotalDuration());
+        $this->assertEquals(0.026, $this->Data->getTotalDistance());
+    }
+
+    public function testTotalDurationAndDistanceIfNotPresent()
+    {
+        $this->Data->HeartRate = [126, 128, 129, 130, 130, 133];
+
+        $this->assertNull($this->Data->getTotalDuration());
+        $this->assertNull($this->Data->getTotalDistance());
+    }
+
+    public function testTotalDurationAndDistanceForEmptyArrays()
+    {
+        $this->Data->Time = [0, 0, 0, 0, 0, 0];
+        $this->Data->Distance = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
+        $this->Data->HeartRate = [126, 128, 129, 130, 130, 133];
+
+        $this->assertNull($this->Data->getTotalDuration());
+        $this->assertNull($this->Data->getTotalDistance());
+    }
 }
